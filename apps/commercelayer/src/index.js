8ce22f33afd09a5374ad5148ade3930e4092de7f
Martin Lehner <lehner.m.e@gmail.com>
2020-02-06 13:24:00 +0100

    Use correct commercelayer app as source

--- apps/commercelayer/src/index.js
+++ apps/commercelayer/src/index.js
@@ -1,169 +1,172 @@
-import { init } from 'contentful-ui-extensions-sdk';
-import debounce from 'lodash/debounce';
-import axios from 'axios';
-import Vue from 'vue/dist/vue.esm.js';
-import './index.css';
-import 'bulma/css/bulma.css';
+import CLayerAuth from '@commercelayer/js-auth';
+import chunk from 'lodash/chunk';
+import flatMap from 'lodash/flatMap';
 
-init(function(extension) {
-  extension.window.startAutoResizer();
+import { setup, renderSkuPicker } from 'shared-sku-app';
 
-  // Available SKU component
-  Vue.component('sku-item', {
-    props: ['sku', 'endpoint'],
-    template: `<div class="column is-one-third">
-            <div class="sku" @click="selectSku">
-              <img :src="sku.attributes.image_url"></img>
-              <p class="name">{{ sku.attributes.name }}</p>
-              <p class="help">{{ sku.attributes.code }}</p>
-            </div>
-          </div>`,
-    methods: {
-      selectSku: function() {
-        this.$emit('select-sku', this.sku.id);
-      }
+import logo from './logo.svg';
+import descriptor from '../extension.json';
+import { dataTransformer } from './dataTransformer';
+
+const DIALOG_ID = 'root';
+const PER_PAGE = 20;
+
+let accessToken = null;
+
+function makeCTA(fieldType) {
+  return fieldType === 'Array' ? 'Select products' : 'Select a product';
+}
+
+function validateParameters(parameters) {
+  if (parameters.clientId.length < 1) {
+    return 'Provide your Commerce Layer client ID.';
+  }
+
+  if (parameters.apiEndpoint.length < 1) {
+    return 'Provide the Commerce Layer API endpoint.';
+  }
+
+  return null;
+}
+
+async function getAccessToken(clientId, endpoint) {
+  if (!accessToken) {
+    /* eslint-disable-next-line require-atomic-updates */
+    accessToken = (await CLayerAuth.getIntegrationToken({
+      clientId,
+      endpoint,
+      // The empty client secret is needed for legacy reasons, as the
+      // CLayerAuth SDK will throw if not present. By setting to empty
+      // string we prevent the SDK exception and the value is ignored
+      // by the Commerce Layer Auth API.
+      clientSecret: ''
+    })).accessToken;
+  }
+  return accessToken;
+}
+
+/**
+ * This function is needed to make the pagination of Commerce Layer work with the
+ * shared-sku-app library.
+ *
+ * When fetching the SKUs via the Commerce Layer JS SDK the metadata object which
+ * includes the total count of records needed by the shared-sku-picker paginator
+ * is missing. But it is there when fetching the SKUs via a plain HTTP req.
+ */
+async function fetchSKUs(installationParams, search, pagination) {
+  const validationError = validateParameters(installationParams);
+  if (validationError) {
+    throw new Error(validationError);
+  }
+
+  const { clientId, apiEndpoint } = installationParams;
+  const accessToken = await getAccessToken(clientId, apiEndpoint);
+
+  const URL = `${apiEndpoint}/api/skus?page[size]=${PER_PAGE}&page[number]=${pagination.offset /
+    PER_PAGE +
+    1}${search.length ? `&filter[q][name_or_code_cont]=${search}` : ''}`;
+
+  const res = await fetch(URL, {
+    headers: {
+      Accept: 'application/vnd.api+json',
+      Authorization: `Bearer ${accessToken}`
+    },
+    method: 'GET'
+  });
+
+  return await res.json();
+}
+
+/**
+ * Fetches the product previews for the products selected by the user.
+ */
+const fetchProductPreviews = async function fetchProductPreviews(skus, config) {
+  if (!skus.length) {
+    return [];
+  }
+
+  const PREVIEWS_PER_PAGE = 25;
+
+  const { clientId, apiEndpoint } = config;
+  const accessToken = await getAccessToken(clientId, apiEndpoint);
+
+  // Commerce Layer's API automatically paginated results for collection endpoints.
+  // Here we account for the edge case where the user has picked more than 25
+  // products, which is the max amount of pagination results. We need to fetch
+  // and compile the complete selection result doing 1 request per 25 items.
+  const resultPromises = chunk(skus, PREVIEWS_PER_PAGE).map(async skusSubset => {
+    const URL = `${apiEndpoint}/api/skus?page[size]=${PREVIEWS_PER_PAGE}&filter[q][code_in]=${skusSubset}`;
+    const res = await fetch(URL, {
+      headers: {
+        Accept: 'application/vnd.api+json',
+        Authorization: `Bearer ${accessToken}`
+      },
+      method: 'GET'
+    });
+    return await res.json();
+  });
+
+  const results = await Promise.all(resultPromises);
+
+  return flatMap(results, ({ data }) => data.map(dataTransformer(config.apiEndpoint)));
+};
+
+async function renderDialog(sdk) {
+  const container = document.getElementById(DIALOG_ID);
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+
+  renderSkuPicker(DIALOG_ID, {
+    sdk,
+    fetchProductPreviews,
+    fetchProducts: async (search, pagination) => {
+      const result = await fetchSKUs(sdk.parameters.installation, search, pagination);
+
+      return {
+        pagination: {
+          count: PER_PAGE,
+          limit: PER_PAGE,
+          total: result.meta.record_count,
+          offset: pagination.offset
+        },
+        products: result.data.map(dataTransformer(sdk.parameters.installation.apiEndpoint))
+      };
     }
   });
 
-  // Selected SKU component
-  Vue.component('sku-selected', {
-    props: ['sku', 'endpoint'],
-    template: `<div class="column is-one-third">
-            <div class="selected-sku">
-              <img :src="sku.attributes.image_url"></img>
-              <p class="name">{{ sku.attributes.name }}</p>
-              <p class="help">{{ sku.attributes.code }}</p>
-              <p class="help">
-                <a :href="skuEditURL" target="_blank">Edit</a>
-                &middot;
-                <a @click="unselectSku" class="has-text-danger">Remove</a>
-              </p>
-            </div>
-          </div>`,
-    methods: {
-      unselectSku: function() {
-        this.$emit('unselect-sku');
-      }
-    },
-    computed: {
-      skuEditURL() {
-        return `${this.endpoint}/admin/skus/${this.sku.id}/edit`;
-      }
-    }
+  sdk.window.startAutoResizer();
+}
+
+async function openDialog(sdk, currentValue, config) {
+  const skus = await sdk.dialogs.openCurrentApp({
+    allowHeightOverflow: true,
+    position: 'center',
+    title: makeCTA(sdk.field.type),
+    shouldCloseOnOverlayClick: true,
+    shouldCloseOnEscapePress: true,
+    parameters: config,
+    width: 1400
   });
 
-  new Vue({
-    el: '#app',
-    data: {
-      accessToken: null,
-      endpoint: null,
-      query: null,
-      selectedSkuId: null,
-      skus: [],
-      links: {},
-      meta: {}
-    },
-    computed: {
-      selectedSku() {
-        return this.skus.find(sku => {
-          return sku.id === this.selectedSkuId;
-        });
-      }
-    },
-    created: function() {
-      // Set endpoint
-      this.endpoint = extension.parameters.instance.endpoint;
+  return Array.isArray(skus) ? skus : [];
+}
 
-      // Assign the debounced function
-      this.debouncedGetSkus = debounce(this.getSkus, 500);
+function isDisabled(/* currentValue, config */) {
+  // No restrictions need to be imposed as to when the field is disabled from the app's side
+  return false;
+}
 
-      // Get an access token (valid for 2 hours)
-      // Get the selected SKU or the list of all SKUs
-      axios
-        .post(`${this.endpoint}/oauth/token`, {
-          grant_type: 'client_credentials',
-          client_id: extension.parameters.instance.clientID,
-          client_secret: extension.parameters.instance.clientSecret
-        })
-        .then(response => {
-          this.accessToken = response.data.access_token;
-          const fieldValue = extension.field.getValue();
-          if (fieldValue) {
-            const skuId = fieldValue.id;
-            this.getSku(skuId);
-            this.selectedSkuId = skuId;
-          } else {
-            this.getSkus();
-          }
-        })
-        .catch(function() {
-          document.getElementById('commerce-layer-hint').innerHTML =
-            'Error connecting to Commerce Layer. Please double check your configuration.';
-        });
-    },
-    methods: {
-      getSku: function(skuId) {
-        axios
-          .get(`${this.endpoint}/api/skus/${skuId}`, {
-            headers: {
-              Accept: 'application/vnd.api+json',
-              Authorization: `Bearer ${this.accessToken}`
-            }
-          })
-          .then(response => {
-            this.skus.push(response.data.data);
-          });
-      },
-      getSkus: function(url) {
-        if (url === undefined) {
-          url = `${this.endpoint}/api/skus`;
-        }
-        axios
-          .get(url, {
-            params: {
-              'filter[q][name_or_code_or_reference_cont]': this.query,
-              'page[size]': 9
-            },
-            headers: {
-              Accept: 'application/vnd.api+json',
-              Authorization: `Bearer ${this.accessToken}`
-            }
-          })
-          .then(response => {
-            this.skus = response.data.data;
-            this.links = response.data.links;
-            this.meta = response.data.meta;
-          });
-      },
-      prevPage: function() {
-        this.getSkus(this.links.prev);
-      },
-      nextPage: function() {
-        this.getSkus(this.links.next);
-      },
-      selectSku: function(skuId) {
-        this.selectedSkuId = skuId;
-        this.setFieldValue();
-      },
-      unselectSku: function() {
-        this.selectedSkuId = null;
-        extension.field.setValue(null);
-        this.query = null;
-        this.getSkus();
-      },
-      setFieldValue: function() {
-        extension.field.setValue({
-          id: this.selectedSku.id,
-          code: this.selectedSku.attributes.code,
-          link: this.selectedSku.links.self,
-          mode: this.selectedSku.meta.mode
-        });
-      }
-    },
-    watch: {
-      query: function() {
-        this.debouncedGetSkus();
-      }
-    }
-  });
+setup({
+  makeCTA,
+  name: 'Commerce Layer',
+  logo,
+  description:
+    'The Commerce Layer app allows editors to select products from their Commerce Layer account and reference them inside of Contentful entries.',
+  color: '#212F3F',
+  parameterDefinitions: descriptor.parameters.installation,
+  fetchProductPreviews,
+  renderDialog,
+  openDialog,
+  isDisabled,
+  validateParameters
 });
