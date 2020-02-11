60a230dfac2e9f44b8d7384a2d2936ea762c5403
Jakub Elżbieciak <jakub@contentful.com>
2020-02-11 15:39:27 +0100

    Use .promise() for Rekognition calls

--- apps/ai-image-tagging/lambda/tag.js
+++ apps/ai-image-tagging/lambda/tag.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { promisify } = require('util');
-
 const fetch = require('node-fetch');
 const AWS = require('aws-sdk');
 
 const IMAGES_BASE = 'https://images.ctfassets.net';
+
+const rekog = new AWS.Rekognition();
 
 const fetchImage = async imageUrl => {
   const res = await fetch(imageUrl);
@@ -15,20 +15,18 @@ const fetchImage = async imageUrl => {
   } else {
     throw new Error(`Non-200 (${res.status}) response for GET ${imageUrl}.`);
   }
-}
+};
 
 const getDetectParams = imageData => ({
   Image: { Bytes: imageData, },
   MaxLabels: 10,
   MinConfidence: 70.0
-})
+});
 
 module.exports = async path => {
   const imageData = await fetchImage(IMAGES_BASE + path);
+  const params = getDetectParams(imageData);
+  const tags = await rekog.detectLabels(params).promise();
 
-  const rekog = new AWS.Rekognition()
-  const detectLabels = promisify(rekog.detectLabels.bind(rekog));
-  const tags = await detectLabels(getDetectParams(imageData));
-
-  return tags.Labels.map(label => label.Name)
+  return tags.Labels.map(label => label.Name);
 }
