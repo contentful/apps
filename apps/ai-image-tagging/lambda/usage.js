24a053cd6544b328a88aa7a552e86cee18361a27
Jakub Elżbieciak <jakub@contentful.com>
2020-02-11 15:57:08 +0100

    Use Dynamo DocumentClient for usage

--- apps/ai-image-tagging/lambda/usage.js
+++ apps/ai-image-tagging/lambda/usage.js
@@ -1,39 +1,30 @@
 'use strict';
 
-const { promisify } = require('util');
-
 const AWS = require('aws-sdk');
 
-module.exports = async spaceId => {
-  const ddb = new AWS.DynamoDB();
-  const updateItem = promisify(ddb.updateItem.bind(ddb));
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
+module.exports = async spaceId => {
   const now = new Date();
   const year = `${now.getFullYear()}`;
   const month = `${now.getMonth()+1}`;
   const formattedMonth = month.length === 1 ? `0${month}` : month;
   const period = [year, formattedMonth].join('-');
 
-  const { Attributes } = await updateItem({
+  const { Attributes } = await documentClient.update({
     TableName: process.env.TABLE_NAME,
-    Key: {
-      period: { S: period },
-      spaceId: { S: spaceId }
-    },
+    Key: { period, spaceId },
     UpdateExpression: 'SET #attr = if_not_exists(#attr, :zero) + :incr',
     ExpressionAttributeNames: { '#attr': 'reqs' },
-    ExpressionAttributeValues: {
-      ':incr': { N: '1' },
-      ':zero': { N: '0' }
-    },
+    ExpressionAttributeValues: { ':incr': 1, ':zero': 0 },
     ReturnValues: 'ALL_NEW'
-  });
+  }).promise();
 
-  const reqs = Attributes && Attributes.reqs && Attributes.reqs.N;
+  const reqs = Attributes && Attributes.reqs;
 
-  if (reqs) {
-    return parseInt(reqs, 10);
-  } else {
+  if (!reqs) {
     throw new Error(`Failed to report usage for ${spaceId}.`);
   }
+
+  return reqs;
 }
