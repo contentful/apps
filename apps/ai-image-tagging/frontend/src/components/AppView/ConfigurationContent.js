fc99f540473849639c66ecc35407a4e68d5cfbf3
Kado Damball <kadodamball@hotmail.com>
2020-02-25 14:01:29 +0100

    Copy fixes for AI tagging

--- apps/ai-image-tagging/frontend/src/components/AppView/ConfigurationContent.js
+++ apps/ai-image-tagging/frontend/src/components/AppView/ConfigurationContent.js
@@ -13,27 +13,27 @@ export const ConfigurationContent = () => (
         Create a new entry of type &quot;AI Image Tagging&quot; (or the name you chose during
         the installation)
       </ListItem>
-      <ListItem>Fill in the required fields and publish it</ListItem>
+      <ListItem>Fill in the required fields and publish</ListItem>
     </List>
     <Paragraph className={styles.paragraph}>
       Follow the next steps to reference the newly created image with AI Image Tagging in another
       content type:
     </Paragraph>
     <List className={styles.list}>
-      <ListItem>Go to the &quot;Content model&quot; page</ListItem>
+      <ListItem>Go to the &quot;content model&quot; page</ListItem>
       <ListItem>
         Edit the content type that needs to reference the image with the AI tags
       </ListItem>
-      <ListItem>Create a new field of type &quot;Reference&quot;</ListItem>
+      <ListItem>Create a new field of type &quot;reference&quot;</ListItem>
       <ListItem>
         Set a validation rule requiring that the content type the reference points to is of type
         &quot;AI Image Tagging&quot;
       </ListItem>
       <ListItem>
-        Optionally set the appearance of the new reference field to &quot;Entry Card&quot; in its
+        Optionally set the appearance of the new reference field to &quot;entry card&quot; in its
         settings
       </ListItem>
-      <ListItem>Go back to the &quot;Content&quot; page</ListItem>
+      <ListItem>Go back to the &quot;content&quot; page</ListItem>
       <ListItem>
         Create/edit an entry of the content type referencing the image with the AI tags
       </ListItem>
@@ -48,15 +48,15 @@ export const ConfigurationContent = () => (
       we created for you during the installation. To do that follow the next steps:
     </Paragraph>
     <List className={styles.list}>
-      <ListItem>Go to the &quot;Content&quot; page</ListItem>
+      <ListItem>Go to the &quot;content&quot; page</ListItem>
       <ListItem>
         Delete any entries of type &quot;AI Image Tagging&quot; (or the name you chose during
         installation)
       </ListItem>
-      <ListItem>Go to the &quot;Content model&quot; page</ListItem>
+      <ListItem>Go to the &quot;content model&quot; page</ListItem>
       <ListItem>
-        Edit the content type the app created, select &quot;Actions&quot; from the top menu and
-        &quot;Delete&quot;
+        Edit the content type the app created, select &quot;actions&quot; from the top menu and
+        &quot;delete&quot;
       </ListItem>
     </List>
   </>
