742156e1612ed04b1d33b3f6c6ad91be15e621d4
Yiotis Kaltsikis <giotisgr@gmail.com>
2020-02-27 16:30:01 +0100

    Make header sticky for larger screens

--- lib/shared-sku-app/src/SkuPicker/styles.ts
+++ lib/shared-sku-app/src/SkuPicker/styles.ts
@@ -1,26 +1,39 @@
 import { css } from 'emotion';
 import tokens from '@contentful/forma-36-tokens';
 
-/**
- * Returns the target height of the modal window body in pixels.
- * Relative units were causing multiple resizings of the iframe.
- */
-function getBodyHeight() {
-  const HEADER_HEIGHT = 114;
+const HEADER_HEIGHT = 114;
+
+const SHOULD_HAVE_STICKY_HEADER = window.outerHeight >= 900;
+
+function makeBodyStyle() {
+  // Modal window height dynamically calculated in pixels.
+  // Usage of css relative units was causing a bug with
+  // multiple resizings of the modal window.
   const twentyPerc = window.outerHeight * 0.3;
-  return window.outerHeight - twentyPerc - HEADER_HEIGHT;
+  const height = window.outerHeight - twentyPerc - HEADER_HEIGHT;
+
+  const padding = SHOULD_HAVE_STICKY_HEADER
+    ? `calc(${tokens.spacingL} + ${HEADER_HEIGHT}px)  ${tokens.spacingL} 0 ${tokens.spacingL}`
+    : `${tokens.spacingL} ${tokens.spacingL} 0 ${tokens.spacingL}`;
+
+  return css({ height, padding });
 }
 
 export const styles = {
   header: css({
+    borderBottom: `1px solid ${tokens.colorElementMid}`,
     display: 'flex',
     justifyContent: 'space-between',
-    padding: tokens.spacingL
+    padding: tokens.spacingL,
+    ...(SHOULD_HAVE_STICKY_HEADER && {
+      backgroundColor: 'white',
+      position: 'fixed',
+      top: 0,
+      zIndex: 1,
+      width: `calc(100% - 2rem)`
+    })
   }),
-  body: css({
-    height: getBodyHeight(),
-    padding: `${tokens.spacingL} ${tokens.spacingL} 0 ${tokens.spacingL}`
-  }),
+  body: makeBodyStyle(),
   total: css({
     fontSize: tokens.fontSizeS,
     color: tokens.colorTextLight,
