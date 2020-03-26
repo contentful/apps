fb22fc8d7ec3e6e0c53ff714eaaab45679fea5f6
Frantisek Dufka <frantisek.dufka@oriflame.com>
2020-03-26 12:56:16 +0100

    add start folder for media library dialog
    Add parameter for starting folder to show() method as per https://cloudinary.com/documentation/media_library_widget#3_set_the_configuration_options
    This is global setting per space. In future It would be also nice to have such setting per content type (instance parameter?).

--- apps/cloudinary/src/index.js
+++ apps/cloudinary/src/index.js
@@ -77,7 +77,7 @@ function renderDialog(sdk) {
     insertHandler: data => sdk.close(data)
   });
 
-  instance.show();
+  instance.show({folder: {path: config.startFolder}});
 
   sdk.window.updateHeight(window.outerHeight);
 }
@@ -153,6 +153,14 @@ setup({
       "type": "Number",
       "required": false,
       "default": 10
+    },
+    {
+      "id": "startFolder",
+      "name": "Starting folder",
+      "description": "Path to starting folder for the media library dialog",
+      "type": "Symbol",
+      "required": false,
+      "default": ""
     }
   ],
   makeThumbnail,
