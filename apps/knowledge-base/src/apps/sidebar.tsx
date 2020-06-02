0ec823e97f73c37d7e17b6265b3d253cb9d8cb13
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-06-02 14:53:08 +0200

    feat: add footer buttons and improve error messages

--- apps/knowledge-base/src/apps/sidebar.tsx
+++ apps/knowledge-base/src/apps/sidebar.tsx
@@ -3,7 +3,6 @@ import {
   Paragraph,
   Button as FormaButton,
   Spinner,
-  Icon,
 } from '@contentful/forma-36-react-components';
 import styled from '@emotion/styled';
 import { useNetlify } from '../providers/netlify-provider';
@@ -18,66 +17,64 @@ const Button = styled(FormaButton)<{
 `;
 
 const Text = styled(Paragraph)`
-  margin: 8px 0;
+  margin: 4px 0;
 
   color: #888;
-  font-size: 12px;
-`;
-
-const SpinnerContainer = styled.div`
-  text-align: center;
+  font-size: 14px;
 `;
 
 const BuildButtonContainer = styled.div`
   margin-bottom: 16px;
 `;
 
-const TextWithIcon = styled(Text)`
-  display: grid;
-  grid-template-columns: 18px auto;
-  column-gap: 4px;
-  align-items: start;
-
-  & > svg {
-    margin-right: 4px;
-  }
-`;
-
-const ErrorText = styled(TextWithIcon)`
+const ErrorText = styled(Text)`
   color: #d9453f;
 `;
 
-const SuccessText = styled(TextWithIcon)`
-  color: #0eb87f;
+const Footer = styled.footer`
+  margin-top: 8px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 16px;
 `;
 
 const Sidebar: React.FC = () => {
   const sdk = useSdk();
   const netlify = useNetlify();
-  const params: Record<string, any> = sdk.instance.parameters.installation;
-  const { id: entryId, contentType } = sdk.instance.entry.getSys();
-  const buildHookUrl = params.netlifySelectedSiteBuildHookUrl;
-  const siteUrl = params.netlifySelectedSiteUrl;
-  const siteId = params.netlifySelectedSiteId;
-  const [lastReadyBuild, setLastReadyBuild] = useState<Record<string, any>>({});
+
+  const [siteSettingsUrl, setSiteSettingsUrl] = useState('');
   const [currentBuild, setCurrentBuild] = useState<Record<string, any>>({});
   const [isBuilding, setIsBuilding] = useState(false);
   const [hasBuildFailed, setHasBuildFailed] = useState(false);
   const [isLoadingDeployInfo, setIsLoadingDeployInfo] = useState(false);
   const [deployInfoFailed, setDeployInfoFailed] = useState(false);
   const [failedBuildReason, setFailedBuildReason] = useState();
-  const lastBuildDate = isBuilding
-    ? currentBuild?.created_at
-    : lastReadyBuild?.created_at;
+
+  const params: Record<string, any> = sdk.instance.parameters.installation;
+  const buildHookUrl = params.netlifySelectedSiteBuildHookUrl;
+  const siteUrl = params.netlifySelectedSiteUrl;
+  const siteId = params.netlifySelectedSiteId;
+
+  const { id: entryId, contentType } = sdk.instance.entry.getSys();
   const shouldShowPreviewButton =
     !!siteUrl && contentType.sys.id === 'kbAppArticle';
+
   const hasWebsiteBeenBuilt =
     !deployInfoFailed && !isLoadingDeployInfo && !hasBuildFailed && !isBuilding;
   const hasBuildBeenFailed = hasBuildFailed && !isBuilding;
 
   useEffect(() => {
     sdk.instance.window.startAutoResizer();
+
     loadDeploys();
+
+    sdk.getEntries({ content_type: 'kbAppSiteSettings' }).then((data) => {
+      const url = data.items[0]
+        ? `https://app.contentful.com/spaces/${sdk.instance.ids.space}/entries/${data.items[0].sys.id}`
+        : '';
+
+      setSiteSettingsUrl(url);
+    });
   }, []);
 
   useEffect(() => {
@@ -107,18 +104,14 @@ const Sidebar: React.FC = () => {
 
       const deploys = await netlify.getDeploysBySiteId(siteId);
       const hasBuildFailed = deploys[0].state === 'error';
-      const ready = deploys.find((deploy) => deploy.state === 'ready');
-      const build = deploys.find((deploy) =>
-        ['enqueued', 'building', 'uploading'].some(
-          (status) => status === deploy.state
-        )
+      const isBuilding = ['enqueued', 'building', 'uploading'].some(
+        (status) => status === deploys[0].state
       );
 
+      setCurrentBuild(deploys[0]);
+      setIsBuilding(isBuilding);
       setHasBuildFailed(hasBuildFailed);
       setFailedBuildReason(hasBuildFailed ? deploys[0].summary.status : '');
-      setLastReadyBuild(ready);
-      setCurrentBuild(build);
-      setIsBuilding(!!build);
     } catch (err) {
       setDeployInfoFailed(true);
     } finally {
@@ -127,6 +120,8 @@ const Sidebar: React.FC = () => {
   }
 
   function formatDate(date) {
+    if (!date) return null;
+
     return new Intl.DateTimeFormat('en-us', {
       weekday: 'long',
       year: 'numeric',
@@ -141,8 +136,7 @@ const Sidebar: React.FC = () => {
   if (!siteUrl && !buildHookUrl) {
     return (
       <ErrorText>
-        <Icon icon="InfoCircle" color="negative" />
-        <span>No website was found. Go to the app settings to add one.</span>
+        No website was found. Go to the app settings to add one.
       </ErrorText>
     );
   }
@@ -157,7 +151,7 @@ const Sidebar: React.FC = () => {
           isFullWidth={true}
           buttonType="muted"
         >
-          Open preview
+          Preview article
         </Button>
       )}
 
@@ -168,67 +162,65 @@ const Sidebar: React.FC = () => {
             type="button"
             disabled={isBuilding}
             isFullWidth={true}
+            buttonType="muted"
           >
             {isBuilding ? (
               <span>
-                Building... <Spinner color="white" size="small" />
+                Building... <Spinner size="small" />
               </span>
             ) : (
               'Build website'
             )}
           </Button>
         )}
-        {isLoadingDeployInfo && (
-          <SpinnerContainer>
-            <Spinner size="small" />
-          </SpinnerContainer>
-        )}
         {deployInfoFailed && (
           <ErrorText>
-            <Icon icon="ErrorCircle" color="negative" />
-            <span>
-              We can&apos;t load your website deploy information, please make
-              sure a Netlify account is connected to the app settings.
-            </span>
+            We can&apos;t load your website deploy information, please make sure
+            a Netlify account is connected to the app settings.
           </ErrorText>
         )}
         {!buildHookUrl && (
           <ErrorText>
-            <Icon icon="ErrorCircle" color="negative" />
-            <span>
-              We couldn&apos;t find your build url. Please, make sure you have
-              selected and saved your website on the app settings and your
-              website has a Build Hook named <b>Contentful</b> on Netlify.
-            </span>
+            We couldn&apos;t find your build url. Please, make sure you have
+            selected and saved your website on the app settings and your website
+            has a Build Hook named <b>Contentful</b> on Netlify.
           </ErrorText>
-        )}
-        {lastBuildDate && (
-          <Text>Last build triggered on {formatDate(lastBuildDate)}.</Text>
         )}
         {hasBuildBeenFailed && (
           <ErrorText>
-            <Icon icon="ErrorCircle" color="negative" />{' '}
-            <span>Build failed. Netlify reason: {failedBuildReason}.</span>
+            Build failed on {formatDate(currentBuild.created_at)} Netlify
+            reason: {failedBuildReason}.
           </ErrorText>
         )}
         {hasWebsiteBeenBuilt && (
-          <SuccessText>
-            <Icon icon="CheckCircle" color="positive" />{' '}
-            <span>Success! Your website has finished building.</span>
-          </SuccessText>
+          <Text>
+            Built successfully on {formatDate(currentBuild.created_at)}.
+          </Text>
         )}
       </BuildButtonContainer>
 
-      {siteUrl && (
-        <Button
-          href={siteUrl}
-          target="_blank"
-          icon="ExternalLink"
-          isFullWidth={true}
-          buttonType="muted"
-        >
-          Open website
-        </Button>
+      {siteUrl && siteSettingsUrl && (
+        <Footer>
+          <Button
+            href={siteUrl}
+            target="_blank"
+            icon="ExternalLink"
+            isFullWidth={true}
+            buttonType="naked"
+          >
+            Open website
+          </Button>
+
+          <Button
+            href={siteSettingsUrl}
+            target="_blank"
+            icon="Settings"
+            isFullWidth={true}
+            buttonType="naked"
+          >
+            Site settings
+          </Button>
+        </Footer>
       )}
     </div>
   );
