21759ef973c50ac067ff06d52966e34a1f311586
Peter Wielander <peter.wielander@contentful.com>
2020-10-05 15:58:44 +0200

    demo-backend: simplify by using node-apps-toolkit

--- examples/default-values-backend/README.md
+++ examples/default-values-backend/README.md
@@ -4,22 +4,23 @@ A demo App that will prefill certain values when an entry is created.
 
 The purpose of this demo is to show how backend Apps can be setup and used.
 
-
 ## Prerequisites
+
 This demo requires an up to date version of node.js and yarn.
 
 In addition you will need a way to make the running app accessible over http, so
-that it can receive events from contentful. If you want to run the App locally, 
-we suggest using [ngrok](https://ngrok.com/) for this. It is a tool that allows 
-you to easily create a url that allows access to applications served from
-localhost.
+that it can receive events from contentful. If you want to run the App locally,
+we suggest using [ngrok](https://ngrok.com/) for this. It is a free tool that
+allows you to easily create a url that allows access to applications served from localhost.
 
 ## Setup instructions
 
 1. Run
-shell```
+
+```shell
 ngrok http 3543
 ```
+
 Take note of the url provided by NGROK. This step is only necessary when running
 the app locally.
 
@@ -27,7 +28,8 @@ Although we are running the `http` command - ngrok will provide a https address.
 We will need to use this secure URL for our app setup to work correctly.
 
 2. Update `.env` with
-shell```
+
+```shell
 CMA_TOKEN= # You can obtain this token from the contentful web app
 ORG_ID= # Your contentful organisation id
 SPACE_ID= # Your contentful spaece id
@@ -35,36 +37,34 @@ ENVIRONMENT_ID= # Your contentul enviornment name
 HOSTED_APP_URL= # The ngrok URL from step 1 - this must a https URL
 ```
 
-3. Generate a public and private key pair
-```shell
-./tools/makekeys
-```
-
 ### Warning!
-The setup for this demo will make install an app, and create a content type in
-whatever orgnization/space/environment you use. We highly recommend that you do
-not use your production environment for testing this App.
 
+The setup for this demo will install an app, and create a content type in
+the organization/space/environment you supplied in your `.env` file.
+We highly recommend that you do not use your production environment for testing this App.
 
-4. Run the app's setup script
+3. Run the app's setup script
 
 ```shell
 yarn
 yarn setup
 ```
+
 This step will
-* Install dependencies
-* Generate public and private keys for your app
-* Create an app definition in your contentful organization
-* Install the app to the space and enviornment you specified
-* Setup a webhook that listens for new entries
+
+- Install dependencies
+- Generate public and private keys for your app
+- Create an app definition in your contentful organization
+- Install the app to the space and environment you specified
+- Setup a webhook that listens for new entries
+
+This step puts a new public and private key pair under `./keys`.
 
 5. Run the backend App!
+
 ```shell
 yarn start
 ```
 
 6. Test it by going to the contentful web app and creating a new entry of the
    `Example` content type. You should see a prefilled title field!
-
-
--- examples/default-values-backend/src/index.ts
+++ examples/default-values-backend/src/index.ts
@@ -1,25 +1,32 @@
+/*
+ * This file is the application backend that would run on your servers.
+ * It's a very straight forward Hapi server that listens for calls
+ * from a webhook, and then uses an AppToken to interact
+ * with the Content Management API (CMA).
+ */
+
 import * as Hapi from "@hapi/hapi";
 import fetch from "node-fetch";
-import { makeAppToken, getAppAccessToken, getPrivateKey, getKeyId } from "./utils";
+import { getManagementToken } from "@contentful/node-apps-toolkit";
 import Path from "path";
 import dotenv from "dotenv";
+import fs from 'fs'
 dotenv.config();
 
-const { APP_ID, CONTENT_TYPE_ID, BASE_URL } = process.env;
+const { APP_ID, CONTENT_TYPE_ID, BASE_URL, PRIVATE_APP_KEY } = process.env;
 
-/* This file is our backend App. It's a very straight forward Hapi server that
- * listens for calls from a webhook, and then uses an AppToken to interact
- * with the Content Management Api (CMA).
- */
+if (!APP_ID) {
+  throw new Error("APP ID not specified. Make sure to run app setup first.");
+}
+if (!PRIVATE_APP_KEY || !fs.existsSync(PRIVATE_APP_KEY)) {
+  throw new Error("Private key not found. Make sure to run app setup first.");
+}
 
 // -------------------
 // MAIN SERVER
 // -------------------
 
 const startServer = async () => {
-  // First we create a JWT token based on our private key, and the App's id
-  const appToken = makeAppToken(APP_ID, getPrivateKey(), getKeyId());
-
   const server = Hapi.server({
     port: 3543,
     host: "localhost",
@@ -33,8 +40,14 @@ const startServer = async () => {
   await server.register(require("@hapi/inert"));
 
   // Here we are attaching the webook handler to our Server
-  server.route(addDefaultData(appToken));
+  server.route({
+    method: "POST",
+    path: "/event-handler",
+    handler: addDefaultDataOnEntryCreation,
+  });
 
+  // Here we register a very simple frontend to allow changing settings
+  // The files for this are under ../public
   server.route({
     method: "GET",
     path: "/frontend",
@@ -42,7 +55,6 @@ const startServer = async () => {
       return h.file("index.html");
     },
   });
-
   server.route({
     method: "GET",
     path: "/{param*}",
@@ -58,14 +70,14 @@ const startServer = async () => {
   console.log("Server running on %s", server.info.uri);
 };
 
-process.on("unhandledRejection", err => {
+process.on("unhandledRejection", (err) => {
   console.error("[Unhandled Rejection]");
 
   console.log(err);
   process.exit(1);
 });
 
-process.on("uncaughtException", error => {
+process.on("uncaughtException", (error) => {
   console.error("[Uncaught Exception]");
   console.error(error);
 
@@ -80,81 +92,86 @@ startServer();
 
 // This route is listening to a webhook that is setup to call whenever an
 // Entry of our example content type is created
-const addDefaultData = (appToken: string) => ({
-  method: "POST",
-  path: "/event-handler",
-  handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-    try {
-      const payload = request.payload as {
-        sys: 
-          { id: string; 
-            version: string; 
-            space: { sys: { id: string } }; 
-            environment: { sys: { id: string } }; 
-            contentType: { sys: { id: string } } ;
-          };
+const addDefaultDataOnEntryCreation = async (
+  request: Hapi.Request,
+  h: Hapi.ResponseToolkit
+) => {
+  try {
+    const payload = request.payload as {
+      sys: {
+        id: string;
+        version: string;
+        space: { sys: { id: string } };
+        environment: { sys: { id: string } };
+        contentType: { sys: { id: string } };
       };
+    };
 
-      // First we extract the Entry id and version from the payload
+    // First we extract the Entry id and version from the payload
+    const { id, version, contentType, space, environment } = payload.sys;
+    console.log(`Received webhook request because Entry ${id} was created`);
 
-      const { id, version, contentType, space, environment } = payload.sys;
-      const spaceId = space.sys.id;
-      const environmentId = environment.sys.id;
-
-      console.log(`Received webhook request because Entry ${id} was created`);
-
-      if (contentType.sys.id !== CONTENT_TYPE_ID) {
-        // If the content type does not match the one we created in setup, we just
-        // ignore the event
-        console.log(
-          `Entry's content type: ${contentType.sys.id} did not match the content type created for the App, ignoring`
-        );
-        return h.response("success").code(204);
-      }
-
-      // We then use that token to get a token from Contentful which our App can use
-      // to interact with the CMA
-      const appAccessToken = await getAppAccessToken(appToken, spaceId, environmentId, APP_ID);
-
-      const appInstallation = await fetch(
-        `${BASE_URL}/spaces/${spaceId}/environments/${environmentId}/app_installations/${APP_ID}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${appAccessToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      ).then(r => r.json());
-
-      const defaultValue = appInstallation?.parameters?.defaultValue
-        ? appInstallation.parameters.defaultValue
-        : "Default value";
-
-      // Then we make a request to contentful's CMA to update the Entry with our
-      // default values
-      const res = await fetch(
-        `${BASE_URL}/spaces/${spaceId}/environments/${environmentId}/entries/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            Authorization: `Bearer ${appAccessToken}`,
-            "X-Contentful-Content-Type": contentType.sys.id,
-            "Content-Type": "application/json",
-            "X-Contentful-Version": version,
-          },
-          body: JSON.stringify({ fields: { title: { "en-US": defaultValue } } }),
-        }
+    if (contentType.sys.id !== CONTENT_TYPE_ID) {
+      // If the content type does not match the one we created in setup, we just
+      // ignore the event
+      console.log(
+        `Entry's content type: ${contentType.sys.id} did not match the content type created for the App, ignoring`
       );
-      if (res.status === 200) {
-        console.log(`Set default values for Entry ${id}`);
-        return h.response("success").code(204);
-      } else {
-        throw new Error("failed to set default values" + (await res.text()));
-      }
-    } catch (e) {
-      console.error(e);
-      throw new Error(e);
+      return h.response("success").code(204);
     }
-  },
-});
+
+    // We generate an AppToken based on our RSA keypair
+    const spaceId = space.sys.id;
+    const environmentId = environment.sys.id;
+    const privateKey = fs.readFileSync(PRIVATE_APP_KEY, {encoding: 'utf8'});
+    const appAccessToken = await getManagementToken(
+      privateKey,
+      {
+        appInstallationId: APP_ID,
+        spaceId,
+        environmentId,
+      }
+    );
+
+    // We get the app installation to read out the custom parameters set in the app settings
+    const appInstallation = await fetch(
+      `${BASE_URL}/spaces/${spaceId}/environments/${environmentId}/app_installations/${APP_ID}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${appAccessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((r) => r.json());
+
+    const defaultValue =
+      appInstallation?.parameters?.defaultValue || "Default value";
+
+    // We make a request to contentful's CMA to update the Entry with our defaul values
+    const res = await fetch(
+      `${BASE_URL}/spaces/${spaceId}/environments/${environmentId}/entries/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${appAccessToken}`,
+          "X-Contentful-Content-Type": contentType.sys.id,
+          "Content-Type": "application/json",
+          "X-Contentful-Version": version,
+        },
+        body: JSON.stringify({
+          fields: { title: { "en-US": defaultValue } },
+        }),
+      }
+    );
+    if (res.status === 200) {
+      console.log(`Set default values for Entry ${id}`);
+      return h.response("success").code(204);
+    } else {
+      throw new Error("failed to set default values" + (await res.text()));
+    }
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
+};
