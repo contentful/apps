0d157a26bf9c242357b4bce4cb9807270649710c
Felix Boenke <FLoppix@users.noreply.github.com>
2020-12-22 13:48:42 +0100

    feat: add reset button for image focal point [EXT-2413] (#170)

--- apps/image-focal-point/src/components/FocalPointView/styles.js
+++ apps/image-focal-point/src/components/FocalPointView/styles.js
@@ -4,12 +4,13 @@ import tokens from '@contentful/forma-36-tokens';
 export const styles = {
   container: css({
     display: 'flex',
-    alignItems: 'flex-start'
+    alignItems: 'center'
   }),
   input: css({
     maxWidth: '280px'
   }),
   button: css({
-    marginLeft: tokens.spacingXs
+    marginLeft: tokens.spacingXs,
+    marginRight: tokens.spacingXs
   })
 };
