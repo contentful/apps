3e1f5b8ac8b32b222bb9671d2589d0f221ce5038
Martin Walker <35141192+martin3walker@users.noreply.github.com>
2021-04-21 16:21:38 +0200

    feat: optionally define entry slugs (#219)
    feat: optionally define entry slugs

--- apps/gatsby/CHANGELOG.md
+++ apps/gatsby/CHANGELOG.md
@@ -3,39 +3,6 @@
 All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
-## [1.2.1](https://github.com/contentful/apps/compare/gatsby-preview@1.2.0...gatsby-preview@1.2.1) (2021-04-06)
-
-
-### Bug Fixes
-
-* revert back to original gatsby app ([#217](https://github.com/contentful/apps/issues/217)) ([364a62d](https://github.com/contentful/apps/commit/364a62d77ad69464ae3bd92711144271ad9e488d))
-
-
-
-
-
-# [1.2.0](https://github.com/contentful/apps/compare/gatsby-preview@1.1.3...gatsby-preview@1.2.0) (2021-04-01)
-
-
-### Features
-
-* define_entry_slugs ([e933688](https://github.com/contentful/apps/commit/e933688293fcb7f2a7277dac628b2374f6b21672))
-
-
-
-
-
-## [1.1.3](https://github.com/contentful/apps/compare/gatsby-preview@1.1.2...gatsby-preview@1.1.3) (2021-04-01)
-
-
-### Reverts
-
-* Revert "start on slug fields" ([c69bfba](https://github.com/contentful/apps/commit/c69bfba254b5718ae63c3864480799104ddf8061))
-
-
-
-
-
 ## [1.1.2](https://github.com/contentful/apps/compare/gatsby-preview@1.1.1...gatsby-preview@1.1.2) (2021-02-16)
 
 **Note:** Version bump only for package gatsby-preview
--- apps/gatsby/src/AppConfig/ContentTypesPanel.js
+++ apps/gatsby/src/AppConfig/ContentTypesPanel.js
@@ -1,16 +1,36 @@
 import {
-  CheckboxField,
+  Button,
+  Flex,
   FieldGroup,
   Heading,
+  List,
+  ListItem,
+  Modal,
   Note,
+  Option,
   Paragraph,
   SkeletonBodyText,
   SkeletonContainer,
+  Select,
   TextLink,
+  TextInput,
   Typography,
 } from "@contentful/forma-36-react-components";
 import styles from "../styles";
-import React from "react";
+import React, {useState} from "react";
+
+const sortContentTypesAlphabetically = (contentTypes) => {
+  const sorted = contentTypes.sort((type1, type2) => {
+    if (type1.name < type2.name) {
+      return -1
+    }
+    if (type1.name > type2.name) {
+      return 1
+    }
+    return 0
+  })
+  return sorted
+}
 
 const ContentTypesSkeleton = () => (
   <SkeletonContainer width="100%">
@@ -20,7 +40,7 @@ const ContentTypesSkeleton = () => (
 
 const NoContentTypes = ({ space, environment }) => (
   <Note noteType="warning">
-    There are no content types available in this environment. You can add one{" "}
+    There are no content types available in this environment. You can add one
     <TextLink
       target="_blank"
       href={
@@ -31,15 +51,35 @@ const NoContentTypes = ({ space, environment }) => (
       rel="noopener noreferrer"
     >
       content type
-    </TextLink>{" "}
+    </TextLink>
     and assign it to the app from this screen.
   </Note>
 );
 
-export const ContentTypesList = ({
+const UrlInput = ({urlConstructors, id, onSlugInput, placeholder, disabled}) => {
+  const valueIndex = urlConstructors ? urlConstructors.findIndex(constructor => constructor.id === id) : -1
+  const value = valueIndex !== -1 ? urlConstructors[valueIndex].slug : ""
+  return (
+    <TextInput 
+      disabled={disabled} 
+      id={id} 
+      value={value} 
+      onChange={(event) => onSlugInput(id, event.target.value)} 
+      placeholder={placeholder} 
+      width={"full"}
+    />
+  )
+}
+
+export const ContentTypesSelection = ({
   contentTypes,
   enabledContentTypes,
+  urlConstructors,
+  disableContentType,
+  onSlugInput,
   onContentTypeToggle,
+  selectorTypeToggle,
+  selectorType,
   space,
   environment,
 }) => {
@@ -50,27 +90,161 @@ export const ContentTypesList = ({
   if (0 === contentTypes.length) {
     return <NoContentTypes space={space} environment={environment} />;
   }
+  
+  const fullEnabledTypes = enabledContentTypes.map(enabledType => {
+    const fullType = contentTypes.find(type => type.sys.id === enabledType)
+    return fullType
+  })
 
-  return contentTypes.map(({ sys, name }) => (
-    <div>
-      <CheckboxField
-        key={sys.id}
-        labelIsLight
-        labelText={name}
-        name={name}
-        checked={enabledContentTypes.includes(sys.id)}
-        value={sys.id}
-        onChange={() => onContentTypeToggle(sys.id)}
-        id={sys.id}
-      />
-    </div>
-  ));
+  //Focus value to compare with selection value to determine whether an update in state is necessary
+  const [focusValue, changeFocus] = useState("");
+  //Modal state
+  const [modalState, updateModalState] = useState({
+    open: false,
+    id: ""
+  })
+
+  //Function to reset modal state
+  const modalReset = () => {
+    updateModalState(
+      {
+        open: false,
+        id: ""
+      }
+    )
+  }
+
+  const sortedContentTypes = sortContentTypesAlphabetically(contentTypes)
+
+  return ( 
+    <>
+    {/* Selectors for existing enabled content types */}
+      {fullEnabledTypes.map(({ sys }, index) => (
+      <Flex marginBottom="spacingM">
+        <Flex marginRight = "spacingS" flexDirection="column">
+          <Select
+            key={`enabledSelect-${index}`}
+            value={sys.id} 
+            onFocus={(event) => changeFocus(event.target.value)}
+            onChange={(event)=> {
+              onContentTypeToggle(event.target.value, focusValue)
+              event.target.blur() //Have to blur target for correct focus value in the case dropdown is changed multiple times
+            }}
+            width={"medium"}
+          >
+            {sortedContentTypes.map(({name, sys}) => 
+              <Option key={`option - ${sys.id}`} value={sys.id} label={name}>
+                {name}
+              </Option>
+            )}
+          </Select>
+        </Flex>
+        <Flex fullWidth flexDirection="column" marginRight = "spacingS">
+          <UrlInput 
+            id={sys.id} 
+            onSlugInput={onSlugInput} 
+            urlConstructors={urlConstructors} 
+            placeholder={'(Optional) slug'}
+          />
+        </Flex>
+        <Flex>
+          <TextLink linkType="negative" onClick={() => updateModalState({open: true, id: sys.id})}>
+            Remove
+          </TextLink>
+        </Flex>
+      </Flex>
+      ))}
+
+      {/* Selector triggered by add content type button */}
+      {selectorType && (
+      <Flex marginBottom="spacingM">
+        <Flex marginRight = "spacingS">
+          <Select
+            onChange={(event) => {
+              onContentTypeToggle(event.target.value, null)
+              selectorTypeToggle()
+            }}
+            defaultValue
+            width={"medium"}
+            key={"placeholder"}
+          >
+            <Option  disabled  value>
+              {"Select a content type"}
+            </Option>
+            {sortedContentTypes.map(({name, sys}) => {
+              // Check if the type is already selected so it can be disabled if so
+              const selected = fullEnabledTypes.findIndex(type => type.name === name) !== -1;
+                return (
+                  <Option 
+                    disabled={selected} 
+                    key={sys.id} 
+                    value={sys.id} 
+                    label={`${name}${selected ? " – already selected":""}`}
+                  >
+                    {name}
+                  </Option>
+                )
+            })}
+          </Select>
+        </Flex>
+        <Flex fullWidth marginRight = "spacingS">
+          <UrlInput 
+            disabled 
+            onSlugInput={onSlugInput} 
+            urlConstructors={urlConstructors} 
+            placeholder={'(Optional) slug'}
+          />
+        </Flex>
+        <Flex>
+          <TextLink linkType="negative" onClick={() => selectorTypeToggle()}>Remove</TextLink>
+        </Flex>
+      </Flex>
+      )}
+
+      {/* Button to add a new content type */}
+      <Flex justifyContent="left" marginTop="spacingXl">
+        <Button buttonType="muted" disabled={selectorType} onClick={() => selectorTypeToggle()}>
+          Add content type
+        </Button>
+      </Flex>
+
+
+      {/* Modal to confirm removal of contentType */}
+      <Modal isShown={modalState.open} onClose={() => modalReset()}>
+        {() => (
+          <>
+            <Modal.Header title={"Are you sure?"}></Modal.Header>
+            <Modal.Controls position={"left"}>
+              <Flex marginTop={"spacingXl"}>
+                <Button buttonType="positive" 
+                  onClick={() => {
+                    disableContentType(modalState.id)
+                    modalReset()
+                  }}
+                >
+                  Remove
+                </Button>
+                <Button buttonType="muted" onClick={() => modalReset()}>
+                  Do not remove
+                </Button>
+              </Flex>
+            </Modal.Controls>
+          </>
+        )}
+      </Modal>
+    </>
+  )
 };
 
 const ContentTypesPanel = ({
   contentTypes,
   enabledContentTypes,
+  urlConstructors,
   onContentTypeToggle,
+  disableContentType,
+  selectorTypeToggle,
+  onSlugInput,
+  selectorType,
   space,
   environment,
 }) => (
@@ -78,19 +252,55 @@ const ContentTypesPanel = ({
     <Heading>Content Types</Heading>
     <Paragraph>
       Select content types that will show the Gatsby Cloud functionality in the
-      sidebar.
+      sidebar. 
     </Paragraph>
-    <div className={styles.checks}>
+    <Paragraph>
+    Optionally, define slugs using:
+    </Paragraph>
+    <List>
+      <ListItem key={`instruction-1`}>
+        <Paragraph>
+          Strings (must be contained in quotes, will be the same for every entry): <strong>"resources"</strong>
+        </Paragraph>
+      </ListItem>
+      <ListItem key={`instruction-2`}>
+        <Paragraph>
+          Dot notation to access fields (will return field value for the entry): <strong>parentField.slug</strong>
+        </Paragraph>
+      </ListItem>
+      <ListItem key={`instruction-3`}>
+        <Paragraph>
+          Backslashes (to seperate different parts of the slug): <strong>"resources"/slugPrefix/parentField.slug</strong>
+        </Paragraph>
+      </ListItem>
+      <ListItem key={`instruction-4`}>
+      <Flex marginBottom={"spacingXl"}>
+        <TextLink
+          target="_blank"
+          href={"https://youtu.be/81JqPzLhPzk"}
+          rel="noopener noreferrer"
+        >
+          Watch short explainer video on optional slugs here.
+        </TextLink>
+      </Flex>
+      </ListItem>
+    </List>
+
       <FieldGroup>
-        <ContentTypesList
+        <ContentTypesSelection 
           space={space}
           environment={environment}
           contentTypes={contentTypes}
           enabledContentTypes={enabledContentTypes}
-          onContentTypeToggle={onContentTypeToggle}
+          urlConstructors={urlConstructors}
+          onSlugInput={onSlugInput}
+          onContentTypeToggle={onContentTypeToggle} 
+          disableContentType={disableContentType} 
+          selectorTypeToggle={selectorTypeToggle}
+          selectorType={selectorType}
         />
       </FieldGroup>
-    </div>
+
   </Typography>
 );
 
