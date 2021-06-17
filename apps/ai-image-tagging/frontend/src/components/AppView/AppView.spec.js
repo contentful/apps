cc6a4a0a1f21f4f79e8ded8e5e2fdf18d820de8f
John Whiles <john.whiles@contentful.com>
2021-06-17 11:09:30 +0200

    Revert "feat: [] use react scripts for ai-image-tagging frontend (#286)"
    This reverts commit 5c32a1795e80561132907baa04f355a1f6c0b9df.

--- apps/ai-image-tagging/frontend/src/components/AppView/AppView.spec.js
+++ apps/ai-image-tagging/frontend/src/components/AppView/AppView.spec.js
@@ -1,60 +1,60 @@
 import React from 'react';
-import { render, waitFor, configure, fireEvent } from '@testing-library/react';
+import { cleanup, render, wait, configure, fireEvent } from '@testing-library/react';
 
 import mockProps from '../../test/mockProps';
 import { AppView } from './AppView';
 
 configure({ testIdAttribute: 'data-test-id' });
 
+const props = {
+  sdk: {
+    ...mockProps.sdk,
+    space: {
+      getContentTypes: jest.fn(() => ({
+        items: []
+      }))
+    },
+    app: {
+      setReady: jest.fn(),
+      isInstalled: jest.fn(),
+      onConfigure: jest.fn(),
+      onConfigurationCompleted: jest.fn()
+    }
+  }
+};
 
 describe('AppView', () => {
-  let props
-  beforeEach(() => {
-    props = {
-      sdk: {
-        ...mockProps.sdk,
-        space: {
-          getContentTypes: jest.fn(() => Promise.resolve({ items: [] }))
-        },
-        app: {
-          setReady: jest.fn(),
-          isInstalled: jest.fn(),
-          onConfigure: jest.fn(),
-          onConfigurationCompleted: jest.fn()
-        }
-      }
-    };
-  })
+  afterEach(() => cleanup());
 
   describe('when the app is not installed', () => {
     beforeEach(() => props.sdk.app.isInstalled.mockImplementation(() => Promise.resolve(false)));
 
     it('should render the app view with installation screen', async () => {
       const appView = render(<AppView {...props} />);
-      await waitFor(() => appView.getByText('Configuration'));
+      await wait();
       expect(appView.container).toMatchSnapshot();
     });
 
     it('should render inline validation if the content type id is taken', async () => {
-      props.sdk.space.getContentTypes.mockImplementation(() => Promise.resolve({
+      props.sdk.space.getContentTypes.mockImplementation(() => ({
         items: [{ sys: { id: 'imageWithFocalPoint' } }]
       }));
-      const { getByTestId, getByText } = render(<AppView {...props} />);
-      await waitFor(() => getByText('Configuration'));
+      const { getByTestId } = render(<AppView {...props} />);
+      await wait();
       expect(getByTestId('content-type-name')).toMatchSnapshot();
       expect(getByTestId('content-type-id')).toMatchSnapshot();
     });
 
     it('should update the content type id field if it is pristine and the content type name is changed', async () => {
-      const { getByTestId, getByText } = render(<AppView {...props} />);
-      await waitFor(() => getByText('Configuration'));
+      const { getByTestId } = render(<AppView {...props} />);
+      await wait();
       fireEvent.change(getByTestId('content-type-name-input'), { target: { value: 'Test Name' } });
       expect(getByTestId('content-type-id-input').value).toEqual('testName');
     });
 
     it('should not update the content type id field if it is dirty and the content type name is changed', async () => {
-      const { getByTestId, getByText } = render(<AppView {...props} />);
-      await waitFor(() => getByText('Configuration'));
+      const { getByTestId } = render(<AppView {...props} />);
+      await wait();
       fireEvent.change(getByTestId('content-type-id-input'), { target: { value: 'someTestId' } });
       fireEvent.change(getByTestId('content-type-name-input'), { target: { value: 'Test Name' } });
       expect(getByTestId('content-type-id-input').value).toEqual('someTestId');
@@ -65,7 +65,7 @@ describe('AppView', () => {
     it('should render the app view with configuration screen', async () => {
       props.sdk.app.isInstalled.mockImplementation(() => true);
       const appView = render(<AppView {...props} />);
-      await waitFor(() => appView.getByText('Configuration'));
+      await wait();
       expect(appView.container).toMatchSnapshot();
     });
   });
