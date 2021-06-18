37936229a6ba4df879d26eea836e344dbf1abab4
John Whiles <john.whiles@contentful.com>
2021-06-18 13:18:59 +0200

    feat: [] move tests into src

---
+++ apps/mux/src/player.test.tsx
@@ -0,0 +1,17 @@
+import Adapter from 'enzyme-adapter-react-16';
+import 'isomorphic-fetch';
+
+import * as React from 'react';
+import { mount, configure } from 'enzyme';
+
+import Player from './player';
+
+configure({ adapter: new Adapter() });
+
+test('preview player has a Mux poster image', () => {
+  const wrapper = mount(<Player playbackUrl="https://stream.mux.com" posterUrl="https://image.mux.com/asdf/thumbnail.jpg" />);
+
+  expect(wrapper.find('video').props().poster).toContain(
+    'https://image.mux.com/asdf/thumbnail.jpg'
+  );
+});
