d68b7a6053de62bd0d033258d8dd4275263a207f
Ante Sepic <antesepic@protonmail.com>
2021-07-26 14:18:49 +0200

    feat: Display product variant title in variant picker and preview

---
+++ apps/shopify/src/constants.js
@@ -0,0 +1,4 @@
+// If one creates product with no variants in SHopify, a default variant is
+// returned with this unfortunate title, and there is no other way to check
+// whether the returned variant is the default one or not
+export const DEFAULT_SHOPIFY_VARIANT_TITLE = 'Default Title';
