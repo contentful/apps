8a99e0052374ffc9c08d954c406bfd83608c187d
Ante Sepic <antesepic@protonmail.com>
2021-08-11 15:01:57 +0200

    :recycle: Make collection and product paginations use common base class

---
+++ apps/shopify/src/basePagination.js
@@ -0,0 +1,79 @@
+import differenceBy from 'lodash/differenceBy';
+import { makeShopifyClient } from './skuResolvers';
+
+const PER_PAGE = 20;
+
+export default class BasePagination {
+  hasNextProductPage = false;
+  products = [];
+
+  prevSearch = '';
+
+  constructor({ sdk, dataTransformer, fetchProducts }) {
+    this.sdk = sdk;
+    this.dataTransformer = dataTransformer;
+    this.fetchProducts = fetchProducts;
+  }
+
+  async init() {
+    this.shopifyClient = await makeShopifyClient(this.sdk.parameters.installation);
+  }
+
+  async fetchNext(search) {
+    const searchHasChanged = search !== this.prevSearch;
+
+    if (searchHasChanged) {
+      this.prevSearch = search;
+      this._resetPagination();
+    }
+
+    const products = await this._fetchMoreProducts(search);
+
+    return {
+      pagination: {
+        hasNextPage: this.hasNextProductPage
+      },
+      products: products.map(this.dataTransformer)
+    };
+  }
+
+  /**
+   * This method will either fetch the first batch of products or the next page
+   * in the pagination based on the user search and depending on whether the user
+   * has already requested an initial batch of products or not
+   */
+  async _fetchMoreProducts(search) {
+    const noProductsFetchedYet = this.products.length === 0;
+    const nextProducts = noProductsFetchedYet
+      ? await this._fetchProducts(search)
+      : await this._fetchNextPage(this.products);
+    this.hasNextProductPage = nextProducts.length === PER_PAGE;
+
+    const newProducts = differenceBy(nextProducts, this.products, 'id');
+
+    this.products = [...this.products, ...newProducts];
+
+    return newProducts;
+  }
+
+  /**
+   * This method is used when the user is fetching products for the first time.
+   * i.e. when they just opened the product picker widget or when they just applied
+   * a new search term.
+   */
+  async _fetchProducts(search) {
+    return this.fetchProducts(search, PER_PAGE);
+  }
+
+  /**
+   * This method is used when the user has already fetched a batch of products
+   * and now want to render the next page.
+   */
+  async _fetchNextPage(products) {
+    return (await this.shopifyClient.fetchNextPage(products)).model;
+  }
+
+  _resetPagination() {
+    this.products = [];
+  }
+}
--- apps/shopify/src/collectionPagination.js
+++ apps/shopify/src/collectionPagination.js
@@ -1,90 +1,21 @@
-import differenceBy from 'lodash/differenceBy';
 import { collectionDataTransformer } from './dataTransformer';
-import { makeShopifyClient } from './skuResolvers';
-
-const PER_PAGE = 20;
-
-class Pagination {
-  hasNextProductPage = false;
-  collections = [];
-
-  prevSearch = '';
-
-  constructor(sdk) {
-    this.sdk = sdk;
-  }
-
-  async init() {
-    this.shopifyClient = await makeShopifyClient(this.sdk.parameters.installation);
-  }
-
-  async fetchNext(search) {
-    const searchHasChanged = search !== this.prevSearch;
-
-    if (searchHasChanged) {
-      this.prevSearch = search;
-      this._resetPagination();
-    }
-
-    const collections = await this._fetchMoreCollections(search);
-
-    return {
-      pagination: {
-        hasNextPage: this.hasNextProductPage
-      },
-      products: collections.map(collectionDataTransformer)
-    };
-  }
-
-  /**
-   * This method will either fetch the first batch of collections or the next page
-   * in the pagination based on the user search and depending on whether the user
-   * has already requested an initial batch of collections or not
-   */
-  async _fetchMoreCollections(search) {
-    const noProductsFetchedYet = this.collections.length === 0;
-    const nextCollections = noProductsFetchedYet
-      ? await this._fetchCollections(search)
-      : await this._fetchNextPage(this.collections);
-    this.hasNextProductPage = nextCollections.length === PER_PAGE;
-
-    const newCollections = differenceBy(nextCollections, this.collections, 'id');
-
-    this.collections = [...this.collections, ...newCollections];
-
-    return newCollections;
-  }
-
-  /**
-   * This method is used when the user is fetching collections for the first time.
-   * i.e. when they just opened the product picker widget or when they just applied
-   * a new search term.
-   */
-  async _fetchCollections(search) {
-    const query = { query: search };
-    return await this.shopifyClient.collection.fetchQuery({
-      first: PER_PAGE,
-      sortBy: 'TITLE',
-      reverse: true,
-      ...(search.length && query)
-    });
-  }
-
-  /**
-   * This method is used when the user has already fetched a batch of collections
-   * and now want to render the next page.
-   */
-  async _fetchNextPage(collections) {
-    return (await this.shopifyClient.fetchNextPage(collections)).model;
-  }
-
-  _resetPagination() {
-    this.collections = [];
-  }
-}
+import BasePagination from './basePagination';
 
 const makePagination = async sdk => {
-  const pagination = new Pagination(sdk);
+  const pagination = new BasePagination({
+    sdk,
+    dataTransformer: collectionDataTransformer,
+    fetchProducts: async function(search, PER_PAGE) {
+      const query = { query: search };
+
+      return await this.shopifyClient.collection.fetchQuery({
+        first: PER_PAGE,
+        sortBy: 'TITLE',
+        reverse: true,
+        ...(search.length && query)
+      });
+    }
+  });
   await pagination.init();
   return pagination;
 };
