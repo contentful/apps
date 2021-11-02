a665d6a72e3c05d272e7543fd7b2999ddbdf4c65
John Whiles <john.whiles@contentful.com>
2021-11-02 07:59:02 +0100

    fix: [] preview button incorrectly hidden (#569)
    * fix: [] preview button incorrectly hidden
    
    If the user didn't have a webhook URL we used to still show them the preview button
    This PR makes it so we show the button if
    * there is a preview URL and Content Sync URL
    or
    * There is neither a preview URL or Content Sync URL
    
    * chore: respond to pr feedback
    
    * chore: remove uneeded check

--- apps/gatsby/src/Sidebar.js
+++ apps/gatsby/src/Sidebar.js
@@ -255,32 +255,39 @@ export default class Sidebar extends React.Component {
   };
 
   render = () => {
-    let { contentSyncUrl, authToken, previewUrl, webhookUrl } = this.sdk.parameters.installation;
+    let { contentSyncUrl, authToken, previewUrl, webhookUrl, previewWebhookUrl } =
+      this.sdk.parameters.installation;
     const { slug } = this.state;
 
     previewUrl = this.getPreviewUrl();
 
-    return (
-      <div className="extension">
-        <div className="flexcontainer">
-          {webhookUrl ? (
-            <>
-              <ExtensionUI
-                disabled={this.state.buttonDisabled}
-                disablePreviewOpen={!!contentSyncUrl}
-                contentSlug={!!slug && slug}
-                previewUrl={previewUrl}
-                authToken={authToken}
-                onOpenPreviewButtonClick={!!contentSyncUrl ? this.handleContentSync : () => {}}
-              />
-              {!!this.state.buttonDisabled && <Spinner />}
-            </>
-          ) : (
+    if (contentSyncUrl && !previewWebhookUrl) {
+      return (
+        <div className="extension">
+          <div className="flexcontainer">
             <HelpText style={STATUS_STYLE}>
               <Icon icon="Warning" color="negative" style={ICON_STYLE} /> Please add a Preview
               Webhook URL to your Gatsby App settings.
             </HelpText>
-          )}
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="extension">
+        <div className="flexcontainer">
+          <>
+            <ExtensionUI
+              disabled={this.state.buttonDisabled}
+              disablePreviewOpen={!!contentSyncUrl}
+              contentSlug={!!slug && slug}
+              previewUrl={previewUrl}
+              authToken={authToken}
+              onOpenPreviewButtonClick={!!contentSyncUrl ? this.handleContentSync : () => {}}
+            />
+            {!!this.state.buttonDisabled && <Spinner />}
+          </>
 
           {!!webhookUrl && !contentSyncUrl && this.renderRefreshStatus()}
         </div>
