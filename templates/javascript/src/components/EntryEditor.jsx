c22e8e60cb3fd8debec72e9e172a06638a8770b9
Kado <kadodamball@hotmail.com>
2022-02-08 18:31:31 +0100

    feat(JSTemplate): add js template for CCA

---
+++ templates/javascript/src/components/EntryEditor.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Paragraph } from '@contentful/f36-components';
+
+const Entry = (props) => {
+  return <Paragraph>Hello Entry Editor Component</Paragraph>;
+};
+
+export default Entry;
---
+++ templates/javascript/test/mocks/mockSdk.js
@@ -0,0 +1,10 @@
+const mockSdk = {
+  app: {
+    onConfigure: jest.fn(),
+    getParameters: jest.fn().mockReturnValueOnce({}),
+    setReady: jest.fn(),
+    getCurrentState: jest.fn(),
+  },
+};
+
+export { mockSdk };
