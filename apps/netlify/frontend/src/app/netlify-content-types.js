af3d13dd893f06476cb26c780a55f5e16334ad36
Yuri Mazursky <yuri.mazursky@contentful.com>
2022-02-25 09:51:36 +0100

    chore: fixes of content types selection

--- apps/netlify/frontend/src/app/netlify-content-types.js
+++ apps/netlify/frontend/src/app/netlify-content-types.js
@@ -9,41 +9,39 @@ import {
   Checkbox,
 } from '@contentful/f36-components';
 
-export default class NetlifyContentTypes extends React.Component {
-  static propTypes = {
-    disabled: PropTypes.bool.isRequired,
-    space: PropTypes.string.isRequired,
-    environment: PropTypes.string.isRequired,
-    contentTypes: PropTypes.array.isRequired,
-    enabledContentTypes: PropTypes.array.isRequired,
-    onEnabledContentTypesChange: PropTypes.func.isRequired,
-  };
+const SELECT_ALL_CHECKBOX = 'selectAll';
 
-  onToggleSelect = () => {
-    const { contentTypes, enabledContentTypes } = this.props;
-    const allSelected = contentTypes.length === enabledContentTypes.length;
-    if (allSelected) {
-      this.props.onEnabledContentTypesChange([]);
+const NetlifyContentTypes = ({
+  contentTypes,
+  enabledContentTypes,
+  onEnabledContentTypesChange,
+  disabled,
+  space,
+  environment,
+}) => {
+  const allSelected = contentTypes.length === enabledContentTypes.length;
+
+  const onChange = (e) => {
+    const isChecked = e.target.checked;
+    const id = e.target.value;
+    let changed = [];
+
+    if (id === SELECT_ALL_CHECKBOX) {
+      changed = allSelected ? [] : contentTypes.map(([id]) => id);
     } else {
-      this.props.onEnabledContentTypesChange(contentTypes.map(([id]) => id));
+      changed = isChecked ? enabledContentTypes.concat([id]) : enabledContentTypes.filter((cur) => cur !== id);
     }
+
+    onEnabledContentTypesChange(changed);
   };
 
-  onChange = (checked, id) => {
-    const enabled = this.props.enabledContentTypes;
-    this.props.onEnabledContentTypesChange(
-      checked ? enabled.concat([id]) : enabled.filter((cur) => cur !== id)
-    );
-  };
-
-  render() {
-    const { disabled, contentTypes, enabledContentTypes, space, environment } = this.props;
-
-    const allSelected = contentTypes.length === enabledContentTypes.length;
-
-    let contentToRender;
-    if (contentTypes.length === 0) {
-      contentToRender = (
+  return (
+    <>
+      <Heading>Assign to sidebars</Heading>
+      <Paragraph>
+        Select which content types will show the Netlify functionality in the sidebar.
+      </Paragraph>
+      {contentTypes.length === 0 ? (
         <Note variant="warning">
           There are <strong>no content types</strong> in this environment. You can add a{' '}
           <TextLink
@@ -60,43 +58,38 @@ export default class NetlifyContentTypes extends React.Component {
           </TextLink>{' '}
           and assign it to the app from this screen.
         </Note>
-      );
-    } else {
-      contentToRender = (
-        <div>
-          <Checkbox.Group>
+      ) : (
+        <Checkbox.Group onChange={onChange} value={allSelected ? [SELECT_ALL_CHECKBOX, ...enabledContentTypes] : enabledContentTypes}>
+          <Checkbox
+            id={SELECT_ALL_CHECKBOX}
+            value={SELECT_ALL_CHECKBOX}
+            isDisabled={disabled}
+          >
+            Select all
+          </Checkbox>
+          {contentTypes.map(([id, name]) => (
             <Checkbox
-              id="selectAll"
-              disabled={disabled}
-              onChange={this.onToggleSelect}
-              checked={allSelected}
+              key={id}
+              id={`ct-box-${id}`}
+              value={id}
+              isDisabled={disabled}
             >
-              Select all
+              {name}
             </Checkbox>
-            {contentTypes.map(([id, name]) => (
-              <Checkbox
-                key={id}
-                id={`ct-box-${id}`}
-                onChange={(e) => this.onChange(e.target.checked, id)}
-                disabled={disabled}
-                checked={enabledContentTypes.includes(id)}
-              >
-                {name}
-              </Checkbox>
-            ))}
-          </Checkbox.Group>
-        </div>
-      );
-    }
+          ))}
+        </Checkbox.Group>
+      )}
+    </>
+  );
+};
 
-    return (
-      <>
-        <Heading>Assign to sidebars</Heading>
-        <Paragraph>
-          Select which content types will show the Netlify functionality in the sidebar.
-        </Paragraph>
-        {contentToRender}
-      </>
-    );
-  }
-}
+NetlifyContentTypes.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  space: PropTypes.string.isRequired,
+  environment: PropTypes.string.isRequired,
+  contentTypes: PropTypes.array.isRequired,
+  enabledContentTypes: PropTypes.array.isRequired,
+  onEnabledContentTypesChange: PropTypes.func.isRequired,
+};
+
+export default NetlifyContentTypes;
