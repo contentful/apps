26a587b853f1ed7af84f8ffa39f3f6282741bd3f
Lorenz Weiß <34910067+loweisz@users.noreply.github.com>
2022-03-24 09:43:04 +0100

    refactor: [] hooks to examples in js (#996)

--- examples/javascript/src/locations/Sidebar.jsx
+++ examples/javascript/src/locations/Sidebar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Paragraph } from '@contentful/f36-components';
+import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
 
-const Sidebar = (props) => {
-  return <Paragraph>Hello Sidebar Component</Paragraph>;
+const Sidebar = () => {
+  const sdk = useSDK()
+  /*
+     To use the cma, inject it as follows.
+     If it is not needed, you can remove the next line.
+  */
+  // const cma = useCMA();
+
+  return <Paragraph>Hello Sidebar Component (AppId: {sdk.ids.app})</Paragraph>;
 };
 
 export default Sidebar;
