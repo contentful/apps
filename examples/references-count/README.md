b9bc0476314d05ffc222122f3425d6f1b419e0de
Bohdan Hutsol <bogdan.gutsol@gmail.com>
2022-04-14 16:18:36 +0300

    feat: entry list location advanced example (#1119)
    * feat: entry list location advanced example
    
    * fix: lockfile version
    
    * fix: add cached contentTypes
    
    * chore: add info message

---
+++ examples/references-count/README.md
@@ -0,0 +1,15 @@
+# Entry List location example
+This example shows how to build an advanced app with an `EntryList` location. It counts a number of references for the entries.
+
+## How to use
+
+Run locally:
+
+```bash
+npm install
+npm start
+```
+
+To test it, you can create an app definition in your Contentful organization settings pointing to `http://localhost:3000` and chosing the `Entry list` location from the proposed locations list.
+
+[Read the docs](https://www.contentful.com/developers/docs/extensibility/app-framework/) for more information.
---
+++ examples/references-count/src/index.ts
@@ -0,0 +1,26 @@
+import { createClient } from 'contentful-management';
+
+import { init, locations, EntryListExtensionSDK } from '@contentful/app-sdk';
+import type { KnownSDK } from '@contentful/app-sdk';
+
+import { onEntryListUpdated } from './locations/EntryList';
+
+init((sdk: KnownSDK) => {
+  const cma = createClient(
+    { apiAdapter: sdk.cmaAdapter },
+    {
+      type: 'plain',
+      defaults: {
+        environmentId: sdk.ids.environment,
+        spaceId: sdk.ids.space,
+      },
+    },
+  );
+
+  if (sdk.location.is(locations.LOCATION_ENTRY_LIST)) {
+    const entryListSdk = sdk as EntryListExtensionSDK;
+    entryListSdk.entryList.onEntryListUpdated((props) =>
+      onEntryListUpdated({ entries: props.entries, sdk: entryListSdk, cma }),
+    );
+  }
+});
