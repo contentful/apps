3faa552dfc9e6fbc71a68a7f6f2e3b2ceca05cd3
Blair <blair.liikala@gmail.com>
2022-05-12 16:21:24 -0500

    feat: added audioonly, max_stored_resolution, max_stored_frame_rate, duration
    feat: added custom domain support for dashboard playback.
    fix: audio only assets will not try to get image thumbnail in dashboard.
    fix: version bumps for HLS.js and UpChunk

--- apps/mux/src/player.tsx
+++ apps/mux/src/player.tsx
@@ -8,6 +8,7 @@ interface PlayerProps {
   playbackUrl: string;
   posterUrl: string;
   ratio: string;
+  audioOnly: boolean;
   onReady: () => void;
 }
 
@@ -51,7 +52,7 @@ class Player extends React.Component<PlayerProps, {}> {
   };
 
   getHeight = () => {
-    if (!this.playerRef.current) return;
+    if (!this.playerRef.current || this.props.audioOnly) return;
     return this.playerRef.current.offsetWidth * this.convertRatio();
   };
 
@@ -60,10 +61,10 @@ class Player extends React.Component<PlayerProps, {}> {
       <div className="player">
         <video
           ref={this.playerRef}
-          poster={this.props.posterUrl}
+          poster={!this.props.audioOnly ? this.props.posterUrl : '#'}
           controls
           width="100%"
-          height={this.getHeight() + 'px'}
+          height={this.getHeight() + 'px'} // Is height really needed?
         />
       </div>
     );
