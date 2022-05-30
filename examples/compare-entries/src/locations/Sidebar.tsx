c7f39dd325156001f35660f0f5579ef7b2f424b2
Andi Pätzold <andreas.paetzold@contentful.com>
2022-05-30 15:57:59 +0200

    feat: fetch available environments

--- examples/compare-entries/src/locations/Sidebar.tsx
+++ examples/compare-entries/src/locations/Sidebar.tsx
@@ -1,42 +1,96 @@
 import { SidebarExtensionSDK } from '@contentful/app-sdk';
-import { Button } from '@contentful/f36-components';
-import { useSDK } from '@contentful/react-apps-toolkit';
-import React, { useEffect } from 'react';
+import { Button, Select, Stack } from '@contentful/f36-components';
+import { useCMA, useSDK } from '@contentful/react-apps-toolkit';
+import { EnvironmentProps, SpaceProps } from 'contentful-management';
+import React, { useEffect, useState } from 'react';
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarExtensionSDK>();
+  const cma = useCMA();
+
+  const [environments, setEnvironments] = useState<EnvironmentProps[] | undefined>(undefined);
+  const [spaces, setSpaces] = useState<SpaceProps[] | undefined>(undefined);
+
+  const [environmentId, setEnvironmentId] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     sdk.window.startAutoResizer();
   }, [sdk.window]);
 
-  // TODO: add dropdown with list of space environments where the app is installed
+  useEffect(() => {
+    cma.appDefinition
+      .getInstallationsForOrg({
+        organizationId: sdk.ids.organization,
+        appDefinitionId: sdk.ids.app!,
+      })
+      .then((installations) => {
+        setEnvironments(installations.includes.Environment);
+      });
+  }, [cma.appDefinition, cma.space, sdk]);
+
+  useEffect(() => {
+    if (!environments) {
+      return;
+    }
+
+    const uniqueSpaces = Array.from(new Set([...environments.map((env) => env.sys.space.sys.id)]));
+    Promise.all(
+      uniqueSpaces.map((spaceId) =>
+        cma.space.get({
+          spaceId,
+        })
+      )
+    ).then((spaces) => {
+      setSpaces(spaces);
+    });
+  }, [cma.space, environments]);
+
+  if (!environments || !spaces) {
+    return null;
+  }
 
   return (
-    <Button
-      onClick={() =>
-        sdk.dialogs.openCurrentApp({
-          title: 'Compare Entry',
-          allowHeightOverflow: true,
-          shouldCloseOnOverlayClick: true,
-          shouldCloseOnEscapePress: true,
-          minHeight: 400,
-          width: 'fullWidth',
-          parameters: {
-            leftEntry: {
-              spaceId: sdk.ids.space,
-              environmentId: sdk.ids.environment,
-              entryId: sdk.ids.entry,
+    <Stack flexDirection="column" alignItems="flex-start">
+      <Select onChange={(e) => setEnvironmentId(e.target.value)}>
+        {environments
+          .filter((environment) => environment.sys.id !== sdk.ids.environment)
+          .map((environment) => (
+            <Select.Option key={environment.sys.id} value={environment.sys.id}>
+              {spaces.find((space) => space.sys.id === environment.sys.space.sys.id)?.name!} /{' '}
+              {environment.name}
+            </Select.Option>
+          ))}
+      </Select>
+      <Button
+        onClick={() => {
+          const environment = environments.find((env) => env.sys.id === environmentId)!;
+          const space = spaces.find((s) => s.sys.id === environment.sys.space.sys.id)!;
+
+          sdk.dialogs.openCurrentApp({
+            title: 'Compare Entry',
+            allowHeightOverflow: true,
+            shouldCloseOnOverlayClick: true,
+            shouldCloseOnEscapePress: true,
+            minHeight: 400,
+            width: 'fullWidth',
+            parameters: {
+              leftEntry: {
+                spaceId: sdk.ids.space,
+                environmentId: sdk.ids.environment,
+                entryId: sdk.ids.entry,
+              },
+              rightEntry: {
+                spaceId: space.sys.id,
+                environmentId: environmentId!,
+                entryId: sdk.ids.entry,
+              },
             },
-            rightEntry: {
-              entryId: sdk.ids.entry,
-            },
-          },
-        })
-      }
-    >
-      Open Dialog
-    </Button>
+          });
+        }}
+      >
+        Compare with Environment
+      </Button>
+    </Stack>
   );
 };
 
