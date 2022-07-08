3d34223a4640a10f5a0a262d4fe0f8243bad5712
Andi Pätzold <andreas.paetzold@contentful.com>
2022-07-08 14:57:10 +0200

    fix: use relative paths

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
+  base: '', // relative paths
   plugins: [react()],
   test: {
     environment: 'happy-dom',
