8a84e39af88e3e72f63026bb5d92f017cfba6c4f
vikaskumr <vikas.kumar@contenful.com>
2022-07-29 17:13:59 +0200

    feat: home location in cca

---
+++ examples/typescript/src/locations/Home.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './Home';
+import { render } from '@testing-library/react';
+import { mockCma, mockSdk } from '../../test/mocks';
+
+jest.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: () => mockSdk,
+  useCMA: () => mockCma,
+}));
+
+describe('Home component', () => {
+  it('Component text exists', () => {
+    const { getByText } = render(<Home />);
+
+    expect(getByText('Hello Home Component (AppId: test-app)')).toBeInTheDocument();
+  });
+});
