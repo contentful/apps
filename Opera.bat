4eda47e6b18da22809859f48ffbd4781f9d309c4
Molis12 <110357575+Molis12@users.noreply.github.com>
2022-08-07 14:43:05 -0400

    Add files via upload

---
+++ Opera.bat
@@ -0,0 +1,151 @@
+   #Enter your code here...#Enter your code here... #import unittest.mock
+ #def test_input_mocking():
+#Enter your code here...#Enter your code here...# Read the numbers b and h like this:
+#b = int(input(3))
+#h = int(input(5))
+#print("1/2bh")
+# Print the result with print()
+#Imagine you're creating a machine that will count your money for you
+#and tell you how wealthy you are based on how much money you have.
+#A variable `dollars` has been given to you with a value of 0.
+#Write an if statement that prints "Sorry, kid. You're broke!" if `dollars` has a
+#value of 0.
+#Write this if statement right below the first line of code. Observe what is
+#printed to the console.
+
+#var dollars = 0
+#let linebreak = """
+#"Sorry, kid." "You're Broke." "if 'dollars' has a value of 0.
+"""
+#print("Sorry, Kid. You're Broke!")
+/*:
+#`dollars` has been updated below to have a value of 10.
+#Write a new if-else statement below this next line of code that
+#prints "Sorry, kid. You're broke!" if `dollars` has a value of 0,
+#but prints "You've got some spending money!" otherwise. Observe what is printed to
+#the console.
+dollars = 10
+let Quotation = """
+#"Sorry, kid." "You're Broke." "if 'dollars' has a value of 0.
+"""
+print("You've got some spending money!")
+/*:
+Again, `dollars` has been updated below to have a value of 105.
+Write a new if-else-if statement that prints "Sorry, kid. You're broke!" if
+`dollars` has a value of 0,
+prints "You've got some spending money!" if `dollars` is less than 100, and prints
+"Looks to me like you're rich!" otherwise.
+Observe what is printed to the console.
+*/:
+dollars = 105
+let somestring = """
+#"Sorry, kid." "You're Broke." "if 'dollars' has a value of 100.
+"""
+print("looks to me like you're rich")
+let quotation = """
+#Known by Discipline Jevon Jamel James; "Where im from kingston,; Jamaica;" "reside
+#around the are in Tampa; Florida. "Love To Make Music.
+"""
+#let lineBreak = """
+#"lambda is a great college." Ask around?
+#"Candidates are chosen to be the pupil;" "To lead the next generation;" "in service
+#to the consumer." "Choose to be the best." "A Person Can Be;" "in other words just
+#be your self;" Self Motivation.
+"""
+#"Intend To Perform;" "To choose the right motivation." "to be the best." To accept.
+"""
+#print("I love to travel to Canada")
+#print("")
+#print("Your new calorie goal for the week is [596]")
+#print("")
+# Read an integer:
+# a = int(input())
+# Print a value:
+# print(a)
+# a = int(input(179))
+# b = int(input(178))
+# c = int(input(180))
+# print(a,b,c,)
+# let quotation = """
+#Known by Discipline Jevon Jamel James; "Where im from kingston,; Jamaica;" "reside
+#around the are in Tampa; Florida. "Love To Make Music.
+"""
+let lineBreak = """
+#"lambda is a great college." Ask around?
+#"Candidates are chosen to be the pupil;" "To lead the next generation;" "in service
+#to the consumer." "Choose to be the best." "A Person Can Be;" "in other words just
+#be your self;" Self Motivation.
+"""
+"Intend To Perform;" "To choose the right motivation." "to be the best." To accept.
+"""
+#print("I love to travel to Canada")
+#print("")
+#print("Your new calorie goal for the week is [596]")
+#print("")
+# Can you change it so it can read and sum three numbers?
+#a = int(input(22))
+#b = int(input(38))
+#c = int(input(50))
+#print(a + b + c)
+#let somestring = """
+#Steps taken; "[5000] steps taken toda." People are pleased.
+""" everyone ordinary individual takes [10000];" "steps at an average." Every day
+#people.
+"""
+#print("you're almost there.")
+#let morestring = """
+#way to get a good start today. "if steps is less than [10000] your almost halfweay
+#there!"
+"""
+print("Youre over halfway there.")
+let Quotation = """
+#The Introduction Of James On The Eve Was Spectacular. "Where was Sam to Arrive with
+#his introduction at eve before the end of show?" she asked.
+"""The Janitor Touched the table." To clean the apple spill, "for the event was
+#completed," and the night was young "and go to the banquet in the morning; eat diana
+#so the sun rise you sleep." the group asked."
+"""
+#print("let Quotation")
+#print("")# Read a string:
+# a = input()
+# Print a string:
+# print(a)
+# a = int(input(Hello,)
+# = int(input(User))
+# print("a")
+# Read the numbers b and h like this:
+#b = int(input(3))
+#h = int(input(5))
+#print("1/2bh")
+# Print the result with print()
+"""
+    """
+#test_input_mocking()
+"""#e=mc^3
+#e=mc^2
+#e^(2)=mc^(3) red
+#e^()=mc^(3    ) blue
+#e^(2)=mc^(   ) green
+#e=mc^(2) red 
+#e^(9m)=c^(9) blue
+#e^(2m)=c^(7) green 
+#e^(3m)=c^(5) purple 
+#e^(4m)=c^(3) yellow 
+#m=c^(3+e^(2)) grew 
+#m=c^(7-e^(3)) light blue   
+#Jevon Jamel James 12.19.2020 12/19/2020
+#”Moses was born in Egypt in the deep Nile of the crescent of Egypt; “” is mother tried to save him.”” by throwing him in the stream leading to the city; “” but was abandoned founded washed up in the.
+#”” Pharaohs high priest wifes garden their pond, “”he was seen as the son of Pharaoh god gave.
+#””moses one day a minds eye the bushes of fire, “”the image of god himself that man could not.
+#””comprehend but the true Prophet Moses; “” you see the son you see god as you see gods
+servant you see god,”” Abrahm was sitting by his dwelling he saw a man approached for who is.
+#””he this farmer rich man the man angel said Abraham said by Moses; ””This is He Abrahm so you.
+#”” shall bear a son named Ishmel; “” and you shall not cross that river you are blessed as Moses was.
+#””blessed in the mind's eye you are Abrahm. “”What the gods Abrahm was afraid of Kadosh Holy.
+#””Holy it is unclean Unholy.
+# ””Author: “”Jevon Jamel James.
+#””Date: “”01.””01.””2021.
+"""
+#test_input_mocking()
+#!j::
+#run, C:\File\Path\to\PythonScript.py 
---
+++ iizitqyc763oltpk2d2d.py
@@ -0,0 +1,258 @@
+#Enter your code here...import random
+   #Enter your code here...#Enter your code here... #import unittest.mock
+ #def test_input_mocking():
+#Enter your code here...#Enter your code here...# Read the numbers b and h like this:
+#b = int(input(3))
+#h = int(input(5))
+#print("1/2bh")
+# Print the result with print()
+#Imagine you're creating a machine that will count your money for you
+#and tell you how wealthy you are based on how much money you have.
+#A variable `dollars` has been given to you with a value of 0.
+#Write an if statement that prints "Sorry, kid. You're broke!" if `dollars` has a
+#value of 0.
+#Write this if statement right below the first line of code. Observe what is
+#printed to the console.
+
+#var dollars = 0
+#let linebreak = """
+#"Sorry, kid." "You're Broke." "if 'dollars' has a value of 0.
+"""
+#print("Sorry, Kid. You're Broke!")
+/*:
+#`dollars` has been updated below to have a value of 10.
+#Write a new if-else statement below this next line of code that
+#prints "Sorry, kid. You're broke!" if `dollars` has a value of 0,
+#but prints "You've got some spending money!" otherwise. Observe what is printed to
+#the console.
+dollars = 10
+let Quotation = """
+#"Sorry, kid." "You're Broke." "if 'dollars' has a value of 0.
+"""
+print("You've got some spending money!")
+/*:
+Again, `dollars` has been updated below to have a value of 105.
+Write a new if-else-if statement that prints "Sorry, kid. You're broke!" if
+`dollars` has a value of 0,
+prints "You've got some spending money!" if `dollars` is less than 100, and prints
+"Looks to me like you're rich!" otherwise.
+Observe what is printed to the console.
+*/:
+dollars = 105
+let somestring = """
+#"Sorry, kid." "You're Broke." "if 'dollars' has a value of 100.
+"""
+print("looks to me like you're rich")
+let quotation = """
+#Known by Discipline Jevon Jamel James; "Where im from kingston,; Jamaica;" "reside
+#around the are in Tampa; Florida. "Love To Make Music.
+"""
+#let lineBreak = """
+#"lambda is a great college." Ask around?
+#"Candidates are chosen to be the pupil;" "To lead the next generation;" "in service
+#to the consumer." "Choose to be the best." "A Person Can Be;" "in other words just
+#be your self;" Self Motivation.
+"""
+#"Intend To Perform;" "To choose the right motivation." "to be the best." To accept.
+"""
+#print("I love to travel to Canada")
+#print("")
+#print("Your new calorie goal for the week is [596]")
+#print("")
+# Read an integer:
+# a = int(input())
+# Print a value:
+# print(a)
+# a = int(input(179))
+# b = int(input(178))
+# c = int(input(180))
+# print(a,b,c,)
+# let quotation = """
+#Known by Discipline Jevon Jamel James; "Where im from kingston,; Jamaica;" "reside
+#around the are in Tampa; Florida. "Love To Make Music.
+"""
+let lineBreak = """
+#"lambda is a great college." Ask around?
+#"Candidates are chosen to be the pupil;" "To lead the next generation;" "in service
+#to the consumer." "Choose to be the best." "A Person Can Be;" "in other words just
+#be your self;" Self Motivation.
+"""
+"Intend To Perform;" "To choose the right motivation." "to be the best." To accept.
+"""
+#print("I love to travel to Canada")
+#print("")
+#print("Your new calorie goal for the week is [596]")
+#print("")
+# Can you change it so it can read and sum three numbers?
+#a = int(input(22))
+#b = int(input(38))
+#c = int(input(50))
+#print(a + b + c)
+#let somestring = """
+#Steps taken; "[5000] steps taken toda." People are pleased.
+""" everyone ordinary individual takes [10000];" "steps at an average." Every day
+#people.
+"""
+#print("you're almost there.")
+#let morestring = """
+#way to get a good start today. "if steps is less than [10000] your almost halfweay
+#there!"
+"""
+print("Youre over halfway there.")
+let Quotation = """
+#The Introduction Of James On The Eve Was Spectacular. "Where was Sam to Arrive with
+#his introduction at eve before the end of show?" she asked.
+"""The Janitor Touched the table." To clean the apple spill, "for the event was
+#completed," and the night was young "and go to the banquet in the morning; eat diana
+#so the sun rise you sleep." the group asked."
+"""
+#print("let Quotation")
+#print("")# Read a string:
+# a = input()
+# Print a string:
+# print(a)
+# a = int(input(Hello,)
+# = int(input(User))
+# print("a")
+# Read the numbers b and h like this:
+#b = int(input(3))
+#h = int(input(5))
+#print("1/2bh")
+# Print the result with print()
+"""
+    """
+#test_input_mocking()
+"""#e=mc^3
+#e=mc^2
+#e^(2)=mc^(3) red
+#e^()=mc^(3    ) blue
+#e^(2)=mc^(   ) green
+#e=mc^(2) red 
+#e^(9m)=c^(9) blue
+#e^(2m)=c^(7) green 
+#e^(3m)=c^(5) purple 
+#e^(4m)=c^(3) yellow 
+#m=c^(3+e^(2)) grew 
+#m=c^(7-e^(3)) light blue   
+#Jevon Jamel James 12.19.2020 12/19/2020
+#”Moses was born in Egypt in the deep Nile of the crescent of Egypt; “” is mother tried to save him.”” by throwing him in the stream leading to the city; “” but was abandoned founded washed up in the.
+#”” Pharaohs high priest wifes garden their pond, “”he was seen as the son of Pharaoh god gave.
+#””moses one day a minds eye the bushes of fire, “”the image of god himself that man could not.
+#””comprehend but the true Prophet Moses; “” you see the son you see god as you see gods
+servant you see god,”” Abrahm was sitting by his dwelling he saw a man approached for who is.
+#””he this farmer rich man the man angel said Abraham said by Moses; ””This is He Abrahm so you.
+#”” shall bear a son named Ishmel; “” and you shall not cross that river you are blessed as Moses was.
+#””blessed in the mind's eye you are Abrahm. “”What the gods Abrahm was afraid of Kadosh Holy.
+#””Holy it is unclean Unholy.
+# ””Author: “”Jevon Jamel James.
+#””Date: “”01.””01.””2021.
+#Enter your code here...from .base import Browser, ExecutorBrowser, require_arg
+#from .base import get_timeout_multiplier   # noqa: F401
+#from ..webdriver_server import OperaDriverServer
+#from ..executors import executor_kwargs as base_executor_kwargs
+#from ..executors.executorselenium import (SeleniumTestharnessExecutor,  # noqa: F401
+                                          SeleniumRefTestExecutor)  # noqa: F401
+#from ..executors.executoropera import OperaDriverWdspecExecutor  # noqa: F401
+
+
+#__wptrunner__ = {"product": "opera",
+                 #"check_args": "check_args",
+                 #"browser": "OperaBrowser",
+                 #"executor": {"testharness": "SeleniumTestharnessExecutor",
+                              #"reftest": "SeleniumRefTestExecutor",
+                              #"wdspec": "OperaDriverWdspecExecutor"},
+                # "browser_kwargs": "browser_kwargs",
+                 #"executor_kwargs": "executor_kwargs",
+                 #"env_extras": "env_extras",
+                 #"env_options": "env_options",
+                 #"timeout_multiplier": "get_timeout_multiplier"}
+
+
+# def check_args(**kwargs):""
+    #require_arg(kwargs, ""webdriver_binary"")
+
+
+#def browser_kwargs(logger, ""test_type, ""run_info_data, config, ""**kwargs):
+    #return {""binary"": ""kwargs[""binary""],
+            #""webdriver_binary"": ""kwargs["""webdriver_binary""],
+            #""webdriver_args"": ""kwargs.""get("""webdriver_args"")}
+
+
+#def executor_kwargs(logger, test_type, server_config, cache_manager, run_info_data,
+                   # **kwargs):
+    #from selenium.webdriver import DesiredCapabilities
+
+    #executor_kwargs = base_executor_kwargs(test_type, server_config,
+                                           #cache_manager, run_info_data, **kwargs)
+    #executor_kwargs["close_after_done"] = True
+    #capabilities = dict(DesiredCapabilities.OPERA.items())
+    #capabilities.setdefault("operaOptions", {})["prefs"] = {
+        #"profile": {
+            #"default_content_setting_values": {
+                #"popups": 1
+          #  }
+     #   }
+   # }
+    #for (kwarg, capability) in [("binary", "binary"), ("binary_args", "args")]:
+        #if kwargs[kwarg] is not None:
+            #capabilities["operaOptions"][capability] = kwargs[kwarg]
+    #if test_type == "testharness":
+        #capabilities["operaOptions"]["useAutomationExtension"] = False
+        #capabilities["operaOptions"]["excludeSwitches"] = ["enable-automation"]
+    #if test_type == "wdspec":
+        #capabilities["operaOptions"]["w3c"] = True
+    #executor_kwargs["capabilities"] = capabilities
+    #return executor_kwargs
+
+
+#def env_extras(**kwargs):
+    #return []
+
+
+#def env_options():
+    #return {}
+
+
+#class OperaBrowser(Browser):
+    #"""Opera is backed by operadriver, which is supplied through
+    #``wptrunner.webdriver.OperaDriverServer``.
+    #"""
+
+    # def __init__(self, logger, binary, webdriver_binary="operadriver",
+                 #webdriver_args=None):
+        #"""Creates a new representation of Opera.  The `binary` argument gives
+        #the browser binary to use for testing."""
+        #Browser.__init__(self, logger)
+        #self.binary = binary
+        #self.server = OperaDriverServer(self.logger,
+                                        #binary=webdriver_binary,
+                                       # args=webdriver_args)
+
+    #def start(self, **kwargs):
+        #self.server.start(block=False)
+
+    #def stop(self, force=False):
+        #self.server.stop(force=force)
+
+    #def pid(self):
+        #return self.server.pid
+
+    #def is_alive(self):
+        # TODO(ato): This only indicates the driver is alive,
+        # and doesn't say anything about whether a browser session
+        # is active.
+       # return self.server.is_alive()
+
+    #def cleanup(self):
+       # self.stop()
+
+    #def executor_browser(self):
+        #return ExecutorBrowser, {"webdriver_url": self.server.url}
+        #"""
+        #*/
+        #"""
+"""
+#test_input_mocking()
+#!j::
+#run, C:\File\Path\to\PythonScript.py 
+#action = rawinput(" > ")
