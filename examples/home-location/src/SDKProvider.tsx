29aefd488315327e3878c2a485f5c43d452d9ade
Lorenz Weiß <lorenz.weis@gmail.com>
2022-08-15 15:56:35 +0200

    feat: use custom sdk provider and npm

---
+++ examples/home-location/src/SDKProvider.tsx
@@ -0,0 +1,48 @@
+import React, {
+  FC,
+  PropsWithChildren,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
+import { init, KnownSDK } from "@contentful/app-sdk";
+import { SDKContext } from "@contentful/react-apps-toolkit/dist/SDKProvider";
+
+const DELAY_TIMEOUT = 4 * 1000;
+
+interface SDKProviderProps {
+  loading?: ReactElement;
+}
+
+// This is an override of the SDKProvider from `react-apps-toolkit`,
+// as the package does not include the canary version of the app-sdk
+// which is needed for the app to run in the home location
+
+// TODO: can be removed when released
+export const SDKProvider: FC<PropsWithChildren<SDKProviderProps>> = (props) => {
+  const [sdk, setSDK] = useState<KnownSDK | undefined>();
+
+  useEffect(() => {
+    const timeout = window.setTimeout(() => {
+      console.warn(
+        "Your app is taking longer than expected to initialize. If you think this is an error with Contentful's App SDK, let us know: https://github.com/contentful/ui-extensions-sdk/issues"
+      );
+    }, DELAY_TIMEOUT);
+    init((sdk: KnownSDK) => {
+      setSDK(sdk);
+      window.clearTimeout(timeout);
+    });
+    return () => window.clearTimeout(timeout);
+  }, []);
+
+  if (!sdk) {
+    if (props.loading) {
+      return props.loading;
+    }
+    return null;
+  }
+
+  return (
+    <SDKContext.Provider value={{ sdk }}>{props.children}</SDKContext.Provider>
+  );
+};
--- examples/home-location/src/index.tsx
+++ examples/home-location/src/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { init, KnownSDK } from "@contentful/app-sdk";
+import { GlobalStyles } from "@contentful/f36-components";
 
-import { GlobalStyles } from '@contentful/f36-components';
-import { SDKProvider } from '@contentful/react-apps-toolkit';
+import LocalhostWarning from "./components/LocalhostWarning";
+import App from "./App";
+import { SDKProvider } from "./SDKProvider";
 
-import LocalhostWarning from './components/LocalhostWarning';
-import App from './App';
+const root = document.getElementById("root");
 
-const root = document.getElementById('root');
+window.addEventListener("message", (e) => {
+  console.log(e.data);
+});
 
-if (process.env.NODE_ENV === 'development' && window.self === window.top) {
-  // You can remove this if block before deploying your app
-  render(<LocalhostWarning />, root);
-} else {
-  render(
-    <SDKProvider>
-      <GlobalStyles />
-      <App />
-    </SDKProvider>,
-    root
-  );
-}
+init((sdk: KnownSDK) => {
+  if (process.env.NODE_ENV === "development" && window.self === window.top) {
+    // You can remove this if block before deploying your app
+    render(<LocalhostWarning />, root);
+  } else {
+    render(
+      <SDKProvider>
+        <GlobalStyles />
+        <App />
+      </SDKProvider>,
+      root
+    );
+  }
+});
