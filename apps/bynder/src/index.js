d170fafb21255aaf442e959ae79b7e2dd4ca529b
Gavin Matthews <gavindmatthews@gmail.com>
2022-12-02 13:54:54 -0700

    Update helper text for MultiSelect
    Added additional context around when to use SingleSelectFile and the ramifications

--- apps/bynder/src/index.js
+++ apps/bynder/src/index.js
@@ -244,7 +244,7 @@ setup({
       type: 'List',
       value: 'MultiSelect,SingleSelectFile',
       default: 'MultiSelect',
-      description: '"SingleSelectFile" allows you to select a specific derivative.',
+      description: '"MultiSelect is the best choice for most customers. If you specifically need access to transformations, use SingleSelectFile. Note that you will need to change your frontend to use transform base URLs to load images in SingleSelectFile mode."',
       required: true,
     },
   ],
