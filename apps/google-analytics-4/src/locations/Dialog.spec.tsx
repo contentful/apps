ccbfc3602a3b276dc686cb7d541fa28864c5d6a4
Jim Dalton <jim.dalton@contentful.com>
2022-12-07 07:39:21 -0800

    Create initial project and app README [INTEG-66] (#2149)
    * Add files created by create-contentful-app
    
    * Add basic README for Google Analytics app
    
    * Scope package name correcty
    
    * Remove unneeded compilerOptions
    
    * Use unique package name

---
+++ apps/google-analytics-4/src/locations/Dialog.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Dialog from './Dialog';
+import { render } from '@testing-library/react';
+import { mockCma, mockSdk } from '../../test/mocks';
+
+jest.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: () => mockSdk,
+  useCMA: () => mockCma,
+}));
+
+describe('Dialog component', () => {
+  it('Component text exists', () => {
+    const { getByText } = render(<Dialog />);
+
+    expect(getByText('Hello Dialog Component (AppId: test-app)')).toBeInTheDocument();
+  });
+});
