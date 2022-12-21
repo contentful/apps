ddec41398b3e9aae0f8c01ffac4d4b7496f7fcb4
Jim Dalton <jim.dalton@contentful.com>
2022-12-21 06:39:22 -0800

    GA4: Add form control to allow for input of service key [INTEG-84] (#2195)
    * feature: add config for service key
    
    * show error when saving invalid form
    
    * Add test for happy path installatoin
    
    * add test for preventing save when errors
    
    * Extract service key converstions to separate utils
    
    * A few small cleanups from code review
    
    * Remove another accidental insert
    
    * Restore root app package to original (master) version
    
    * Install user-event at app level package
    
    * Add missing type declarations for lodash
    
    * feat: require valid key file on installation
    
    * Require key file when installing
    
    * Add tests related to form updates
    
    * Clean up a few extraenous things
    
    * Lift state up to parent component, refactor
    
    * Use Array#at to find last mock call

--- apps/google-analytics-4/test/mocks/index.ts
+++ apps/google-analytics-4/test/mocks/index.ts
@@ -1,2 +1,16 @@
 export { mockCma } from './mockCma';
 export { mockSdk } from './mockSdk';
+
+export const validServiceKeyFile = {
+  type: 'service_account',
+  project_id: 'PROJECT_ID',
+  private_key_id: 'PRIVATE_KEY_ID',
+  private_key: '----- PRIVATE_KEY-----',
+  client_email: 'example4@PROJECT_ID.iam.gserviceaccount.com',
+  client_id: 'CLIENT_ID',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/example4%40PROJECT_ID.iam.gserviceaccount.com',
+};
