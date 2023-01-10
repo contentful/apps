11db301111a8acda2b1a21abb844b58acabf309e
Jim Dalton <jim.dalton@contentful.com>
2023-01-10 11:09:27 -0800

    feat: check credentials endpoint on app configuration screen [INTEG-97] (#2299)
    * feat: set up initial serverless configuration
    
    * feat: check credentials endpoint on app configuration screen
    
    * Clean up some unnecessary code
    
    * Remove unneeded msw initialization
    
    * Pin dependencies
    
    * Use null instead of empty string for no error
    
    * Simplify construction of API urls
    
    * Use single api vs. instantating every time

--- apps/google-analytics-4/lambda/src/app.spec.ts
+++ apps/google-analytics-4/lambda/src/app.spec.ts
@@ -4,11 +4,23 @@ import app from './app';
 
 chai.use(chaiHttp);
 
-describe('App', () => {
+describe('app', () => {
   describe('GET /health', () => {
     it('responds with 204', async () => {
       const response = await chai.request(app).get('/health');
       expect(response).to.have.status(204);
+    });
+  });
+
+  describe('GET /api/credentials', () => {
+    it('responds with 200', async () => {
+      const response = await chai.request(app).get('/api/credentials');
+      expect(response).to.have.status(200);
+    });
+
+    it('returns the expected response body', async () => {
+      const response = await chai.request(app).get('/api/credentials');
+      expect(response.body).to.have.property('status', 'active');
     });
   });
 });
