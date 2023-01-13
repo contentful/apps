10fce672f009e75c847a2f3b4a131a9c73a11b9a
Ivo Toby <ivo.toby@contentful.com>
2023-01-13 14:42:22 +0100

    chore: update blog-post-metrics with latest setup from CCA (#2314)

---
+++ examples/blog-post-metrics/src/locations/Sidebar.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import { render } from '@testing-library/react';
+import { mockCma, mockSdk } from '../../test/mocks';
+
+jest.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: () => mockSdk,
+  useCMA: () => mockCma,
+}));
