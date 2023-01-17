54a19bd04b4743631cc3cc0e461e5d0960bc4a26
Jim Dalton <jim.dalton@gmail.com>
2023-01-17 15:43:25 -0800

    Add API class for Google with listAccountSummmaries support
    Add cause to error
    
    feat: add account summaries endpoint

--- apps/google-analytics-4/lambda/.eslintrc.js
+++ apps/google-analytics-4/lambda/.eslintrc.js
@@ -1,9 +1,1 @@
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  env: {
-    node: true,
-  },
-};
+module.exports = {};
