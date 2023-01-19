4e6d6d98f604953ba027ec2bb94eaf736c98476c
Jim Dalton <jim.dalton@gmail.com>
2023-01-19 08:00:44 -0800

    Restore eslint configs that were accidentally removed

--- apps/google-analytics-4/lambda/.eslintrc.js
+++ apps/google-analytics-4/lambda/.eslintrc.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    node: true,
+  },
+};
