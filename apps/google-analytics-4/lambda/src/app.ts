785355affe258baff52c60e94ba10f0fe9480127
Gary Hepting <ghepting@gmail.com>
2023-01-23 14:36:47 -0500

    adds signed request verification
    - adds support for environment specific custom vars/secrets to be loaded in serverless.yml

--- apps/google-analytics-4/lambda/src/app.ts
+++ apps/google-analytics-4/lambda/src/app.ts
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 import { ApiError } from './lib/errors/api-error';
 import { apiErrorHandler, ApiErrorMap, apiErrorMapper } from './lib/errors/middlewares';
@@ -8,7 +9,35 @@ import {
   GoogleApiServerError,
 } from './services/google-api';
 import { ServiceAccountKeyFile } from './types';
+import { verifySignedRequestMiddleware } from './lib/verify-signed-request-middleware';
+import { InvalidSignature } from './lib/errors/invalid-signature';
+import { UnableToVerifyRequest } from './lib/errors/unable-to-verify-request';
+
+dotenv.config(); // TODO: load env vars from .env.local
+
 const app = express();
+
+// allow all OPTIONS requests
+app.options('/*', function (req, res, _next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    [
+      'Authorization',
+      'X-Contentful-Timestamp',
+      'X-Contentful-Signed-Headers',
+      'X-Contentful-Signature',
+      'X-Contentful-User-ID',
+      'X-Contentful-Space-ID',
+      'X-Contentful-Environment-ID',
+      'X-Contentful-App-ID',
+    ].join(', ')
+  );
+  res.send(200);
+});
+// validate signed requests
+app.use(['/api/credentials', '/api/account_summaries'], verifySignedRequestMiddleware);
 
 // Maps an error class name -> a handler function that takes the error of that type as input and returns
 // a correctly configured API error as output.
@@ -16,6 +45,8 @@ const errorClassToApiErrorMap: ApiErrorMap = {
   GoogleApiError: (e: GoogleApiError) => new ApiError(e.details, e.name, e.httpStatus),
   GoogleApiClientError: (e: GoogleApiClientError) => new ApiError(e.details, e.name, e.httpStatus),
   GoogleApiServerError: (e: GoogleApiServerError) => new ApiError(e.details, e.name, e.httpStatus),
+  InvalidSignature: (e: InvalidSignature) => new ApiError(e.message, e.name, 403),
+  UnableToVerifyRequest: (e: UnableToVerifyRequest) => new ApiError(e.message, e.name, 422),
 };
 
 app.get('/health', function (_req, res) {
@@ -23,6 +54,10 @@ app.get('/health', function (_req, res) {
 });
 
 app.get('/api/credentials', (_req, res) => {
+  // throw new AuthorizationError(
+  //   'Request does not have a valid request signature. ' +
+  //     'See: https://www.contentful.com/developers/docs/extensibility/app-framework/request-verification/'
+  // );
   res.status(200).json({ status: 'active' });
 });
 
@@ -33,6 +68,7 @@ app.get('/api/account_summaries', async (_req, res) => {
   res.status(200).json(result);
 });
 
+// catch and handle errors
 app.use(apiErrorMapper(errorClassToApiErrorMap));
 app.use(apiErrorHandler);
 
