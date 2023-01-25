7cde5b4ae6359bc59d70bf916edb9a3d59423e1c
Jim Dalton <jim.dalton@contentful.com>
2023-01-25 10:59:32 -0800

    Feature/google api error states (#2355)
    * feat: add explicit error handing for google API
    
    * WIP: error middleware
    
    * Flesh out error mapper
    
    * Remove api mapper for now

---
+++ apps/google-analytics-4/lambda/src/errors/api-error.ts
@@ -0,0 +1,49 @@
+export class ApiError<T = Record<string, unknown>> extends Error {
+  errorType: string;
+  status: number;
+  details?: T;
+
+  constructor(message: string, errorType: string, status: number, details?: T) {
+    super(message);
+    this.errorType = errorType;
+    this.status = status;
+    this.details = details;
+  }
+
+  toString() {
+    return JSON.stringify({
+      message: this.message,
+      errorType: this.errorType,
+      status: this.status,
+      details: this.details,
+    });
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      errorType: this.errorType,
+      details: this.details || {},
+    };
+  }
+}
+
+export const isApiError = (e: Error): e is ApiError<unknown> => {
+  if (!('errorType' in e)) {
+    return false;
+  }
+
+  if (typeof e.errorType !== 'string') {
+    return false;
+  }
+
+  if (!('status' in e)) {
+    return false;
+  }
+
+  if (typeof e.status !== 'number') {
+    return false;
+  }
+
+  return true;
+};
--- apps/google-analytics-4/lambda/src/services/google-api.ts
+++ apps/google-analytics-4/lambda/src/services/google-api.ts
@@ -1,10 +1,79 @@
 import { AnalyticsAdminServiceClient, protos } from '@google-analytics/admin';
 import { GoogleAuthOptions } from 'google-auth-library';
+import { Status } from 'google-gax';
 import { ServiceAccountKeyFile } from '../types';
 
-export class GoogleApiError extends Error {}
+export class GoogleApiError extends Error {
+  code?: string;
+  details?: string;
+}
 export class GoogleApiServerError extends GoogleApiError {}
 export class GoogleApiClientError extends GoogleApiError {}
+
+const clientErrorStatuses = [
+  Status.INVALID_ARGUMENT,
+  Status.UNAUTHENTICATED,
+  Status.PERMISSION_DENIED,
+  Status.NOT_FOUND,
+  Status.ABORTED,
+  Status.OUT_OF_RANGE,
+  Status.RESOURCE_EXHAUSTED,
+  Status.CANCELLED,
+  Status.FAILED_PRECONDITION,
+];
+
+// our own interface to match the shape of a GoogleError, since
+// the interface Google provides does not actually match their
+// run time error objects!
+interface GoogleError {
+  name: string;
+  message: string;
+  code: Status;
+  details: string;
+}
+
+export function throwGoogleApiError(e: Error): never {
+  let error: GoogleApiError;
+
+  if (!isGoogleError(e)) {
+    throw new GoogleApiError(e.message, { cause: e });
+  }
+
+  if (clientErrorStatuses.includes(e.code)) {
+    error = new GoogleApiClientError(e.message, { cause: e });
+  } else {
+    // we'll assume that any GoogleErrors (with a code) that aren't client errors
+    // are server errors of some kind
+    error = new GoogleApiServerError(e.message, { cause: e });
+  }
+
+  error.code = Status[e.code];
+  error.details = e.details;
+
+  throw error;
+}
+
+// parses the runtime error objects thrown directly by the Google API
+// into a type we can work with predicatably
+function isGoogleError(e: Error): e is GoogleError {
+  if (!('code' in e)) {
+    return false;
+  }
+
+  if (!Object.values(Status).includes(e.code as Status)) {
+    return false;
+  }
+
+  if (!('details' in e)) {
+    return false;
+  }
+
+  if (typeof e.details !== 'string') {
+    return false;
+  }
+
+  return true;
+}
 
 export class GoogleApi {
   readonly serviceAccountKeyFile: ServiceAccountKeyFile;
@@ -36,7 +105,7 @@ export class GoogleApi {
       return await this.analyticsAdminServiceClient.listAccountSummaries();
     } catch (e) {
       if (e instanceof Error) {
-        throw new GoogleApiServerError(e.message, { cause: e });
+        throwGoogleApiError(e);
       }
       throw e;
     }
