95d99e9d9dfc37543929f6a1f445ba79214a1a4a
Jim Dalton <jim.dalton@contentful.com>
2023-01-26 06:19:57 -0800

    Error mapping [INTEG-126] (#2382)
    * WIP: error middleware
    
    * Flesh out error mapper
    
    * Remove api mapper for now
    
    * Use Record type for error details
    
    * Refactor error handling code
    
    * Merge middlewares together
    
    * Rename middleware to middlewares

---
+++ apps/google-analytics-4/lambda/src/lib/errors/middlewares.ts
@@ -0,0 +1,38 @@
+import { ErrorRequestHandler } from 'express';
+import { isApiError, ApiError } from './api-error';
+
+// Very intentional use of any in the type below. we're allowing the error map to define callbacks
+// that can except an argument of _any_ type, for the purposes of the map. The callbacks themselves
+// will still need to be internally type safe! This just allows us to add attributes to this map
+// without getting stuck on a type incompatibility error. (FWIW unknown is considered an
+// incompatbile assignment)
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+export type ApiErrorMap = Record<string, (e: any) => ApiError<Record<string, unknown>>>;
+
+export const apiErrorHandler: ErrorRequestHandler = (error, _request, response, next) => {
+  if (error) {
+    console.log(JSON.stringify(error));
+
+    if (isApiError(error)) {
+      response.status(error.status).send(error.toJSON());
+    } else {
+      response
+        .status(500)
+        .send({ errorType: 'ServerError', message: 'Internal Server Error', details: null });
+    }
+  }
+  next();
+};
+
+export const apiErrorMapper = (errorMap: ApiErrorMap): ErrorRequestHandler => {
+  return (error, _request, _response, next) => {
+    if (error) {
+      const errorName = error.constructor.name as keyof typeof errorMap;
+      if (errorName in errorMap) {
+        return next(errorMap[errorName](error));
+      }
+      return next(error);
+    }
+    next();
+  };
+};
