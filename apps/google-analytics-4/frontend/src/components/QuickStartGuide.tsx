b5555ba1b5328aedf55f037db00a73b1e8aa2709
pkeavenycontentful <pkeaveny@contentful.com>
2023-01-31 10:04:06 -0700

    Update QuickStartGuide.tsx
    Adjustments based on PR feedback

--- apps/google-analytics-4/frontend/src/components/QuickStartGuide.tsx
+++ apps/google-analytics-4/frontend/src/components/QuickStartGuide.tsx
@@ -1,16 +1,10 @@
 import {
     Button,
     Heading,
-    Collapse,
     Flex,
     Note,
     Paragraph,
-    TextLink,
 } from '@contentful/f36-components';
-import {
-    ChevronDownIcon,
-    ChevronRightIcon,
-} from '@contentful/f36-icons';
 import tokens from '@contentful/f36-tokens';
 import { css } from 'emotion';
 import { useState } from 'react';
@@ -23,15 +17,27 @@ const styles = {
         fontSize: tokens.fontSizeL,
         marginBottom: tokens.spacing2Xs,
     }),
+    subSectionHeading: css({
+        fontSize: tokens.fontSizeM,
+        marginBottom: tokens.spacing2Xs,
+    }),
 }
+
+/* this URL comes from the button located at: https://developers.google.com/analytics/devguides/reporting/data/v1/quickstart-client-libraries */
+const quickStartURL = 'https://console.developers.google.com/henhouse/?pb=%5B%22hh-0%22%2C%22analyticsdata.googleapis.com%22%2Cnull%2C%5B%5D%2C%22https%3A%2F%2Fdevelopers.google.com%22%2Cnull%2C%5B%5D%2Cnull%2C%22Enable%20the%20Google%20Analytics%20Data%20API%20v1%22%2C3%2Cnull%2C%5B%5D%2Cfalse%2Cfalse%2Cnull%2Cnull%2Cnull%2Cnull%2Cfalse%2Cnull%2Cfalse%2Cfalse%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22Quickstart%22%2Ctrue%2C%22Quickstart%22%2Cnull%2Cnull%2Cfalse%5D'
 
 const QuickStartGuide = () => {
     const [expanded, setExpanded] = useState(false)
     return (
         <>
-            <Heading as="h2" className={styles.sectionHeading}>Quickstart (Recommended)</Heading>
+            <Heading as="h3" className={styles.subSectionHeading}>Quickstart (Recommended)</Heading>
             <Paragraph>
-                Create a new Cloud Platform project, automatically enable the Google Analytics Data API, and create a service account all at once.
+                The simplest way to get started using Google Analytics with Contentful is to have us set up a Cloud Platform project with a service account on your behalf.
+                This is a requirement from Google in order for us to send data back and forth to your Analytics dashboard.
+            </Paragraph>
+            <Paragraph>
+                The button below will allow us to create a new Cloud Platform project, automatically enable the Google Analytics Data API, and create a service account with read access to your Organization's data all at once.
+
             </Paragraph>
             <Paragraph>
                 This will open a window that will guide you through the process on the Google side and allow you to download a JSON file. Paste the contents of that JSON file below.
@@ -45,26 +51,15 @@ const QuickStartGuide = () => {
                         size="medium"
                         variant="primary"
                         onClick={() => {
-                            window.open("https://console.developers.google.com/henhouse/?pb=%5B%22hh-0%22%2C%22analyticsdata.googleapis.com%22%2Cnull%2C%5B%5D%2C%22https%3A%2F%2Fdevelopers.google.com%22%2Cnull%2C%5B%5D%2Cnull%2C%22Enable%20the%20Google%20Analytics%20Data%20API%20v1%22%2C3%2Cnull%2C%5B%5D%2Cfalse%2Cfalse%2Cnull%2Cnull%2Cnull%2Cnull%2Cfalse%2Cnull%2Cfalse%2Cfalse%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22Quickstart%22%2Ctrue%2C%22Quickstart%22%2Cnull%2Cnull%2Cfalse%5D",
+                            window.open(quickStartURL,
                                 "_blank", "width=600,height=300,top=50,left=50")
                         }} >Authenticate with Google</Button>
 
                 </Flex>
             </Paragraph>
-            <TextLink
-                as="button"
-                variant="primary"
-                icon={expanded ? <ChevronDownIcon /> : <ChevronRightIcon />}
-                alignIcon="start"
-                onClick={() => setExpanded(!expanded)}
-                testId="keyFileFieldExpander"
-            >
-                Manual Setup (for advanced users)
-            </TextLink>
-            <Collapse isExpanded={expanded}>
-                <Paragraph marginTop="spacingM">If you feel confident setting up a service account manually, we recommend following this guide: // TBD</Paragraph>
+            <Heading as="h3" className={styles.subSectionHeading}>Manual Setup (for advanced users)</Heading>
+            <Paragraph marginTop="spacingM">If you feel confident setting up a service account manually, we recommend following this guide: // TBD</Paragraph>
 
-            </Collapse>
 
         </>
     )
