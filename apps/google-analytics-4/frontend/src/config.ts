6b2d702866b578c6d06e145ac35cd52535c26f26
Gary Hepting <ghepting@gmail.com>
2023-02-22 15:21:19 -0500

    adds sentry exception monitoring and tracing to the GA4 frontend
    - adds scope configurations with contentful app sdk and user contexts

--- apps/google-analytics-4/frontend/src/config.ts
+++ apps/google-analytics-4/frontend/src/config.ts
@@ -8,4 +8,7 @@ function getEnvironmentVariable(environmentVariableName: string) {
 
 export const config = {
   backendApiUrl: getEnvironmentVariable('REACT_APP_BACKEND_API_URL'),
+  version: getEnvironmentVariable('REACT_APP_VERSION'),
+  release: getEnvironmentVariable('REACT_APP_RELEASE'),
+  sentryDSN: getEnvironmentVariable('REACT_APP_SENTRY_DSN'),
 };
