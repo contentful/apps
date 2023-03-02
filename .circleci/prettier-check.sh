166fc7e64ab1d493457f55865ef0566b7b35a26b
Jim Dalton <jim.dalton@gmail.com>
2023-03-02 12:13:41 -0800

    Refactor script and remove unneeded orb

--- .circleci/prettier-check.sh
+++ .circleci/prettier-check.sh
@@ -4,16 +4,14 @@
 # purpose of this script is to fail if a change gets passed the commit hooks which enforce prettier
 # rules.
 
-BASE_BRANCH="${GITHUB_PR_BASE_BRANCH:-master}"
-CHANGED_FILES=$(git diff --name-only HEAD $BASE_BRANCH | xargs)
+CHANGED_FILES=$(git diff --name-only HEAD origin/master *.{ts,tsx,js,jsx} | xargs)
 
 echo "Running 'prettier -l' against changed files to check for problems..."
 echo ""
 
 PRETTIER_OUTPUT=$(npx prettier -l $CHANGED_FILES)
-
-test -n $PRETTIER_OUTPUT && echo "Error: Prettier violations found! See list below:"
-echo $PRETTIER_OUTPUT 
+test -n "$PRETTIER_OUTPUT" && echo "prettier violations found:"
+echo "$PRETTIER_OUTPUT"
 
 # exit 1 if output (i.e. prettier violations found), 0 if no output (i.e. no prettier violations found)
-test -n $PRETTIER_OUTPUT
+test -n "$PRETTIER_OUTPUT"
