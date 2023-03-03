2a6a455f4f8364bf9693789c098dc42d51c271bf
Jim Dalton <jim.dalton@gmail.com>
2023-03-03 13:58:00 -0800

    Update prettier to use common merge base instead of origin/master

--- .circleci/prettier-check.sh
+++ .circleci/prettier-check.sh
@@ -6,7 +6,8 @@
 # kill switch if for some reason this script inadvertently blocks the build
 test -n "$SKIP_PRETTIER_CHECK" && exit 0
 
-CHANGED_FILES=$(git diff --name-only HEAD origin/master | egrep --color=no "\.[jt]s(x)?$" | xargs)
+MERGE_BASE=$(git merge-base HEAD origin/master)
+CHANGED_FILES=$(git diff --name-only HEAD $MERGE_BASE | egrep --color=no "\.[jt]s(x)?$" | xargs)
 echo $CHANGED_FILES
 
 test -z "$CHANGED_FILES" && echo "No files for prettier to check. Skipping..." && exit 0
