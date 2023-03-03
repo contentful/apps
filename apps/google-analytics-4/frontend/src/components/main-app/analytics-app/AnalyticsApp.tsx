11d44ef90cd682f67051e05a5aad5b150b525d6e
Lillian Bitner <lillian.bitner@gmail.com>
2023-03-03 13:50:06 -0700

    INTEG-154 add frontend error handling for GA4 app (#2627)
    * Add preliminary error handling for fetching chart data
    
    * Add testing file
    
    * Fix import
    
    * Fix failing test and add test for mocked api error
    
    * Fix imports and loading states
    
    * Fix loading states
    
    * Fix imports and loading states
    
    * Remove unused code
    
    * Typo fix

--- apps/google-analytics-4/frontend/src/components/main-app/analytics-app/AnalyticsApp.tsx
+++ apps/google-analytics-4/frontend/src/components/main-app/analytics-app/AnalyticsApp.tsx
@@ -3,19 +3,24 @@ import ChartFooter from 'components/main-app/ChartFooter/ChartFooter';
 import ChartHeader from 'components/main-app/ChartHeader/ChartHeader';
 import { useEffect, useState } from 'react';
 import { DateRangeType, Row, RunReportResponse } from 'types';
-import DateRange from '../../helpers/dateRange.enum';
-import { config } from '../../config';
-import { styles } from './AnalyticsApp.styles'
-import ChartContent from './ChartContent';
-import { Flex } from '@contentful/f36-components';
+import DateRange from 'helpers/dateRange.enum';
+import { config } from 'config';
+import { styles } from './AnalyticsApp.styles';
+import ChartContent from 'components/main-app/ChartContent';
+import { Flex, Note } from '@contentful/f36-components';
 
+const DEFAULT_ERR_MSG = 'Oops! Cannot display the analytics data at this time.';
+const EMPTY_DATA_MSG = 'There are no page views to show for this range';
 
 const AnalyticsApp = () => {
-  const [runReportResponse, setRunReportResponse] = useState<RunReportResponse>({} as RunReportResponse);
+  const [runReportResponse, setRunReportResponse] = useState<RunReportResponse>(
+    {} as RunReportResponse
+  );
   const [pageViewData, setPageViewData] = useState<RunReportResponse>(runReportResponse);
-  const [dateRange, setDateRange] = useState<DateRangeType>('lastWeek')
+  const [dateRange, setDateRange] = useState<DateRangeType>('lastWeek');
   const [slugValue] = useFieldValue<string>('slug');
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error>();
 
   useAutoResizer();
 
@@ -23,10 +28,16 @@ const AnalyticsApp = () => {
     const baseUrl = config.backendApiUrl;
 
     async function fetchData() {
-      const response = await fetch(`${baseUrl}/sampleData/runReportResponseHasViews.json`);
-      if (response.ok) {
-        setRunReportResponse(await response.json());
+      try {
+        const response = await fetch(`${baseUrl}/sampleData/runReportResponseHasViews.json`);
+        if (response.ok) {
+          setRunReportResponse(await response.json());
+        }
+      } catch (e) {
+        setError(e as Error);
       }
+
+      setLoading(false);
     }
 
     fetchData();
@@ -47,19 +58,16 @@ const AnalyticsApp = () => {
           break;
       }
       return { ...runReportResponse, rows: [...newRows] };
-
-    }
+    };
     if (runReportResponse.rowCount) {
       const newData = sliceByDateRange(dateRange);
       setPageViewData(newData);
-      setLoading(false)
     }
-  }, [dateRange, runReportResponse])
-
+  }, [dateRange, runReportResponse]);
 
   const handleDateRangeChange = (e: DateRangeType) => {
     setDateRange(e);
-  }
+  };
 
   const pageViews =
     runReportResponse.rows &&
@@ -70,25 +78,36 @@ const AnalyticsApp = () => {
 
   const metricName = runReportResponse.metricHeaders && runReportResponse.metricHeaders[0].name;
 
-  const mockData = [1000, -500, 500, 230];
-  const mockLabels = ['January', 'February', 'March', 'April'];
+  const renderChartContent = () => {
+    if (error) {
+      return <Note variant="negative">{error?.message || DEFAULT_ERR_MSG}</Note>;
+    } else if (!pageViewData.rowCount) {
+      return <Note variant="warning">{EMPTY_DATA_MSG}</Note>;
+    }
+
+    return <ChartContent pageViewData={pageViewData} />;
+  };
 
   return (
     <>
-      {loading ?
+      {loading ? (
         <Flex justifyContent="center" alignItems="center" className={styles.wrapper}>
           <div className={styles.loader}></div>
-        </Flex> :
+        </Flex>
+      ) : (
         <>
           <ChartHeader
             metricName={metricName ? metricName : ''}
             metricValue={pageViews || pageViews === 0 ? pageViews.toString() : ''}
             handleChange={handleDateRangeChange}
           />
-          {pageViewData.rowCount ? <ChartContent pageViewData={pageViewData} /> : <>There are no pageviews to show for this range</>}
-          <ChartFooter slugName={slugValue ? slugValue : ''} viewUrl="https://analytics.google.com/" />
+          {renderChartContent()}
+          <ChartFooter
+            slugName={slugValue ? slugValue : ''}
+            viewUrl="https://analytics.google.com/"
+          />
         </>
-      }
+      )}
     </>
   );
 };
