f050cc9afe2b3778ae113f1d219108bc7aa35955
Lillian Bitner <lillian.bitner@gmail.com>
2023-03-03 13:50:06 -0700

    INTEG-154 add frontend error handling for GA4 app (#2627)
    * Add preliminary error handling for fetching chart data
    
    * Add testing file
    
    * Fix import
    
    * Fix failing test and add test for mocked api error
    
    * Fix imports and loading states
    
    * Fix loading states
    
    * Fix imports and loading states
    
    * Remove unused code
    
    * Typo fix

--- apps/google-analytics-4/frontend/src/components/main-app/ChartContent.tsx
+++ apps/google-analytics-4/frontend/src/components/main-app/ChartContent.tsx
@@ -2,30 +2,35 @@ import { Row, RunReportResponse } from '@/types';
 import LineChart from './line-chart/LineChart';
 
 interface Props {
-    pageViewData: RunReportResponse;
+  pageViewData: RunReportResponse;
 }
 
 const ChartContent = (props: Props) => {
-    const { pageViewData } = props;
+  const { pageViewData } = props;
 
-    const parseRowViews = (): number[] => {
-        return pageViewData.rows.map((r: Row) => +r.metricValues[0].value)
-    }
+  const parseRowViews = (): number[] => {
+    return pageViewData.rows.map((r: Row) => +r.metricValues[0].value);
+  };
 
-    const parseRowDates = (): string[] => {
-        return pageViewData.rows.map((r: Row) => {
-            const d = r.dimensionValues[0].value;
-            return new Date(`${d.substring(0, 4)}-${d.substring(4, 6)}-${d.substring(6, 8)}`).toLocaleDateString('en-us', { month: "short", day: "numeric" })
-        })
-    }
+  const parseRowDates = (): string[] => {
+    return pageViewData.rows.map((r: Row) => {
+      const d = r.dimensionValues[0].value;
+      return new Date(
+        `${d.substring(0, 4)}-${d.substring(4, 6)}-${d.substring(6, 8)}`
+      ).toLocaleDateString('en-us', { month: 'short', day: 'numeric' });
+    });
+  };
 
-    return (
-        <>
-            <LineChart dataValues={parseRowViews()} xAxisLabels={parseRowDates()} tooltipMetricLabel={''}
-                accessibilityLabel={''} />
-
-        </>
-    )
-}
+  return (
+    <>
+      <LineChart
+        dataValues={parseRowViews()}
+        xAxisLabels={parseRowDates()}
+        tooltipMetricLabel={''}
+        accessibilityLabel={'Analytics line chart'}
+      />
+    </>
+  );
+};
 
 export default ChartContent;
