06f1da41847e2568c51bf5d1d67726a7e70e469b
Jim Dalton <jim.dalton@gmail.com>
2023-03-08 13:53:47 -0800

    WIP

---
+++ apps/google-analytics-4/lambda/src/config.ts
@@ -0,0 +1,12 @@
+function getEnvironmentVariable(environmentVariableName: string) {
+  const environmentVariableValue = process.env[environmentVariableName];
+  if (environmentVariableValue === undefined) {
+    throw new Error(`Missing environment variable: '${environmentVariableName}'`);
+  }
+  return environmentVariableValue;
+}
+
+export const config = {
+  signingSecret: getEnvironmentVariable('SIGNING_SECRET'),
+  stage: getEnvironmentVariable('STAGE'),
+};
--- apps/google-analytics-4/lambda/src/middlewares/verifySignedRequests.spec.ts
+++ apps/google-analytics-4/lambda/src/middlewares/verifySignedRequests.spec.ts
@@ -7,15 +7,63 @@ import {
   validServiceAccountKeyFileBase64,
   validServiceAccountKeyIdBase64,
 } from '../../test/mocks/googleApi';
+import { createRequest } from 'node-mocks-http';
+import Express from 'express';
+import { verifySignedRequestMiddleware } from './verifySignedRequests';
 
 chai.use(chaiHttp);
+
+const sandbox = sinon.createSandbox();
 
 const serviceAccountKeyHeaders = {
   'X-Contentful-ServiceAccountKeyId': validServiceAccountKeyIdBase64,
   'X-Contentful-ServiceAccountKey': validServiceAccountKeyFileBase64,
 };
 
-describe('verifySignedRequestMiddleware', () => {
+describe.only('verifySignedRequestMiddleware', () => {
+  const next = sinon.stub();
+  let request: Express.Request;
+  const signingSecret = 'x'.repeat(64);
+
+  beforeEach(() => {
+    sandbox.stub(process.env, 'STAGE').value('test');
+    sandbox.stub(process.env, 'SIGNING_SECRET').value(signingSecret);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('when request has valid signature', () => {
+    const method = 'GET' as const;
+    const rawPath = '/undefined/foo/bar';
+    const path = '/foo/bar';
+    const rawHeaders = {
+      'x-contentful-signed-headers':
+        'x-contentful-environment-id,x-contentful-signed-headers,x-contentful-space-id,x-contentful-timestamp,x-contentful-user-id,x-contentful-app-id',
+      'x-contentful-user-id': 'user-id',
+      'x-contentful-space-id': 'space-id',
+      'x-contentful-environment-id': 'environment-id',
+      'x-contentful-app-id': 'app-id',
+    };
+
+    beforeEach(() => {
+      const timestamp = Date.now();
+      const rawRequest = { method, path: rawPath, headers: rawHeaders };
+      console.log('rawRequest', rawRequest);
+      const signatureHeaders = NodeAppsToolkit.signRequest(signingSecret, rawRequest, timestamp);
+      const headers = { ...rawHeaders, ...signatureHeaders };
+      request = createRequest({ method, path, headers });
+    });
+
+    it('does nothing', async () => {
+      verifySignedRequestMiddleware(request, {} as Express.Response, next);
+      sinon.assert.calledWith(next);
+    });
+  });
+});
+
+describe('verifySignedRequestMiddleware2', () => {
   const verifyRequestStub = sinon.stub();
 
   beforeEach(() => {
