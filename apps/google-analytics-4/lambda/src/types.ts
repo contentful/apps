9f58189f66392e8b777e7d3cf673e4399c5716a4
Lillian Bitner <lillian.bitner@contentful.com>
2023-03-10 13:46:16 -0700

    remove input type

--- apps/google-analytics-4/lambda/src/types.ts
+++ apps/google-analytics-4/lambda/src/types.ts
@@ -25,7 +25,6 @@ export interface RunReportParamsType {
   endDate: string;
   dimensions: string | string[];
   metrics: string | string[];
-  sortResponse: boolean;
 }
 
 export type ReportRowType = {
