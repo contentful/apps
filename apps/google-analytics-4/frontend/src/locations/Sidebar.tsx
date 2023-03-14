b990f2419003743411e6fba965baef318ea77b19
Lillian Bitner <lillian.bitner@contentful.com>
2023-03-14 11:58:34 -0700

    Add content type

--- apps/google-analytics-4/frontend/src/locations/Sidebar.tsx
+++ apps/google-analytics-4/frontend/src/locations/Sidebar.tsx
@@ -2,20 +2,23 @@ import AnalyticsApp from 'components/main-app/analytics-app/AnalyticsApp';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import { useApi } from 'hooks/useApi';
 
-const hardCodedSlug = '/en-US';
-
 const Sidebar = () => {
   const sdk = useSDK<any>();
 
-  const { serviceAccountKey, serviceAccountKeyId, savedPropertyId } = sdk.parameters.installation;
+  const { serviceAccountKey, serviceAccountKeyId, savedPropertyId, contentTypes } =
+    sdk.parameters.installation;
+
+  const currentContentType = sdk.contentType.sys.id;
 
   const api = useApi(serviceAccountKeyId, serviceAccountKey);
 
   return (
     <>
-      {savedPropertyId && (
-        <AnalyticsApp api={api} propertyId={savedPropertyId} reportSlug={hardCodedSlug} />
-      )}
+      <AnalyticsApp
+        api={api}
+        propertyId={savedPropertyId}
+        reportSlug={contentTypes[currentContentType].urlPrefix}
+      />
     </>
   );
 };
