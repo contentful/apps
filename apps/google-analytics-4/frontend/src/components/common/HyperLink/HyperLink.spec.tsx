c7a16111acdda00958faf7525eefe7ca70255238
Lillian Bitner <lillian.bitner@contentful.com>
2023-03-27 08:25:16 -0600

    Cleaned-up variables declarations

--- apps/google-analytics-4/frontend/src/components/common/HyperLink/HyperLink.spec.tsx
+++ apps/google-analytics-4/frontend/src/components/common/HyperLink/HyperLink.spec.tsx
@@ -1,24 +1,24 @@
 import HyperLink from './HyperLink';
 import { render, screen } from '@testing-library/react';
 
-const body = 'Have a great day!';
-const substring = 'great';
+const BODY = 'Have a great day!';
+const SUBSTRING = 'great';
 
 const { getByTestId } = screen;
 
 describe('HyperLink component', () => {
   it('can render the TextLink component in place of substring value', () => {
-    render(<HyperLink body={body} substring={substring} />);
+    render(<HyperLink body={BODY} substring={SUBSTRING} />);
 
     const textLink = getByTestId('cf-ui-text-link');
 
     expect(textLink).toBeVisible();
-    expect(textLink.firstChild).toHaveTextContent(substring);
+    expect(textLink.firstChild).toHaveTextContent(SUBSTRING);
   });
 
   it('can handle onClick of link', () => {
     const mockOnClick = jest.fn();
-    render(<HyperLink body={body} substring={substring} onClick={mockOnClick} />);
+    render(<HyperLink body={BODY} substring={SUBSTRING} onClick={mockOnClick} />);
 
     const textLink = getByTestId('cf-ui-text-link');
     textLink.click();
