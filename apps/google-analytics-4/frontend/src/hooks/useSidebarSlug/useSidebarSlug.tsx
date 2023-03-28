48cc339917426ff0da0375612da597ea0408c7bc
Lillian Bitner <lillian.bitner@contentful.com>
2023-03-28 09:45:26 -0600

    Listen for changes in published state of entry

--- apps/google-analytics-4/frontend/src/hooks/useSidebarSlug/useSidebarSlug.tsx
+++ apps/google-analytics-4/frontend/src/hooks/useSidebarSlug/useSidebarSlug.tsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from 'react';
 import { ContentTypeValue } from 'types';
 import { useSDK } from '@contentful/react-apps-toolkit';
-import { SidebarExtensionSDK } from '@contentful/app-sdk';
+import { ContentEntitySys, SidebarExtensionSDK } from '@contentful/app-sdk';
 import { pathJoin } from 'utils/pathJoin';
 import useGetFieldValue from '../useGetFieldValue';
 
 export const useSidebarSlug = (slugFieldInfo: ContentTypeValue) => {
+  const [isPublished, setIsPublished] = useState(false);
   const sdk = useSDK<SidebarExtensionSDK>();
 
   const { slugField, urlPrefix } = slugFieldInfo;
   const slugFieldValue = useGetFieldValue(slugField);
 
+  const checkPublishedStatus = (sys: ContentEntitySys) => {
+    setIsPublished(Boolean(sys.publishedAt));
+  };
+
+  useEffect(() => {
+    sdk.entry.onSysChanged((sys) => checkPublishedStatus(sys));
+  }, [sdk.entry]);
+
   const reportSlug = `/${pathJoin(urlPrefix || '', slugFieldValue || '')}`;
   const slugFieldIsConfigured = Boolean(slugField);
   const contentTypeHasSlugField = slugField in sdk.entry.fields;
-  const isPublished = Boolean(sdk.entry.getSys().publishedAt);
 
   return {
     slugFieldIsConfigured,
