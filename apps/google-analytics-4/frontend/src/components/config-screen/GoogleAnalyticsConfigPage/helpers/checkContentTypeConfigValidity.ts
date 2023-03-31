e621a79abecec132ef33c9e098af893203170c16
Lillian Bitner <lillian.bitner@contentful.com>
2023-03-31 16:46:25 -0600

    Change variable name

--- apps/google-analytics-4/frontend/src/components/config-screen/GoogleAnalyticsConfigPage/helpers/checkContentTypeConfigValidity.ts
+++ apps/google-analytics-4/frontend/src/components/config-screen/GoogleAnalyticsConfigPage/helpers/checkContentTypeConfigValidity.ts
@@ -5,12 +5,11 @@ import { isEmpty } from 'lodash';
 // at least one content type is selected and configured with a slug field.
 
 export const checkContentTypeConfigValidity = (parameters: KeyValueMap) => {
-  if (!parameters.contentTypes || !isEmpty(parameters.contentTypes)) return false;
+  if (isEmpty(parameters.contentTypes)) return false;
 
   const aConfiguredContentType = Object.keys(parameters.contentTypes).find((key) => key);
-  const atLeastOneSlugFieldPresent =
+  const conentTypeAndSlugFieldConfigured =
     aConfiguredContentType && parameters.contentTypes[aConfiguredContentType].slugField;
-  const contentTypesAreConfigured = parameters.contentTypes && atLeastOneSlugFieldPresent;
 
-  return contentTypesAreConfigured;
+  return conentTypeAndSlugFieldConfigured;
 };
