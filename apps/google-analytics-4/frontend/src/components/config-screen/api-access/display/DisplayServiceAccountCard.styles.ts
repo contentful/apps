d755b9cae97f0157a7e75f3c78bfa0b4231ca8f1
Lillian Bitner <lillian.bitner@contentful.com>
2023-04-03 15:59:25 -0600

    Fix styling of status links

---
+++ apps/google-analytics-4/frontend/src/components/config-screen/api-access/display/DisplayServiceAccountCard.styles.ts
@@ -0,0 +1,11 @@
+import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
+
+export const styles = {
+  textLink: css({
+    lineHeight: tokens.lineHeightS,
+  }),
+  statusWrapper: css({
+    alignItems: 'flex-end',
+  }),
+};
