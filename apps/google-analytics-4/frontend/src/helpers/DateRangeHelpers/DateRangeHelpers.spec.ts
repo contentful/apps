c9c94b78fc66968a2923267f2dba84289a25bcbd
Lillian Bitner <lillian.bitner@contentful.com>
2023-04-03 09:17:04 -0600

    Fix broken test

--- apps/google-analytics-4/frontend/src/helpers/DateRangeHelpers/DateRangeHelpers.spec.ts
+++ apps/google-analytics-4/frontend/src/helpers/DateRangeHelpers/DateRangeHelpers.spec.ts
@@ -1,9 +1,11 @@
 import { DateRangeType } from '@/types';
 import getRangeDates, { RANGE_OPTIONS } from './DateRangeHelpers';
 
+const DAY_IN_MS = 1000 * 3600 * 24;
+
 const calculateRangeDates = (range: DateRangeType) => {
   const { start, end } = getRangeDates(range);
-  const getDate = (date: string) => Number(new Date(date).toLocaleDateString().split('/')[1]);
+  const getDate = (date: string) => new Date(date).getTime();
   return {
     startDay: getDate(start),
     endDay: getDate(end),
@@ -14,12 +16,12 @@ describe('handle date range helper', () => {
   it('formats dates correctly for week range', () => {
     const { startDay, endDay } = calculateRangeDates('lastWeek');
 
-    expect(endDay - startDay).toBe(RANGE_OPTIONS.lastWeek.startDaysAgo);
+    expect(Math.abs(endDay - startDay) / DAY_IN_MS).toBe(RANGE_OPTIONS.lastWeek.startDaysAgo);
   });
 
   it('formats dates correctly for day range', () => {
     const { startDay, endDay } = calculateRangeDates('lastDay');
 
-    expect(endDay - startDay).toBe(RANGE_OPTIONS.lastDay.startDaysAgo);
+    expect(Math.abs(endDay - startDay) / DAY_IN_MS).toBe(RANGE_OPTIONS.lastDay.startDaysAgo);
   });
 });
