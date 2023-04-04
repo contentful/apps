c9cb421b0e1808c05ccd84b194c095dc07d38117
pkeavenycontentful <pkeaveny@contentful.com>
2023-04-04 11:43:16 -0600

    Update config.ts

--- apps/sap-commerce-cloud/src/config.ts
+++ apps/sap-commerce-cloud/src/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 function getEnvironmentVariable(environmentVariableName: string) {
   const environmentVariableValue = process.env[environmentVariableName];
   if (environmentVariableValue === undefined) {
