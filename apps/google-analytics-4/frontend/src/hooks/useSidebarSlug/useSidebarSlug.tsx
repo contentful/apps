6cf9c5cd8e6ab1ce4cafcf1d6bad2768ef87f0fe
Lillian Bitner <lillian.bitner@contentful.com>
2023-04-05 08:40:39 -0600

    Make adjustments per PR feedback

--- apps/google-analytics-4/frontend/src/hooks/useSidebarSlug/useSidebarSlug.tsx
+++ apps/google-analytics-4/frontend/src/hooks/useSidebarSlug/useSidebarSlug.tsx
@@ -5,33 +5,29 @@ import { ContentEntitySys, SidebarExtensionSDK } from '@contentful/app-sdk';
 import { pathJoin } from 'utils/pathJoin';
 import useGetFieldValue from '../useGetFieldValue';
 
-export const useSidebarSlug = (slugFieldInfo: ContentTypeValue) => {
-  const [isPublished, setIsPublished] = useState(false);
-  const [debouncedSlugFieldValue, setDebouncedSlugFieldValue] = useState('');
+const SLUG_FIELD_INPUT_DELAY = 500;
 
+export const useSidebarSlug = (slugFieldInfo: ContentTypeValue) => {
   const sdk = useSDK<SidebarExtensionSDK>();
 
   const { slugField, urlPrefix } = slugFieldInfo;
   const slugFieldValue = useGetFieldValue(slugField);
+
+  const [isPublished, setIsPublished] = useState(false);
+  const [debouncedSlugFieldValue, setDebouncedSlugFieldValue] = useState(slugFieldValue);
 
   const handlePublishedStatus = (sys: ContentEntitySys) => {
     setIsPublished(Boolean(sys.publishedAt));
   };
 
   useEffect(() => {
-    const timeout = setTimeout(() => setDebouncedSlugFieldValue(slugFieldValue), 700);
+    const timeout = setTimeout(
+      () => setDebouncedSlugFieldValue(slugFieldValue),
+      SLUG_FIELD_INPUT_DELAY
+    );
 
     return () => clearTimeout(timeout);
   }, [slugFieldValue]);
-
-  useEffect(() => {
-    // This only updates state on component mount.
-    // Any other update when slugFieldValue changes occurs
-    // in the above useEffect
-
-    setDebouncedSlugFieldValue(slugFieldValue);
-    // eslint-disable-next-line
-  }, []);
 
   useEffect(() => {
     sdk.entry.onSysChanged((sys) => handlePublishedStatus(sys));
