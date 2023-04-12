38242231ed05301e29d87aad90fb575d74402e17
Lisa White <lisa.white@contentful.com>
2023-04-12 14:15:11 -0600

    Fix limits for experiments and projects

--- apps/optimizely/src/optimizely-client.js
+++ apps/optimizely/src/optimizely-client.js
@@ -25,39 +25,64 @@ export default class OptimizelyClient {
     this.onReauth();
   };
 
+  _getItemsPerPage = async (item) => {
+    let items = [];
+    const PER_PAGE = 100;
+    const MAX_REQUESTS = 10;
+
+    for (let i = 1; i <= MAX_REQUESTS; i++) {
+      const results = await this.makeRequest(this._getItemsUrl(PER_PAGE, i, item));
+      items = [...items, ...results];
+      if (results.length < PER_PAGE) {
+        break;
+      }
+    }
+
+    if (item === 'experiment') {
+      items = items.filter((experiment) => {
+        return experiment.status !== 'archived';
+      });
+    }
+
+    items = items.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+
+    return items;
+  };
+
+  _getItemsUrl = (perPage, page, item) => {
+    switch (item) {
+      case 'project':
+        return `/projects?per_page=${perPage.toString()}&page=${page.toString()}`;
+      case 'experiment':
+        return `/experiments?project_id=${
+          this.project
+        }&per_page=${perPage.toString()}&page=${page.toString()}`;
+      default:
+        return '';
+    }
+  };
+
   getProjects() {
-    return this.makeRequest('/projects');
+    return this._getItemsPerPage('project');
   }
 
   getExperiment = (experimentId) => {
     return this.makeRequest(`/experiments/${experimentId}`);
   };
 
-  _getExperimentsPerPage = (perPage, page) => {
-    return this.makeRequest(
-      `/experiments?project_id=${
-        this.project
-      }&per_page=${perPage.toString()}&page=${page.toString()}`
-    );
-  };
-
   getExperiments = async () => {
-    let experiments = [];
-    const PER_PAGE = 100;
-    const MAX_REQUESTS = 5;
-
-    for (let i = 1; i <= MAX_REQUESTS; i++) {
-      const results = await this._getExperimentsPerPage(PER_PAGE, i);
-      experiments = [...experiments, ...results];
-      if (results.length < PER_PAGE) {
-        break;
-      }
-    }
-
-    experiments = experiments.filter((experiment) => {
-      return experiment.status !== 'archived';
-    });
-    return experiments;
+    return this._getItemsPerPage('experiment');
   };
 
   getExperimentResults = (experimentId) => {
