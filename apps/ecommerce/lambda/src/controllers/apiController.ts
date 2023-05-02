5272e6afcf25008d97a9fe93bd00fd4a35500f18
Patrick Keaveny <122041643+pkeavenycontentful@users.noreply.github.com>
2023-05-02 10:27:22 -0600

    Added static response JSON to new /product endpoint (#3352)
    Co-authored-by: pkeavenycontentful <pkeaveny@contentful.com>

--- apps/ecommerce/lambda/src/controllers/apiController.ts
+++ apps/ecommerce/lambda/src/controllers/apiController.ts
@@ -4,6 +4,23 @@ const ApiController = {
   ping: (req: Request, res: Response) => {
     return res.send({ status: 'ok', message: 'pong' });
   },
+  product: (req: Request, res: Response) => {
+    console.log(req.params);
+    return res.send({
+      sys: {
+        type: 'ResourceLink',
+        linkType: 'IntegrationResource',
+        urn: 'crn:shopify:::product:products/8191006998814',
+      },
+      name: 'Metallica T Shirt',
+      description: "An awesome men's T-shirt with metallica on it",
+      status: 'Out of stock',
+      image: 'https://cdn.shopify.com/images/foobar.jpg',
+      extras: {
+        sku: 'abc123',
+      },
+    });
+  },
 };
 
 export default ApiController;
