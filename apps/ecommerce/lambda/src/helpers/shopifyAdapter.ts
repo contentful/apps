e0cc892a640ace7056d52536a4f9528f4c7c1dc8
pkeavenycontentful <patrick.keaveny@contentful.com>
2023-05-12 12:58:38 -0600

    Some errors

--- apps/ecommerce/lambda/src/helpers/shopifyAdapter.ts
+++ apps/ecommerce/lambda/src/helpers/shopifyAdapter.ts
@@ -1,14 +1,13 @@
 import { Product } from 'shopify-buy'
-import { HydratedResourceData } from '../types'
+import { ExternalResource, JSONObject } from '../types'
 
-export const convertResponseToResource = (product: Product): HydratedResourceData => {
+export const convertResponseToResource = (product: Product): ExternalResource => {
     return {
         name: product.title,
         description: product.description,
         image: product.images[0].url,
         status: product.availableForSale ? 'Available' : 'Not Available',
         extras: {
-            ...product
         }
     }
 }
