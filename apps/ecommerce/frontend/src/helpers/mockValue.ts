5d5551630fa622505d8e8411c11b277d805e229f
pkeavenycontentful <patrick.keaveny@contentful.com>
2023-05-16 13:50:17 -0600

    Added rand() to mockValue to simulate different scenarios
    - Added real product id from our partner sandbox (public URL)
    - Added rand() to choose between the real product id or a random one to simulate a successful response versus a missing product

--- apps/ecommerce/frontend/src/helpers/mockValue.ts
+++ apps/ecommerce/frontend/src/helpers/mockValue.ts
@@ -1,10 +1,12 @@
 import { FieldAppSDK } from '@contentful/app-sdk';
 import { ExternalResourceLink } from 'types';
 
+const rand = Math.ceil(Math.random() * 3);
+
 const mockValue = (sdk: FieldAppSDK) =>
   ({
     sys: {
-      urn: crypto.randomUUID(),
+      urn: rand >= 1 ? 'gid://shopify/Product/8191006671134' : crypto.randomUUID(),
       type: 'ResourceLink',
       linkType: sdk.parameters.instance.linkType,
     },
