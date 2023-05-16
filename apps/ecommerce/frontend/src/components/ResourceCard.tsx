75219707459957f104bb965f4c2b795be0b64721
George Chios <38636581+rusticpenguin@users.noreply.github.com>
2023-05-16 10:05:14 -0600

    refactors fetch into a hook (#3456)
    * refactors fetch into a hook
    
    * Feat/refactor fields (#3467)
    
    * refactors multi/single html into one component
    
    * refactors logic for getting provider and type
    
    * updates fetch headers

--- apps/ecommerce/frontend/src/components/ResourceCard.tsx
+++ apps/ecommerce/frontend/src/components/ResourceCard.tsx
@@ -1,101 +1,60 @@
 import { Badge, Box, Card, Flex, Grid, Text } from '@contentful/f36-components';
-import { ExternalResource, ResourceCardProps, ExternalResourceLink } from '../types';
-import { useEffect, useState } from 'react';
-import fetchWithSignedRequest from '../helpers/signedRequests';
-import { useCMA, useSDK } from '@contentful/react-apps-toolkit';
-import { FieldAppSDK } from '@contentful/app-sdk';
-import { config } from '../config';
-import MissingResourceCard from './MissingResourceCard';
-import { useDebounce } from 'usehooks-ts';
+import { useState } from 'react';
 import ResourceCardRawData from './ResourceCardRawData';
 import tokens from '@contentful/f36-tokens';
 import ResourceCardMenu from './ResourceCardMenu';
+import MissingResourceCard from './MissingResourceCard';
+import { useDebounce } from 'usehooks-ts';
+import useExternalResource from '../hooks/useExternalResource';
+import { ExternalResource, ExternalResourceLink } from '../types';
+import { RenderDragFn } from '@contentful/field-editor-reference/dist/types';
+import { getResourceProviderAndType } from '../helpers/resourceProviderUtils';
 
-const ResourceCard = (props: ResourceCardProps) => {
-  const sdk = useSDK<FieldAppSDK>();
-  const cma = useCMA();
+interface Props {
+  value: ExternalResourceLink;
+  data?: ExternalResource;
+  index?: number;
+  total?: number;
+  onRemove: Function;
+  dragHandleRender?: RenderDragFn;
+  onMoveToTop?: Function;
+  onMoveToBottom?: Function;
+}
 
-  const [externalResource, setExternalResource] = useState<ExternalResource>({});
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | undefined>(undefined);
-  const [errorStatus, setErrorStatus] = useState<number | undefined>(undefined);
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
-  const debouncedValue = useDebounce(props.value, 300);
+const ResourceCard = (props: Props) => {
+  const { value, index, total, onRemove, dragHandleRender, onMoveToTop, onMoveToBottom } = props;
+
+  const debouncedValue = useDebounce(value, 300);
+  const { externalResource, isLoading, error, errorMessage, errorStatus } =
+    useExternalResource(debouncedValue);
   const [showJson, setShowJson] = useState<boolean>(false);
-
-  const fetchExternalResource = async (resource: ExternalResourceLink) => {
-    const url = new URL(`${config.backendApiUrl}/api/resource`);
-    const [resourceProvider, resourceType] = resource.sys?.linkType?.split(':');
-    const data = await fetchWithSignedRequest(
-      url,
-      sdk.ids.app!,
-      cma,
-      'POST',
-      {
-        'x-contentful-data-provider': resourceProvider.toLowerCase(),
-        'X-Contentful-Data-Provider-BaseURL': sdk.parameters.instance.baseUrl,
-      },
-      resource
-    )
-      .then((res) => {
-        if (res.ok) {
-          setError(undefined);
-          setErrorStatus(undefined);
-          return res.json();
-        }
-
-        setErrorStatus(res.status);
-        throw new Error(res.statusText);
-      })
-      .then((data) => data)
-      .catch((error) => {
-        console.error(errorStatus, error.message);
-        setError(
-          `Error fetching ${resourceType ? resourceType : 'external resource'}${
-            resource.sys?.urn ? ` "${resource.sys.urn}"` : ''
-          }${resourceProvider ? ` from ${resourceProvider}` : ''}.`
-        );
-        setErrorMessage(error.message);
-        return {};
-      });
-
-    return data;
-  };
-
-  useEffect(() => {
-    fetchExternalResource(props.value).then((resource) => {
-      setExternalResource(resource);
-      setIsLoading(false);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedValue]);
 
   if (error) {
     return (
       <MissingResourceCard
-        index={props.index}
-        total={props.total}
-        onRemove={props.onRemove}
-        onMoveToBottom={props.onMoveToBottom}
-        onMoveToTop={props.onMoveToTop}
+        index={index}
+        total={total}
+        onRemove={onRemove}
+        onMoveToBottom={onMoveToBottom}
+        onMoveToTop={onMoveToTop}
         isLoading={isLoading}
         error={error}
-        value={JSON.stringify(props.value)}
+        value={JSON.stringify(value)}
         errorMessage={errorMessage}
         errorStatus={errorStatus}
-        dragHandleRender={props.dragHandleRender}
+        dragHandleRender={dragHandleRender}
       />
     );
   }
 
-  const [resourceProvider, resourceType] = props.value.sys?.linkType?.split(':');
+  const { resourceProvider, resourceType } = getResourceProviderAndType(value);
 
   return (
     <Card
       padding="none"
       isLoading={isLoading}
-      withDragHandle={!!props.dragHandleRender}
-      dragHandleRender={props.dragHandleRender}
+      withDragHandle={!!dragHandleRender}
+      dragHandleRender={dragHandleRender}
       isHovered={false}>
       <Box paddingLeft="spacingM" style={{ borderBottom: `1px solid ${tokens.gray200}` }}>
         <Flex alignItems="center" fullWidth={true} justifyContent="space-between">
@@ -105,13 +64,14 @@ const ResourceCard = (props: ResourceCardProps) => {
           <Flex alignItems="center" isInline={true}>
             {externalResource.status && <Badge variant="featured">{externalResource.status}</Badge>}
             <ResourceCardMenu
-              onRemove={() => props.onRemove(props.index)}
+              onRemove={() => onRemove(index)}
               isDataVisible={showJson}
-              onToggleDataVisible={() => setShowJson((previousState) => !previousState)}
-              index={props.index}
-              total={props.total}
-              onMoveToBottom={() => props.onMoveToBottom?.call(null, props.index)}
-              onMoveToTop={() => props.onMoveToTop?.call(null, props.index)}
+              onShowData={() => setShowJson(true)}
+              onHideData={() => setShowJson(false)}
+              index={index}
+              total={total}
+              onMoveToBottom={() => onMoveToBottom?.call(null, index)}
+              onMoveToTop={() => onMoveToTop?.call(null, index)}
             />
           </Flex>
         </Flex>
@@ -137,7 +97,7 @@ const ResourceCard = (props: ResourceCardProps) => {
           )}
         </Flex>
         <ResourceCardRawData
-          value={JSON.stringify(props.value)}
+          value={JSON.stringify(value)}
           isVisible={showJson}
           onHide={() => setShowJson(false)}
         />
