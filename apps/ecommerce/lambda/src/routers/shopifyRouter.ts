ea2e91be261aabdfd6f768c4f9234e3bec53608e
Lisa White <lisa.white@contentful.com>
2023-05-17 10:35:29 -0600

    Update provider configs to static JSON files

--- apps/ecommerce/lambda/src/routers/shopifyRouter.ts
+++ apps/ecommerce/lambda/src/routers/shopifyRouter.ts
@@ -4,6 +4,5 @@ const app = Router();
 
 app.post('/resource', ShopifyController.resource);
 app.post('/healthcheck', ShopifyController.healthcheck);
-app.get('/config', ShopifyController.config);
 
 export default app;
