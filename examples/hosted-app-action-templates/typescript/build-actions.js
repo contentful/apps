0bc1f56b3bb2925966dddd3dfe565c3795971e0d
vikaskumr <vikas.kumar@contentful.com>
2023-05-17 12:56:16 +0530

    fix: removed try/catch

--- examples/hosted-app-action-templates/typescript/build-actions.js
+++ examples/hosted-app-action-templates/typescript/build-actions.js
@@ -14,29 +14,24 @@ const getEntryPoints = () =>
   }, {});
 
 const main = async (watch = false) => {
-  try {
-    const config = {
-      entryPoints: getEntryPoints(),
-      minify: true,
-      bundle: true,
-      platform: 'node',
-      outdir: 'build/actions',
-      logLevel: 'info',
-      format: 'esm',
-      target: 'es6',
-    };
+  const config = {
+    entryPoints: getEntryPoints(),
+    minify: true,
+    bundle: true,
+    platform: 'node',
+    outdir: 'build/actions',
+    logLevel: 'info',
+    format: 'esm',
+    target: 'es6',
+  };
 
-    if (watch) {
-      const context = await esbuild.context(config);
-      await context.watch();
-    } else {
-      await esbuild.build(config);
-    }
-    console.log('App actions successfully built');
-  } catch (e) {
-    console.log('Error building app actions');
-    throw Error(e);
+  if (watch) {
+    const context = await esbuild.context(config);
+    await context.watch();
+  } else {
+    await esbuild.build(config);
   }
+  console.log('App actions successfully built');
 };
 
 main(argv._.includes('watch')).then(() => console.log('actions built successfully'));
