2733d9f6315d56c5cfee607f1c683dfa440b3079
vikaskumr <vikas.kumar@contentful.com>
2023-05-17 14:40:24 +0530

    fix: proper .ts extension in mainifest file

--- examples/hosted-app-action-templates/typescript/build-actions.js
+++ examples/hosted-app-action-templates/typescript/build-actions.js
@@ -14,24 +14,29 @@ const getEntryPoints = () =>
   }, {});
 
 const main = async (watch = false) => {
-  const config = {
-    entryPoints: getEntryPoints(),
-    minify: true,
-    bundle: true,
-    platform: 'node',
-    outdir: 'build/actions',
-    logLevel: 'info',
-    format: 'esm',
-    target: 'es6',
-  };
+  try {
+    const config = {
+      entryPoints: getEntryPoints(),
+      minify: true,
+      bundle: true,
+      platform: 'node',
+      outdir: 'build/actions',
+      logLevel: 'info',
+      format: 'esm',
+      target: 'es6',
+    };
 
-  if (watch) {
-    const context = await esbuild.context(config);
-    await context.watch();
-  } else {
-    await esbuild.build(config);
+    if (watch) {
+      const context = await esbuild.context(config);
+      await context.watch();
+    } else {
+      await esbuild.build(config);
+    }
+    console.log('App actions successfully built');
+  } catch (e) {
+    console.log('Error building app actions');
+    throw Error(e);
   }
-  console.log('App actions successfully built');
 };
 
 main(argv._.includes('watch')).then(() => console.log('actions built successfully'));
