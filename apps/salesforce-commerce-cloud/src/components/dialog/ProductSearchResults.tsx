cddb1b4b8416b824f4e825cd9ed30386efed1801
John Graham <john.carroll.graham@gmail.com>
2023-05-31 12:02:42 -0400

    Initial commit of Salesforce Commerce Cloud app.

---
+++ apps/salesforce-commerce-cloud/src/components/dialog/ProductSearchResults.tsx
@@ -0,0 +1,157 @@
+import React, { useState } from 'react'
+import { css } from '@emotion/react'
+
+import { Box, Flex, SkeletonContainer, SkeletonImage } from '@contentful/f36-components'
+import { AssetIcon } from '@contentful/f36-icons'
+import tokens from '@contentful/f36-tokens'
+
+import { SearchResultsProps, SearchResultProps, height, headerHeight, stickyHeaderBreakpoint } from './SearchPicker'
+
+const ProductSearchResults = (props:SearchResultsProps) => {
+  
+    const resultsStyles = css`
+      max-height: ${height}px;
+      padding: ${tokens.spacingL} ${tokens.spacingL} 0 ${tokens.spacingL};
+      @media screen and (min-height: ${stickyHeaderBreakpoint}px;) {
+        padding: calc(${tokens.spacingL + headerHeight}px) ${tokens.spacingL} 0 ${tokens.spacingL};
+      }
+    `
+  
+    return (
+      <Box as="section" css={resultsStyles} >
+        <div css={css`
+          display: flex;
+          flex-wrap: wrap;
+          margin-left: -${tokens.spacingS};
+          margin-right: -${tokens.spacingS};
+        `}>
+          {props.searchResults.map((result) => (
+            <ProductSearchResult 
+              key={props.fieldType==='product' ? result.id : `${result.catalogId}:${result.id}`}
+              fieldType={props.fieldType}
+              onItemSelect={props.onItemSelect}
+              selected={props.selectedItems}
+              result={result}
+            />
+          ))}
+        </div>
+      </Box>
+    )
+  }
+
+const ProductSearchResult = (props:SearchResultProps) => {
+    const { result, fieldType, onItemSelect, selected } = props
+    const resultId = fieldType === 'product' ? result.id : `${result.catalogId}:${result.id}`
+    const isSelected = typeof selected === 'string' ? result.id === selected : selected?.includes(resultId)
+  
+    const [imageHasLoaded, setImageHasLoaded] = useState<boolean>(false)
+    const [imageHasErrored, setImageHasErrored] = useState<boolean>(false)
+  
+    const productWrapperStyles = css`
+      flex: 0 0 calc(50% - ${parseFloat(tokens.spacingS) * 2}rem);
+      padding: ${tokens.spacingS};
+      position: relative;
+      @media screen and (min-width: 767px) {
+        flex: 0 0 calc(33.3% - ${parseFloat(tokens.spacingS) * 2}rem)
+      }
+      @media screen and (min-width: 992px) {
+        flex: 0 0 calc(25% - ${parseFloat(tokens.spacingS) * 2}rem)
+      }
+    `
+  
+    const productStyles = css`
+      border: 1px solid ${tokens.gray200};
+      border-radius: 3px;
+      box-shadow: 0px 0px 0px 1px inset rgba(48, 114, 190, 0), 0 1px 3px rgba(0, 0, 0, 0);
+      display: flex;
+      flex-direction: column;
+      padding: ${tokens.spacingS};
+      outline: 0;
+      transition: all ${tokens.transitionDurationDefault} ${tokens.transitionEasingDefault};
+      width: 100%;
+      transform: translateZ(0);
+      will-change: box-shadow, border-color;
+      &:hover {
+        border-color: ${tokens.gray400};
+        cursor: pointer;
+      }
+    `
+  
+    const selectedProductStyles = css`
+      border-color: rgba(48, 114, 190, 1);
+      box-shadow: 0px 0px 0px 1px inset rgba(48, 114, 190, 1), 0 1px 3px rgba(0, 0, 0, 0.08);
+      &:hover {
+        border-color: rgba(48, 114, 190, 1);
+        cursor: pointer;
+      }
+    `
+  
+    const skeletonImageStyles = css`
+      width: 100%;
+      height: 290px;
+    `
+  
+    const resultNameStyles = css`
+      flex: 1 0 auto;
+      font-weight: ${tokens.fontWeightDemiBold};
+      text-transform: capitalize;
+    `
+  
+    const resultIdStyles = css`
+      flex: 0 1 auto;
+      color: ${tokens.gray600};
+      font-size: ${tokens.fontSizeS};
+      margin-top: 0;
+      margin-bottom: 0;
+      max-width: 289px;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    `
+  
+    const productStylesCss = [productStyles]
+    if (isSelected) {
+      productStylesCss.push(selectedProductStyles)
+    }
+  
+    return (
+      <Flex css={productWrapperStyles} >
+        <div
+          data-test-id={`item-preview-${resultId}`}
+          role="switch"
+          aria-checked={isSelected}
+          tabIndex={-1}
+          onClick={() => onItemSelect(resultId)}
+          css={productStylesCss}
+        >
+          {!imageHasLoaded && !imageHasErrored && (
+            <SkeletonContainer css={skeletonImageStyles} >
+              <SkeletonImage width={400} height={290} />
+            </SkeletonContainer>
+          )}
+          {imageHasErrored && (
+            <div><AssetIcon /></div>
+          )}
+          {!imageHasErrored && (
+            <div>
+              <img
+                onLoad={() => setImageHasLoaded(true)}
+                onError={() => setImageHasErrored(true)}
+                style={{display: imageHasLoaded ? 'block' : 'none'}}
+                src={result.image.absUrl}
+                alt={result.image.alt.default}
+                data-test-id="image"
+              />
+            </div>
+          )}
+          <p css={resultNameStyles}>{result.name.default}</p>
+          <p css={resultIdStyles}>ID: {result.id}</p>
+          {fieldType === 'category' && (
+            <p css={resultIdStyles}>Catalog ID: {result.catalogId}</p>
+          )}
+        </div>
+      </Flex>
+    )
+  }
+  
+export default ProductSearchResults
---
+++ apps/salesforce-commerce-cloud/src/components/field/ItemCard.tsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import { css } from '@emotion/react'
+import { useQuery} from '@tanstack/react-query';
+
+// Contentful Imports
+import { Card, Flex, Text, IconButton, Box } from '@contentful/f36-components';
+import { WarningIcon, ExternalLinkIcon, CloseIcon } from '@contentful/f36-icons'
+import tokens from '@contentful/f36-tokens'
+import { useSDK } from '@contentful/react-apps-toolkit'
+import { FieldAppSDK } from '@contentful/app-sdk';
+
+// Local Imports
+import { AppInstallationParameters } from '../../locations/ConfigScreen';
+import SfccClient from '../../utils/Sfcc';
+
+interface ItemProps {
+  id: string;
+  type: 'product' | 'category';
+  onRemove: (id:string) => void;
+}
+
+interface ItemCardProps extends ItemProps {
+  withDragHandle?: boolean;
+  dragHandleRender?: (props: {
+    isDragging?: boolean;
+    drag: React.ReactElement;
+  }) => React.ReactElement;
+}
+
+const ItemCard = (props:ItemCardProps) => {
+  const sdk = useSDK<FieldAppSDK>()
+  const installParameters = sdk.parameters.installation as AppInstallationParameters
+  const client = new SfccClient(installParameters)
+
+  const [itemId, catalogId] = props.id.split(":")
+  const {isLoading, data: itemData } = useQuery({
+    queryKey: ['itemInfo', props.id],
+    queryFn: props.type === 'product' ?
+             () => client.fetchProduct(itemId) : 
+             () => client.fetchCategory(itemId, catalogId)
+  });
+
+  return (
+    <Card
+      padding="none"
+      isLoading={isLoading}
+      withDragHandle={props.withDragHandle}
+      dragHandleRender={props.dragHandleRender}
+    >
+      <Flex flexDirection="column">
+        <Flex alignItems="center" padding="spacingXs">
+          {itemData ? (
+            <ItemPreview id={props.id} type={props.type} itemData={itemData} onRemove={props.onRemove} />
+          ) : (
+            <MissingItem type={props.type} id={props.id} onRemove={props.onRemove} />
+          )}
+        </Flex>
+      </Flex>
+    </Card>
+  )
+}
+
+interface ItemPreviewProps extends ItemProps {
+  itemData: any;
+}
+
+const ItemPreview = (props:ItemPreviewProps) => {
+  const { itemData } = props
+  const useImage = props.type === 'product'
+
+  const categoryStyle = css`
+    margin-left: ${tokens.spacingXs}
+  `
+
+  // const externalLink = props.type === 'product' ? 'https://example.com' : 'https://google.com'
+  const actionsProps:ItemActionsProps = {
+    id: props.id,
+    type: props.type,
+    onRemove: props.onRemove,
+  }
+
+  return (
+    <>
+      { useImage && itemData.image.absUrl && 
+        <img src={itemData.image.absUrl} alt={itemData.image.alt.default} height="75" width="75" />
+      }
+      <Flex flexDirection="column" marginLeft="spacingS">
+         <Text as="div" fontWeight="fontWeightDemiBold" fontSize='fontSizeM'>
+          {itemData.name.default}
+        </Text>
+        <Text as="div" fontWeight="fontWeightMedium" fontSize='fontSizeS' fontColor='gray600'>
+          ID: {itemData.id}
+          {props.type === 'category' && (
+            <>
+              <Box as="span" css={css`margin-left: ${tokens.spacingXs};`}>|</Box>
+              <Box as="span" css={categoryStyle}>Catalog: {itemData.name?.default ? itemData.name.default : itemData.catalogId}</Box>
+            </>
+          )}
+        </Text>
+        <ItemActions {...actionsProps} />
+       </Flex>
+    </>
+  );
+}
+
+interface ItemActionsProps extends ItemProps {
+  externalLink?: string;
+}
+
+const ItemActions = (props:ItemActionsProps) => {
+  const wrapperStyles = css`
+    position: absolute;
+    top: ${tokens.spacingXs};
+    right: ${tokens.spacingXs};
+  `
+
+  const linkStyle = css`
+    display: inline-block;
+    margin-right: ${tokens.spacingXs};
+    &:hover {
+      svg {
+        fill: ${tokens.colorBlack}
+      }
+    }
+  `
+
+  const externalIconStyle = css`
+    transition: fill ${tokens.transitionDurationDefault} ${tokens.transitionEasingDefault}
+  `
+
+  return (
+    <div 
+      css={wrapperStyles}
+    >
+      {props.externalLink && (
+        <a 
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://example.com"
+          css={linkStyle}  
+        >
+          <ExternalLinkIcon 
+            color="muted"
+            css={externalIconStyle}
+          />
+        </a>        
+      )}
+      <IconButton
+        aria-label="Delete"
+        icon={<CloseIcon />}
+        variant="transparent"
+        onClick={() => props.onRemove(props.id)}
+      />
+    </div>
+  )
+}
+
+const MissingItem = (props:ItemProps) => {
+  return (
+    <>
+      <WarningIcon variant="warning" />
+      <Text marginLeft="spacingS">{props.type.charAt(0).toUpperCase() + props.type.slice(1)} not found: {props.id}</Text>
+      <ItemActions id={props.id} type={props.type} onRemove={props.onRemove} />
+    </>
+  )
+}
+
+
+export default ItemCard;
