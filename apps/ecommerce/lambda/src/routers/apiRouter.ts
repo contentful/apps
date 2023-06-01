f7ec74a8ee3dcf11192c681c2f09e8adf821e391
Gary Hepting <ghepting@gmail.com>
2023-06-01 14:08:41 -0400

    adds proxy API endpoint for data provider config.json

--- apps/ecommerce/lambda/src/routers/apiRouter.ts
+++ apps/ecommerce/lambda/src/routers/apiRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ApiController } from '../controllers';
 const app = Router();
 
+app.get('/config.json', ApiController.config);
 app.get('/credentials', ApiController.checkCredentials);
 app.post('/resource', ApiController.resource);
 
