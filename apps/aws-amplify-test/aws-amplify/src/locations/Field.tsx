276c6e408f8cb8123497eb24b23f9415116cbfe3
pkeavenycontentful <patrick.keaveny@contentful.com>
2023-06-06 11:33:31 -0600

    Added test amplify definition

---
+++ apps/aws-amplify-test/aws-amplify/src/locations/Field.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Paragraph } from '@contentful/f36-components';
+import { FieldAppSDK } from '@contentful/app-sdk';
+import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+
+const Field = () => {
+  const sdk = useSDK<FieldAppSDK>();
+  /*
+     To use the cma, inject it as follows.
+     If it is not needed, you can remove the next line.
+  */
+  // const cma = useCMA();
+  // If you only want to extend Contentful's default editing experience
+  // reuse Contentful's editor components
+  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
+  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
+};
+
+export default Field;
---
+++ apps/aws-amplify-test/aws-amplify/src/locations/Page.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Page from './Page';
+import { render } from '@testing-library/react';
+import { mockCma, mockSdk } from '../../test/mocks';
+
+jest.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: () => mockSdk,
+  useCMA: () => mockCma,
+}));
+
+describe('Page component', () => {
+  it('Component text exists', () => {
+    const { getByText } = render(<Page />);
+
+    expect(getByText('Hello Page Component (AppId: test-app)')).toBeInTheDocument();
+  });
+});
