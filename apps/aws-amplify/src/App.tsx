27432c2bb927cf17c027e16e7ac9806c3e021a07
Matt Gordon <matt.gordon@contentful.com>
2023-06-06 13:24:08 -0600

    [WIP] Initial amplify shell

---
+++ apps/aws-amplify/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useMemo } from 'react';
+import { locations } from '@contentful/app-sdk';
+import ConfigScreen from './locations/ConfigScreen';
+import Sidebar from './locations/Sidebar';
+
+import { useSDK } from '@contentful/react-apps-toolkit';
+
+const ComponentLocationSettings = {
+  [locations.LOCATION_APP_CONFIG]: ConfigScreen,
+  [locations.LOCATION_ENTRY_SIDEBAR]: Sidebar,
+};
+
+const App = () => {
+  const sdk = useSDK();
+
+  const Component = useMemo(() => {
+    for (const [location, component] of Object.entries(ComponentLocationSettings)) {
+      if (sdk.location.is(location)) {
+        return component;
+      }
+    }
+  }, [sdk.location]);
+
+  return Component ? <Component /> : null;
+};
+
+export default App;
