736dc15a1e3e5083585da8719e6543b4fdf54659
Lillian Bitner <lillian.bitner@contentful.com>
2023-06-09 11:32:59 -0600

    Fix another import error

--- apps/ecommerce/frontend/src/components/Config/ConfigBody/ConfigBody.spec.tsx
+++ apps/ecommerce/frontend/src/components/Config/ConfigBody/ConfigBody.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import ConfigBody from './ConfigBody/ConfigBody';
+import ConfigBody from './ConfigBody';
 
 const mockProviderConfig = {
   name: 'Ecom Provider',
