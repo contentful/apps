05d2d4256912a66471accffaf5d42070da414dea
pkeavenycontentful <patrick.keaveny@contentful.com>
2023-06-09 11:12:39 -0600

    Linting

--- apps/aws-amplify/src/locations/Sidebar.tsx
+++ apps/aws-amplify/src/locations/Sidebar.tsx
@@ -14,6 +14,7 @@ import { useState } from 'react';
     var open = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function () {
       var args: any = slice.call(arguments);
+      // eslint-disable-next-line no-useless-escape
       var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
       if (
         targetOrigin &&
