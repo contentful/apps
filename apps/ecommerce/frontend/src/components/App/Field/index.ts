7f1554f9ef8080c0d3fca91c078a503e984295b3
Lillian Bitner <lillian.bitner@contentful.com>
2023-06-09 11:27:20 -0600

    Fix name issue of export

--- apps/ecommerce/frontend/src/components/App/Field/index.ts
+++ apps/ecommerce/frontend/src/components/App/Field/index.ts
@@ -1,3 +1,3 @@
 export { default as SingleResource } from './SingleResource/SingleResource';
-export { default as MultipleResource } from './MultipleResources/MultipleResources';
+export { default as MultipleResources } from './MultipleResources/MultipleResources';
 export { FieldFallback } from './ResourceField';
