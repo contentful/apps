6532683c8fe84f7a4be958804ce5b9727eae5674
Lillian Bitner <lillian.bitner@contentful.com>
2023-06-12 14:11:13 -0600

    Add test for ResourceCardMenu

---
+++ apps/ecommerce/frontend/src/components/App/Field/ResourceCard/ResourceCardMenu/ResourceCardMenu.spec.tsx
@@ -0,0 +1,42 @@
+import ResourceCardMenu from './ResourceCardMenu';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+const { getByTestId, findByTestId, queryByTestId } = screen;
+
+jest.mock('hooks/field/useResourceMenuItems', () => () => ({
+  menuItems: [<div key="mock-menu-item">Mock Menu Item</div>],
+})) > jest.mock('hooks/field/useResourceMenuItems');
+
+const props = {
+  onRemove: jest.fn(),
+  isDataVisible: true,
+  onShowData: jest.fn(),
+  onHideData: jest.fn(),
+};
+
+describe('ResourceCardMenu component', () => {
+  it('mounts', () => {
+    render(<ResourceCardMenu {...props} />);
+
+    const iconButton = getByTestId('cf-ui-icon-button');
+    const icon = getByTestId('cf-ui-icon');
+
+    expect(iconButton).toBeVisible();
+    expect(icon).toBeVisible();
+  });
+
+  it('opens and closes menu on click', async () => {
+    render(<ResourceCardMenu {...props} />);
+
+    const iconButton = getByTestId('cf-ui-icon-button');
+    fireEvent.click(iconButton);
+    const menuList = await findByTestId('cf-ui-menu-list');
+
+    expect(menuList).toBeVisible();
+
+    fireEvent.click(iconButton);
+
+    const menu = queryByTestId('cf-ui-menu');
+    expect(menu).toBeFalsy();
+  });
+});
