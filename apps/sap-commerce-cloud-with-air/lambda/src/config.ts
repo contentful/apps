178249dc76dc2d385743d31ceeb24b1751ab7c62
ryunsong-contentful <ryun.song@contentful.com>
2023-06-26 16:40:37 -0600

    Adds signed request verification logic

---
+++ apps/sap-commerce-cloud-with-air/lambda/src/config.ts
@@ -0,0 +1,23 @@
+type EnvironmentVariable = string | null | number | undefined;
+
+function getEnvironmentVariable(
+  environmentVariableName: string,
+  fallback?: EnvironmentVariable
+): string {
+  const environmentVariableValue = process.env[environmentVariableName];
+  if (!environmentVariableValue) {
+    if (fallback) return fallback.toString();
+    throw new Error(`Missing environment variable: '${environmentVariableName}'`);
+  }
+  return environmentVariableValue;
+}
+
+export const config = {
+  stage: getEnvironmentVariable('STAGE'),
+  awsRegion: getEnvironmentVariable('AWS_REGION'),
+  sentryDSN: getEnvironmentVariable('SENTRY_DSN'),
+  environment: getEnvironmentVariable('NODE_ENV'),
+  release: getEnvironmentVariable('CIRCLE_SHA1', 'no-release-hash-set'),
+  domainName: getEnvironmentVariable('DOMAIN_NAME'),
+  appConfigs: JSON.parse(getEnvironmentVariable('APP_CONFIGS')),
+};
