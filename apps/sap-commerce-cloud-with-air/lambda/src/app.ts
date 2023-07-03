22d106376b25a969b02bc890b3e04311f93f4fbb
Matt Gordon <matt.gordon@contentful.com>
2023-07-03 12:31:23 -0600

    Revert configs and lambda setup, whitelist cors for /sap routes

--- apps/sap-commerce-cloud-with-air/lambda/src/app.ts
+++ apps/sap-commerce-cloud-with-air/lambda/src/app.ts
@@ -9,9 +9,7 @@ import RateLimit from 'express-rate-limit';
 
 const app = express();
 app.use(express.json());
-
-app.use(cors(corsConfig)); // enable CORS on /api/* routes
-const apiRouteConstraint = ['/*'];
+const apiRouteConstraint = ['/sap/*'];
 
 // Initialize Sentry as early as possible
 Sentry.init({
@@ -21,17 +19,20 @@ Sentry.init({
 });
 
 app.use(Middleware.setSentryContext);
+
+// IMPORTANT: The Sentry request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
+
+// enable CORS on /sap/* routes
+app.use(apiRouteConstraint, cors(corsConfig)); // enable CORS on /api/* routes
+
 // set up rate limiter: maximum of five requests per minute
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 1000, // limit each IP to 1000 requests per windowMs
 });
-
 // apply rate limiter to all requests
 app.use(limiter);
-
-// IMPORTANT: The Sentry request handler must be the first middleware on the app
-app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 
 // TODO: mount headers before verify
 app.use(apiRouteConstraint, Middleware.loadAppConfig); // load app config on /api/* routes
@@ -40,8 +41,6 @@ app.use(apiRouteConstraint, Middleware.getAppInstallationParameters); // get app
 
 app.use('/sap', SAPController);
 
-app.get('/ping', async (req, res) => {
-  res.send('Hello World!');
-});
+app.use(Middleware.sentryErrorHandler);
 
 export default app;
