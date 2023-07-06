449a2d30f4054a5c6e9537e1cd9536182b2b013b
Lisa White <lisa.white@contentful.com>
2023-07-05 20:24:11 -0600

    Filter out deleted files in prettier check (#3995)

--- .circleci/prettier-check.sh
+++ .circleci/prettier-check.sh
@@ -7,7 +7,7 @@
 test -n "$SKIP_PRETTIER_CHECK" && exit 0
 
 MERGE_BASE=$(git merge-base HEAD origin/master)
-CHANGED_FILES=$(git diff --name-only HEAD $MERGE_BASE | egrep --color=no "\.[jt]s(x)?$" | xargs)
+CHANGED_FILES=$(git diff --name-only --diff-filter=d $MERGE_BASE | egrep --color=no "\.[jt]s(x)?$" | xargs)
 echo $CHANGED_FILES
 
 test -z "$CHANGED_FILES" && echo "No files for prettier to check. Skipping..." && exit 0
