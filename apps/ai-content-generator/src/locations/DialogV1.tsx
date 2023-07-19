210cdfeb825c83900e2265b3d052f1c39c285bc8
Lillian Bitner <lillian.bitner@contentful.com>
2023-07-19 16:50:44 -0600

    Revert changes and add env variable flag

--- apps/ai-content-generator/src/locations/DialogV1.tsx
+++ apps/ai-content-generator/src/locations/DialogV1.tsx
@@ -1,9 +1,9 @@
-import { DialogAppSDK } from '@contentful/app-sdk';
+import React from 'react';
 import { Paragraph } from '@contentful/f36-components';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
 const Dialog = () => {
-  const sdk = useSDK<DialogAppSDK>();
+  const sdk = useSDK();
   /*
      To use the cma, inject it as follows.
      If it is not needed, you can remove the next line.
