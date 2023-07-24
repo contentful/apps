7dc76068b34a8e805feb08c6769a9cfe27922432
George Chios <george.chios@contentful.com>
2023-07-24 16:59:33 -0600

    removes rogue 3, and labels files with v1

--- apps/ai-content-generator/src/richTextModel.ts
+++ apps/ai-content-generator/src/richTextModel.ts
@@ -1,20 +1,21 @@
+// V1
 export default function RichTextModel(text: string) {
-    return {
-      nodeType: 'document',
-      data: {},
-      content: [
-        {
-          nodeType: 'paragraph',
-          data: {},
-          content: [
-            {
-              nodeType: 'text',
-              value: text.replace(/^\n+/, ''),
-              data: {},
-              marks: [],
-            },
-          ],
-        },
-      ],
-    };
-  }
+  return {
+    nodeType: 'document',
+    data: {},
+    content: [
+      {
+        nodeType: 'paragraph',
+        data: {},
+        content: [
+          {
+            nodeType: 'text',
+            value: text.replace(/^\n+/, ''),
+            data: {},
+            marks: [],
+          },
+        ],
+      },
+    ],
+  };
+}
