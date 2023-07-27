9ad9ebb50c9ca1b21eb31737a0fbd5c154bd0c0f
George Chios <george.chios@contentful.com>
2023-07-27 12:08:33 -0600

    fix: content generation now uses gavin's new prompt

--- apps/ai-content-generator/src/configs/prompts/contentPrompt.ts
+++ apps/ai-content-generator/src/configs/prompts/contentPrompt.ts
@@ -1,3 +1,4 @@
-const contentPrompt = (prompt: string) => `Write a blog post in 400 words about "${prompt}"`;
+const contentPrompt = (prompt: string) =>
+  `You have to write the content of a blog post in 400 words or less about ${prompt}. Only generate the body content for the blog post and do not include a title or header in the response. Pretend that you are just writing the article, and someone else is writing the title.`;
 
 export default contentPrompt;
