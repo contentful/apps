374355bf4252749216307580e09766a5a8666e61
George Chios <38636581+rusticpenguin@users.noreply.github.com>
2023-08-01 13:18:44 -0600

    fix: save content before launching ai content generator (#4353)
    * fix: save content before launching ai content generator

--- apps/ai-content-generator/src/components/app/sidebar/SidebarButtons.tsx
+++ apps/ai-content-generator/src/components/app/sidebar/SidebarButtons.tsx
@@ -1,10 +1,24 @@
 import { Box } from '@contentful/f36-components';
 import featureConfig, { AIFeature } from '@configs/features/featureConfig';
 import FeatureButton from './feature-button/FeatureButton';
+import { useState } from 'react';
 
 const SidebarButtons = () => {
+  const [isSaving, setIsSaving] = useState(false);
+
+  const handleSaving = (toggleTo: boolean) => {
+    setIsSaving(toggleTo);
+  };
+
   const featureList = Object.keys(featureConfig).map((feature) => {
-    return <FeatureButton key={feature} feature={feature as AIFeature} />;
+    return (
+      <FeatureButton
+        key={feature}
+        feature={feature as AIFeature}
+        isSaving={isSaving}
+        onSaving={handleSaving}
+      />
+    );
   });
 
   return <Box>{featureList}</Box>;
--- apps/ai-content-generator/src/components/app/sidebar/feature-button/FeatureButton.tsx
+++ apps/ai-content-generator/src/components/app/sidebar/feature-button/FeatureButton.tsx
@@ -1,12 +1,15 @@
 import { useSDK } from '@contentful/react-apps-toolkit';
 import { SidebarAppSDK } from '@contentful/app-sdk';
-import { Button, Tooltip } from '@contentful/f36-components';
+import { Button, Spinner, Tooltip } from '@contentful/f36-components';
 import featureConfig, { AIFeature } from '@configs/features/featureConfig';
 import { styles } from './FeatureButton.styles';
 import { makeDialogConfig } from '@configs/dialog/dialogConfig';
+import { useState } from 'react';
 
 interface Props {
   feature: AIFeature;
+  isSaving: boolean;
+  onSaving: (isSaving: boolean) => void;
 }
 
 interface FieldLocales {
@@ -14,9 +17,11 @@ interface FieldLocales {
 }
 
 const FeatureButton = (props: Props) => {
-  const { feature } = props;
+  const { feature, isSaving, onSaving } = props;
   const buttonCopy = featureConfig[feature].buttonTitle;
   const sdk = useSDK<SidebarAppSDK>();
+
+  const [isOpeningDialog, setIsOpeningDialog] = useState(false);
 
   const fields = sdk.entry.fields;
   const fieldLocales = Object.entries(fields).reduce((acc, entryField) => {
@@ -25,7 +30,25 @@ const FeatureButton = (props: Props) => {
     return acc;
   }, {} as FieldLocales);
 
-  const handleOnClick = () => {
+  const updateSaveState = (toggleTo: boolean) => {
+    onSaving(toggleTo);
+    setIsOpeningDialog(toggleTo);
+  };
+
+  const handleOnClick = async () => {
+    if (isSaving) {
+      return;
+    }
+
+    try {
+      updateSaveState(true);
+      await sdk.entry.save();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      updateSaveState(false);
+    }
+
     const entryId = sdk.entry.getSys().id;
 
     const dialogConfig = makeDialogConfig({ feature, entryId, fieldLocales });
@@ -36,7 +59,7 @@ const FeatureButton = (props: Props) => {
   return (
     <Tooltip placement="top" id={feature}>
       <Button css={styles.button} onClick={handleOnClick}>
-        {buttonCopy}
+        {buttonCopy} {isOpeningDialog && <Spinner size="small" />}
       </Button>
     </Tooltip>
   );
