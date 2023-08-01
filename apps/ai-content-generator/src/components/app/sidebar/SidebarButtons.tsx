7c973f89fe405e964550df72bd24de0a6bc4f84b
George Chios <george.chios@contentful.com>
2023-08-01 12:02:47 -0600

    fix: save content before launching ai content generator

--- apps/ai-content-generator/src/components/app/sidebar/SidebarButtons.tsx
+++ apps/ai-content-generator/src/components/app/sidebar/SidebarButtons.tsx
@@ -1,10 +1,19 @@
 import { Box } from '@contentful/f36-components';
 import featureConfig, { AIFeature } from '@configs/features/featureConfig';
 import FeatureButton from './feature-button/FeatureButton';
+import { useState } from 'react';
 
 const SidebarButtons = () => {
+  const [isSaving, setIsSaving] = useState(false);
   const featureList = Object.keys(featureConfig).map((feature) => {
-    return <FeatureButton key={feature} feature={feature as AIFeature} />;
+    return (
+      <FeatureButton
+        key={feature}
+        feature={feature as AIFeature}
+        isSaving={isSaving}
+        setIsSaving={setIsSaving}
+      />
+    );
   });
 
   return <Box>{featureList}</Box>;
