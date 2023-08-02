848e464baaaf8a8487670e3ed7eddc878d6c634e
George Chios <george.chios@contentful.com>
2023-08-02 12:39:24 -0600

    fix disclaimer message

--- apps/ai-content-generator/src/components/app/sidebar/disclaimer-message/DisclaimerMessage.spec.tsx
+++ apps/ai-content-generator/src/components/app/sidebar/disclaimer-message/DisclaimerMessage.spec.tsx
@@ -4,13 +4,9 @@ import DisclaimerMessage, { BODY_MSG } from './DisclaimerMessage';
 
 describe('DisclaimerMessage component', () => {
   it('mounts', async () => {
-    // const { getByText, unmount } = render(<DisclaimerMessage />);
-    // TODO: Figure out why this is not working with the vitest library
-    // const message =
-    // "This feature uses a third party AI tool. Please ensure your use of the tool and any AI-generated content complies with applicable laws, your compan's policies, and all other";
-    // expect(message).toBeTruthy();
-    // const text = container.querySelector('a');
-    // expect(getByText(message)).toBeTruthy();
-    // unmount();
+    const { getByText, unmount } = render(<DisclaimerMessage />);
+    const message = 'Terms and Policies';
+
+    expect(getByText(message)).toBeTruthy();
   });
 });
