8c3845458f9390f1e7e4bdad60077697e293fb18
ryunsong-contentful <ryun.song@contentful.com>
2023-08-02 16:21:33 -0600

    Adds launch darkly to AI Image Generator

--- apps/ai-image-generator/frontend/src/index.tsx
+++ apps/ai-image-generator/frontend/src/index.tsx
@@ -2,21 +2,29 @@ import { createRoot } from 'react-dom/client';
 
 import { GlobalStyles } from '@contentful/f36-components';
 import { SDKProvider } from '@contentful/react-apps-toolkit';
+import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 
 import App from './App';
 import LocalhostWarning from './components/LocalhostWarning';
+import launchDarklyConfig from './configs/launch-darkly/launchDarklyConfig';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+(async () => {
+  const LDProvider = await asyncWithLDProvider(launchDarklyConfig);
 
-if (import.meta.env.DEV && window.self === window.top) {
-  // You can remove this if block before deploying your app
-  root.render(<LocalhostWarning />);
-} else {
-  root.render(
-    <SDKProvider>
-      <GlobalStyles />
-      <App />
-    </SDKProvider>
-  );
-}
+  const container = document.getElementById('root')!;
+  const root = createRoot(container);
+
+  if (import.meta.env.DEV && window.self === window.top) {
+    // You can remove this if block before deploying your app
+    root.render(<LocalhostWarning />);
+  } else {
+    root.render(
+      <SDKProvider>
+        <LDProvider>
+          <GlobalStyles />
+          <App />
+        </LDProvider>
+      </SDKProvider>
+    );
+  }
+})();
