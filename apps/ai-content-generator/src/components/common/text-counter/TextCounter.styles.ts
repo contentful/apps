3f16328f67343e1cfacdc434b6a86a7229175cf2
George Chios <george.chios@contentful.com>
2023-08-03 10:56:14 -0600

    add colors

--- apps/ai-content-generator/src/components/common/text-counter/TextCounter.styles.ts
+++ apps/ai-content-generator/src/components/common/text-counter/TextCounter.styles.ts
@@ -1,13 +1,13 @@
+import tokens from '@contentful/f36-tokens';
 import { css } from '@emotion/react';
 
 export const styles = {
-  // How do I access these colors from forma36?
   validCount: css({
     marginBottom: 0,
-    color: '#414D63',
+    color: tokens.gray700,
   }),
   invalidCount: css({
     marginBottom: 0,
-    color: '#DA294A',
+    color: tokens.red500,
   }),
 };
