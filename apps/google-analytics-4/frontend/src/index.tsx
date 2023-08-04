9894dcc747c63f7ff627f5717d9657e0089f462c
Lillian Bitner <lillian.bitner@contentful.com>
2023-08-04 14:58:26 -0600

    Remove sentry tracing

--- apps/google-analytics-4/frontend/src/index.tsx
+++ apps/google-analytics-4/frontend/src/index.tsx
@@ -1,6 +1,4 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
-import React from 'react';
 import { render } from 'react-dom';
 
 import { GlobalStyles } from '@contentful/f36-components';
@@ -13,7 +11,7 @@ import { SegmentAnalyticsProvider } from 'providers/SegmentAnalyticsProvider';
 
 Sentry.init({
   dsn: config.sentryDSN,
-  integrations: [new BrowserTracing()],
+  integrations: [new Sentry.BrowserTracing()],
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
