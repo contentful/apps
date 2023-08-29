7b5681c0f1e686dcc1f054f7e162a326fe635171
Bas Kiers <kiers.bas@gmail.com>
2023-08-29 11:40:38 +0200

    feat: update example to make use of new interface [MONET-1335]
    * feat: update example to make use of new interface
    
    * chore: add new changes in interface type
    
    * feat: remove exmaple implementation code

--- examples/hosted-delivery-function-templates/typescript/example.ts
+++ examples/hosted-delivery-function-templates/typescript/example.ts
@@ -1,24 +1,38 @@
-import { DeliveryFunctionEventHandlers, DeliveryFunctionRequestEventType } from '@contentful/node-apps-toolkit';
+import {
+  DeliveryFunctionEventHandler as EventHandler,
+  DeliveryFunctionEventType as EventType,
+} from '@contentful/node-apps-toolkit';
 
-export const handlers: DeliveryFunctionEventHandlers = {
-  [DeliveryFunctionRequestEventType.GRAPHQL_FIELD_MAPPING]: async (event, context) => {
-    const result = []
-    const fields = event.fields;
-    fields.forEach(({ contentTypeId, field }) => {
-      result.push({
-        contentTypeId,
-        fieldId: field.id,
-        graphQLOutputType: 'Product',
-        graphQLQueryField: 'product',
-        graphQLQueryArgument: 'id'
-      })
-    })
-    return result;
-  },
-  [DeliveryFunctionRequestEventType.GRAPHQL_QUERY]: async (event, context) => {
+const fieldMappingHandler: EventHandler<EventType.GRAPHQL_FIELD_MAPPING> = (event, context) => {
+  const fields = event.fields.map(({ contentTypeId, field }) => {
     return {
-      data: {},
-      errors: []
-    }
+      contentTypeId,
+      fieldId: field.id,
+      graphQLOutputType: 'Foo',
+      graphQLQueryField: 'bar',
+      graphQLQueryArguments: { id: '' },
+    };
+  });
+
+  return {
+    namespace: 'MyApp',
+    fields,
+  };
+};
+
+const queryHandler: EventHandler<EventType.GRAPHQL_QUERY> = (event, context) => {
+  return {
+    data: {},
+    errors: [],
+  };
+};
+
+export const handler: EventHandler = (event, context) => {
+  if (event.type === EventType.GRAPHQL_FIELD_MAPPING) {
+    return fieldMappingHandler(event, context);
   }
-}
+  if (event.type === EventType.GRAPHQL_QUERY) {
+    return queryHandler(event, context);
+  }
+  throw new Error('Unknown Event');
+};
