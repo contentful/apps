625919715eaa0957fd7ceb49dc8a6fa2f9993a13
ryunsong-contentful <ryun.song@contentful.com>
2023-09-22 14:52:14 -0600

    adds read the docs and removes ld from aiig

--- apps/ai-image-generator/frontend/src/App.tsx
+++ apps/ai-image-generator/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { locations } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import ConfigScreen from './locations/ConfigScreen';
 import Dialog from './locations/Dialog';
 import EntryEditor from './locations/EntryEditor';
@@ -8,10 +8,8 @@ import Field from './locations/Field';
 import Page from './locations/Page';
 import Sidebar from './locations/Sidebar';
 import Home from './locations/Home';
-import { useFlags, useLDClient } from 'launchdarkly-react-client-sdk';
-import { launchDarklyConfigType } from './configs/launch-darkly/launchDarklyConfig';
 
-const ComponentLocationSettings = (isV2: boolean) => ({
+const ComponentLocationSettings = {
   [locations.LOCATION_APP_CONFIG]: ConfigScreen,
   [locations.LOCATION_ENTRY_FIELD]: Field,
   [locations.LOCATION_ENTRY_EDITOR]: EntryEditor,
@@ -19,31 +17,18 @@ const ComponentLocationSettings = (isV2: boolean) => ({
   [locations.LOCATION_ENTRY_SIDEBAR]: Sidebar,
   [locations.LOCATION_PAGE]: Page,
   [locations.LOCATION_HOME]: Home,
-});
+};
 
 const App = () => {
   const sdk = useSDK();
-  const { aiigFlagV2 } = useFlags<launchDarklyConfigType>();
-  const ldClient = useLDClient();
-
-  useEffect(() => {
-    ldClient?.identify({
-      key: sdk.user.sys.id,
-      custom: {
-        currentSpaceId: sdk.ids.space,
-      },
-    });
-  }, [ldClient, sdk.ids.space, sdk.user.sys.id]);
 
   const Component = useMemo(() => {
-    const locations = ComponentLocationSettings(aiigFlagV2);
-
-    for (const [location, component] of Object.entries(locations)) {
+    for (const [location, component] of Object.entries(ComponentLocationSettings)) {
       if (sdk.location.is(location)) {
         return component;
       }
     }
-  }, [sdk.location, aiigFlagV2]);
+  }, [sdk.location]);
 
   return Component ? <Component /> : null;
 };
