3c2a5e0fb74c182d0eff3935c1abf86aa6c9a8de
Jim Dalton <jim.dalton@gmail.com>
2023-09-28 07:47:53 -0700

    add comment

--- apps/typeform/frontend/src/constants.ts
+++ apps/typeform/frontend/src/constants.ts
@@ -1,5 +1,7 @@
 const SDK_WINDOW_HEIGHT = 450;
 const BASE_URL = 'https://api.typeform.com';
+
+// TODO Move to env var
 const CLIENT_ID = 'HC3UDnoiaP1UCMqJ7kCAyTFdHrDt8nLtXx4BKRJxom2M';
 
 export { SDK_WINDOW_HEIGHT, BASE_URL, CLIENT_ID };
