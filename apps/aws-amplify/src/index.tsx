e9f981760127f755507643f5c8d959c435359b21
Lillian Bitner <lillian.bitner@gmail.com>
2023-10-05 15:18:46 -0600

    add: components from new component library (#5068)

--- apps/aws-amplify/src/index.tsx
+++ apps/aws-amplify/src/index.tsx
@@ -3,14 +3,14 @@ import { render } from 'react-dom';
 import { GlobalStyles } from '@contentful/f36-components';
 import { SDKProvider } from '@contentful/react-apps-toolkit';
 
-import LocalhostWarning from './components/LocalhostWarning';
+import { LocalHostWarning } from '@contentful/integration-component-library';
 import App from './App';
 
 const root = document.getElementById('root');
 
 if (process.env.NODE_ENV === 'development' && window.self === window.top) {
   // You can remove this if block before deploying your app
-  render(<LocalhostWarning />, root);
+  render(<LocalHostWarning />, root);
 } else {
   render(
     <SDKProvider>
