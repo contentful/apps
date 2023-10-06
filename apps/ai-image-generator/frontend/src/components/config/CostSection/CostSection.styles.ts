bfb6265716cf86be812811d753eeba9a7386d643
ryunsong-contentful <ryun.song@contentful.com>
2023-10-06 15:01:42 -0600

    adds missing link about dall e credits in aiig config page

--- apps/ai-image-generator/frontend/src/components/config/CostSection/CostSection.styles.ts
+++ apps/ai-image-generator/frontend/src/components/config/CostSection/CostSection.styles.ts
@@ -2,12 +2,16 @@ import tokens from '@contentful/f36-tokens';
 import { css } from 'emotion';
 
 export const styles = {
+  header: css({
+    marginBottom: tokens.spacingS,
+  }),
   link: css({
-    color: tokens.gray900
+    color: tokens.gray900,
+    margin: 0,
   }),
   wrapper: css({
     'p:last-child': {
-        marginTop: 0
-    }
-  })
+      marginTop: 0,
+    },
+  }),
 };
--- apps/ai-image-generator/frontend/src/components/config/CostSection/CostSection.tsx
+++ apps/ai-image-generator/frontend/src/components/config/CostSection/CostSection.tsx
@@ -2,6 +2,7 @@ import { Flex, HelpText, Subheading, Text } from '@contentful/f36-components';
 import configPageCopies from 'constants/configPageCopies';
 import { HyperLink } from '@contentful/integration-component-library';
 import { styles } from './CostSection.styles';
+import { ExternalLinkIcon } from '@contentful/f36-icons';
 
 const CostSection = () => {
   const {
@@ -13,11 +14,14 @@ const CostSection = () => {
     creditLinkBody,
     creditLinkSubstring,
     creditLinkHref,
+    learnBodyHref,
+    learnMoreBody,
+    learnMoreSubstring,
   } = configPageCopies.costSection;
   return (
     <Flex className={styles.wrapper} flexDirection="column">
       <Subheading>{sectionTitle}</Subheading>
-      <Text fontWeight="fontWeightMedium" fontColor="gray900">
+      <Text className={styles.header} fontWeight="fontWeightMedium" fontColor="gray900">
         {sectionSubheading}
       </Text>
       <HelpText className={styles.link}>
@@ -25,6 +29,8 @@ const CostSection = () => {
           body={pricingLinkBody}
           substring={pricingLinkSubstring}
           href={pricingLinkHref}
+          icon={<ExternalLinkIcon />}
+          alignIcon="end"
         />
       </HelpText>
       <HelpText className={styles.link}>
@@ -32,6 +38,17 @@ const CostSection = () => {
           body={creditLinkBody}
           substring={creditLinkSubstring}
           href={creditLinkHref}
+          icon={<ExternalLinkIcon />}
+          alignIcon="end"
+        />
+      </HelpText>
+      <HelpText className={styles.link}>
+        <HyperLink
+          body={learnMoreBody}
+          substring={learnMoreSubstring}
+          href={learnBodyHref}
+          icon={<ExternalLinkIcon />}
+          alignIcon="end"
         />
       </HelpText>
     </Flex>
