2f1780aee209b75b7142a4e4ab183cc93b142051
Jim Dalton <jim.dalton@gmail.com>
2023-10-26 14:49:21 -0700

    chore: add MS Teams backend

---
+++ apps/microsoft-teams/backend/src/types.ts
@@ -0,0 +1,17 @@
+export interface ActionError {
+  type: string;
+  message: string;
+  details?: Record<string, any>;
+}
+
+export interface AppActionCallResponseSuccess<TResult> {
+  ok: true;
+  data: TResult;
+}
+
+export interface AppActionCallResponseError {
+  ok: false;
+  errors: ActionError[];
+}
+
+export type AppActionCallResponse<T> = AppActionCallResponseSuccess<T> | AppActionCallResponseError;
---
+++ apps/microsoft-teams/backend/test/mocks.ts
@@ -0,0 +1,32 @@
+import { AppActionCallContext } from '@contentful/node-apps-toolkit';
+import { Adapter, PlainClientAPI, createClient } from 'contentful-management';
+import sinon from 'sinon';
+
+export const makeMockPlainClient = (responses: any[], stub: sinon.SinonStub): PlainClientAPI => {
+  for (const [callNumber, response] of responses.entries()) {
+    stub.onCall(callNumber).returns(response);
+  }
+  const apiAdapter: Adapter = {
+    makeRequest: async <T>(args: T) => {
+      return stub(args);
+    },
+  };
+  return createClient({ apiAdapter }, { type: 'plain' });
+};
+
+export const makeMockAppActionCallContext = (
+  responses: any[],
+  cmaStub = sinon.stub()
+): AppActionCallContext => {
+  return {
+    cma: makeMockPlainClient(responses, cmaStub),
+    appActionCallContext: {
+      spaceId: 'space-id',
+      environmentId: 'environment-id',
+      appInstallationId: 'app-installation-id',
+      userId: 'user-id',
+      cmaHost: 'api.contentful.com',
+      uploadHost: 'upload.contentful.com',
+    },
+  };
+};
