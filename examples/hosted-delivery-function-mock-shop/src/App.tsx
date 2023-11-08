0f003fe30d4cf5838c51e20e322db3faa805af4b
Laurence Bortfeld <laurence.bortfeld@contentful.com>
2023-11-08 15:09:51 +0100

    chore: home page not used

--- examples/hosted-delivery-function-mock-shop/src/App.tsx
+++ examples/hosted-delivery-function-mock-shop/src/App.tsx
@@ -5,13 +5,11 @@ import { useSDK } from "@contentful/react-apps-toolkit";
 import ConfigScreen from "./locations/ConfigScreen";
 import Field from "./locations/Field";
 import Dialog from "./locations/Dialog";
-import Home from "./locations/Home";
 
 const ComponentLocationSettings = {
   [locations.LOCATION_APP_CONFIG]: ConfigScreen,
   [locations.LOCATION_ENTRY_FIELD]: Field,
   [locations.LOCATION_DIALOG]: Dialog,
-  [locations.LOCATION_HOME]: Home,
 };
 
 const App = () => {
--- examples/hosted-delivery-function-mock-shop/src/locations/Home.tsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from '@contentful/f36-components';
-
-const Home = () => {
-  return <Heading>Mock Shop Tutorial</Heading>;
-};
-
-export default Home;
