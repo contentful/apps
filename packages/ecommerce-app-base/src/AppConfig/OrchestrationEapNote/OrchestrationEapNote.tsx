c1552fe4ad0d593d86e570fd1cfb79ffff10dba2
cemreyuksel <cemre.yuksel@contentful.com>
2023-11-17 13:59:47 +0100

    feat: update

--- packages/ecommerce-app-base/src/AppConfig/OrchestrationEapNote/OrchestrationEapNote.tsx
+++ packages/ecommerce-app-base/src/AppConfig/OrchestrationEapNote/OrchestrationEapNote.tsx
@@ -18,19 +18,19 @@ interface Props {
 
 export const OrchestrationEapNote = ({ hideOrchestrationEapNote, name }: Props) => {
   const [, setHideOrchestrationEapNote] = useState(false);
-
+  const title = `The ${name} app supports External references`;
   return (
     <Note
       style={{ display: hideOrchestrationEapNote ? 'none' : 'grid' }}
       className={styles.eapNote}
       withCloseButton={true}
       onClose={() => setHideOrchestrationEapNote(true)}
-      title="This app supports External references">
+      title={title}>
       Contentful now supports unified delivery of content from both Contentful and {name} available
       on our GraphQL API.
       <br />
       <br />
-      Learn more and get started today with our Introduction to{' '}
+      Learn more and get started today with our introduction to{' '}
       <TextLink
         variant="primary"
         target="_blank"
