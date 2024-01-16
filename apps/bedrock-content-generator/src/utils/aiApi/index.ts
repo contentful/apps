f9b6f8d260c53a3481f3255707798d0530beaaef
Malte Janßen <maltjan@amazon.de>
2024-01-16 20:53:17 +0100

    remove unused openai code

--- apps/bedrock-content-generator/src/utils/aiApi/index.ts
+++ apps/bedrock-content-generator/src/utils/aiApi/index.ts
@@ -8,13 +8,6 @@ import {
   ResponseStream,
 } from "@aws-sdk/client-bedrock-runtime";
 
-/**
- * This class is used to interact with OpenAI's API.
- * Allowing us to create and manage a stream to the API, similar to openai's node package.
- * @param baseUrl string
- * @param apiKey string
- * @param model string
- */
 class AI {
   model?: string;
   decoder: TextDecoder;
@@ -63,6 +56,7 @@ class AI {
       decoder: TextDecoder,
       stream: AsyncIterable<ResponseStream>,
     ) {
+
       for await (const chunk of stream) {
         if (chunk.chunk) {
           const textData = decoder.decode(chunk.chunk.bytes);
@@ -77,30 +71,7 @@ class AI {
     return transformStream(this.decoder, stream.body);
   };
 
-  /**
-   * Use this function in a while loop to parse the stream returned from OpenAI's API.
-   * This function will return false if the stream is done.
-   * @param stream ReadableStreamDefaultReader<Uint8Array>
-   * @returns string | false
-   */
-  // parseStream = async (stream: ReadableStreamDefaultReader<Uint8Array>) => {
-  //   const { done, value } = await stream.read();
 
-  //   if (done) {
-  //     return false;
-  //   }
-
-  //   const dataList = this.decoder.decode(value as Buffer);
-  //   const lines = dataList.split(/\n{2}/);
-
-  //   const textData = lines.reduce(streamToParsedText, "");
-
-  //   if (textData) {
-  //     return textData;
-  //   }
-
-  //   return "";
-  // };
 
   /**
    * This function will send a stop signal to OpenAI's API.
