c80adf9dee510864436bd362460f43db35f38545
Lisa White <lisa.white@contentful.com>
2024-01-17 15:54:38 -0700

    test: update tests for test notification

--- apps/microsoft-teams/app-actions/src/actions/send-test.spec.ts
+++ apps/microsoft-teams/app-actions/src/actions/send-test.spec.ts
@@ -2,8 +2,15 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { handler } from './send-test';
 import { makeMockAppActionCallContext } from '../../test/mocks';
-import { SysLink, ContentTypeProps, ContentFields } from 'contentful-management';
+import {
+  SysLink,
+  ContentTypeProps,
+  ContentFields,
+  AppInstallationProps,
+} from 'contentful-management';
 import { AppActionCallContext } from '@contentful/node-apps-toolkit';
+import helpers from '../helpers';
+import { AppActionCallResponseSuccess } from '../types';
 
 describe('sendTestNotification.handler', () => {
   let cmaRequestStub: sinon.SinonStub;
@@ -15,8 +22,9 @@ describe('sendTestNotification.handler', () => {
     contentTypeId: 'blogPost',
     spaceName: 'My test space',
   };
+  const tenantId = 'msteams-tenant-id';
 
-  const cmaClientMockResponses: ContentTypeProps[] = [
+  const cmaClientMockResponses: (ContentTypeProps | AppInstallationProps)[] = [
     {
       sys: {
         type: 'ContentType',
@@ -32,19 +40,41 @@ describe('sendTestNotification.handler', () => {
       displayField: 'displayField',
       fields: [] as ContentFields[],
     },
+    {
+      sys: {
+        type: 'AppInstallation',
+        appDefinition: {} as SysLink,
+        environment: {} as SysLink,
+        space: {} as SysLink,
+        version: 1,
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
+      },
+      parameters: {
+        tenantId: tenantId,
+      },
+    },
   ];
 
   beforeEach(() => {
     cmaRequestStub = sinon.stub();
     context = makeMockAppActionCallContext(cmaClientMockResponses, cmaRequestStub);
+    sinon
+      .stub(helpers, 'sendTestNotification')
+      .returns(Promise.resolve({ ok: true, data: 'message-id' }));
   });
 
-  // TODO: add test to verify the output
-  it('calls the cma to get the tenant id from app installation params', async () => {
+  it('calls the cma to get the content type name', async () => {
     await handler(parameters, context);
     expect(cmaRequestStub).to.have.been.calledWithMatch({
       entityType: 'ContentType',
       action: 'get',
     });
+  });
+
+  it('returns the ok result', async () => {
+    const result = (await handler(parameters, context)) as AppActionCallResponseSuccess<string>;
+    expect(result).to.have.property('ok', true);
+    expect(result).to.have.property('data', 'message-id');
   });
 });
---
+++ apps/microsoft-teams/app-actions/src/utils.spec.ts
@@ -0,0 +1,39 @@
+import { AppInstallationProps, SysLink } from 'contentful-management';
+import { fetchTenantId } from './utils';
+import sinon from 'sinon';
+import { makeMockPlainClient } from '../test/mocks';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+describe('fetchTenantId', () => {
+  const cmaStub = sinon.stub();
+  const tenantId = 'msteams-tenant-id';
+
+  const mockAppInstallation: AppInstallationProps = {
+    sys: {
+      type: 'AppInstallation',
+      appDefinition: {} as SysLink,
+      environment: {} as SysLink,
+      space: {} as SysLink,
+      createdAt: 'createdAt',
+      version: 1,
+      updatedAt: 'updatedAt',
+    },
+    parameters: {
+      tenantId: tenantId,
+    },
+  };
+  const cma = makeMockPlainClient([mockAppInstallation], cmaStub);
+  const appInstallationId = 'appInstallationId';
+
+  it('returns the tenant id', async () => {
+    const result = await fetchTenantId(cma, appInstallationId);
+    expect(cmaStub).to.have.been.calledWithMatch({
+      entityType: 'AppInstallation',
+      action: 'get',
+    });
+    expect(result).to.eq(tenantId);
+  });
+});
