7097255078979f1a5e3c37f8c7fa24eab0498584
Jim Dalton <jim.dalton@gmail.com>
2024-01-24 07:44:23 -0800

    remove unneeded type

--- apps/microsoft-teams/frontend/src/customTypes/configPage.ts
+++ apps/microsoft-teams/frontend/src/customTypes/configPage.ts
@@ -1,14 +1,3 @@
-export const TOPIC_ACTION_MAP = {
-  'ContentManagement.Entry.create': 'created',
-  'ContentManagement.Entry.save': 'saved',
-  'ContentManagement.Entry.auto_save': 'auto saved',
-  'ContentManagement.Entry.archive': 'archived',
-  'ContentManagement.Entry.unarchive': 'unarchived',
-  'ContentManagement.Entry.publish': 'published',
-  'ContentManagement.Entry.unpublish': 'unpublished',
-  'ContentManagement.Entry.delete': 'deleted',
-} as const;
-
 export enum AppEventKey {
   ENTRY_PUBLISH = 'ContentManagement.Entry.publish',
   ENTRY_UNPUBLISHED = 'ContentManagement.Entry.unpublish',
