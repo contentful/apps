e2947c18d476ca8550d95d07361a8f504250c8ad
Matthew Gordon <matt.gordon@contentful.com>
2024-01-24 08:58:17 -0700

    feat: [INTEG-1686] - Add vercel base app (#6084)
    Initial vercel vite app

---
+++ apps/vercel/src/locations/ConfigScreen.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { mockCma, mockSdk } from '../../test/mocks';
+import ConfigScreen from './ConfigScreen';
+
+vi.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: () => mockSdk,
+  useCMA: () => mockCma,
+}));
+
+const saveAppInstallation = () => {
+  // We manually call the LAST onConfigure() callback (this is important, as earlier calls have stale data)
+  return mockSdk.app.onConfigure.mock.calls.at(-1)[0]();
+};
+
+describe('ConfigScreen', () => {
+  const testToken = 'abc1234';
+
+  it('renders setup view', async () => {
+    const { unmount } = render(<ConfigScreen />);
+    // simulate the user clicking the install button
+    await mockSdk.app.onConfigure.mock.calls[0][0]();
+
+    expect(screen.getByText('Set Up Vercel')).toBeTruthy();
+    expect(screen.getByText('Vercel Access Token')).toBeTruthy();
+    unmount();
+  });
+});
---
+++ apps/vercel/src/locations/ConfigScreen.tsx
@@ -0,0 +1,118 @@
+import { useCallback, useState, useEffect, ChangeEvent } from 'react';
+import { ConfigAppSDK } from '@contentful/app-sdk';
+import {
+  Box,
+  Flex,
+  FormControl,
+  Heading,
+  HelpText,
+  Paragraph,
+  Stack,
+  TextInput,
+  TextLink,
+} from '@contentful/f36-components';
+import { useSDK } from '@contentful/react-apps-toolkit';
+import { ExternalLinkIcon } from '@contentful/f36-icons';
+import { styles } from '../components/config-screen/ConfigScreen.styles';
+import VercelIcon from '../components/common/VercelIcon';
+
+export interface AppInstallationParameters {
+  accessToken?: string;
+}
+
+const ConfigScreen = () => {
+  const sdk = useSDK<ConfigAppSDK>();
+  const [parameters, setParameters] = useState<AppInstallationParameters>({});
+  const [accessToken, setAccessToken] = useState<string>('');
+
+  const onConfigure = useCallback(async () => {
+    const currentState = await sdk.app.getCurrentState();
+
+    const parametersToSave = {
+      ...parameters,
+      accessToken,
+    };
+
+    setParameters(parametersToSave);
+
+    return {
+      parameters: parametersToSave,
+      targetState: currentState,
+    };
+  }, [parameters, sdk, accessToken]);
+
+  useEffect(() => {
+    sdk.app.onConfigure(() => onConfigure());
+  }, [sdk, onConfigure]);
+
+  useEffect(() => {
+    (async () => {
+      const currentParameters = await sdk.app.getParameters();
+
+      if (currentParameters) {
+        setParameters(currentParameters);
+        setAccessToken(currentParameters.accessToken);
+      }
+
+      sdk.app.setReady();
+    })();
+  }, [sdk]);
+
+  const handleAccessTokenChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAccessToken(e.target.value);
+  };
+
+  return (
+    <>
+      <Box className={styles.background} />
+      <Box className={styles.body}>
+        <Box>
+          <Heading>Set Up Vercel</Heading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.
+          </Paragraph>
+        </Box>
+        <Stack spacing="spacingL" flexDirection="column">
+          <Box style={styles.box}>
+            <FormControl id="accessToken" isRequired={true}>
+              <FormControl.Label aria-label="accessToken" htmlFor="accessToken">
+                Vercel Access Token
+              </FormControl.Label>
+              <TextInput
+                testId="accessToken"
+                spellCheck={false}
+                name="accessToken"
+                type="password"
+                placeholder={'ex. atE2sdftcIp01O1isdfXc3QTdT4...'}
+                value={accessToken}
+                onChange={handleAccessTokenChange}
+              />
+              <HelpText>
+                Follow{' '}
+                <TextLink
+                  icon={<ExternalLinkIcon />}
+                  alignIcon="end"
+                  href="https://vercel.com/docs/rest-api#creating-an-access-token"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  these detailed instructions
+                </TextLink>{' '}
+                to create an access token in the Vercel dashboard.
+              </HelpText>
+            </FormControl>
+          </Box>
+        </Stack>
+      </Box>
+      <Box style={styles.icon}>
+        <Flex alignItems="center" justifyContent="center">
+          <VercelIcon />
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default ConfigScreen;
