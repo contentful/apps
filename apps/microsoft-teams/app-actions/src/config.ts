27c91b0aab62d006a1dcd586dbcb329e97185c7d
Jim Dalton <jim.dalton@gmail.com>
2024-01-25 07:35:33 -0800

    feat: add support for actually sending messages

--- apps/microsoft-teams/app-actions/src/config.ts
+++ apps/microsoft-teams/app-actions/src/config.ts
@@ -1,3 +1,5 @@
+import { MsTeamsBotService } from './services/msteams-bot-service';
+
 type EnvironmentVariable = string | null | number | undefined;
 
 // Defining this environmentVariables object since we need to have the explicit strings for the env vars defined
@@ -19,7 +21,15 @@ function getEnvironmentVariable(
   return environmentVariableValue;
 }
 
+const envVars = {
+  botServiceUrl: getEnvironmentVariable('MSTEAMS_BOT_SERVICE_BASE_URL'),
+  apiKey: getEnvironmentVariable('MSTEAMS_CLIENT_API_KEY'),
+};
+
+const msTeamsBotService = new MsTeamsBotService(envVars.botServiceUrl, envVars.apiKey);
+
 export const config = {
   botServiceUrl: getEnvironmentVariable('MSTEAMS_BOT_SERVICE_BASE_URL'),
   apiKey: getEnvironmentVariable('MSTEAMS_CLIENT_API_KEY'),
+  msTeamsBotService,
 };
---
+++ apps/microsoft-teams/app-actions/src/services/msteams-bot-service.ts
@@ -0,0 +1,32 @@
+import { EntryActivityMessage, SendMessageResult } from '../types';
+
+export class MsTeamsBotService {
+  constructor(public readonly botServiceUrl: string, public readonly apiKey: string) {}
+
+  async sendEntryActivityMessage(
+    entryActivitytMessage: EntryActivityMessage,
+    tenantId: string
+  ): Promise<SendMessageResult> {
+    const res = await fetch(
+      `${this.botServiceUrl}/api/tenant/${tenantId}/entry_activity_messages`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': this.apiKey,
+        },
+        body: JSON.stringify(entryActivitytMessage),
+      }
+    );
+    const responseBody = await res.json();
+    this.assertSendMessageResult(responseBody);
+    return responseBody;
+  }
+
+  private assertSendMessageResult(value: unknown): asserts value is SendMessageResult {
+    if (typeof value !== 'object' || !value)
+      throw new TypeError('invalid type returned from MsTeamsBotService');
+    if (!('ok' in value))
+      throw new TypeError('malformed SendMessageResult returrnd from MsTeamsBotService API');
+  }
+}
