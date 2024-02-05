71864637d397870bac85094c9aa9746220c9fc5e
Matt Gordon <matt.gordon@contentful.com>
2024-02-05 13:30:11 -0700

    Fix tests

--- apps/vercel/test/mocks/mockSdk.ts
+++ apps/vercel/test/mocks/mockSdk.ts
@@ -1,14 +1,30 @@
 import { vi } from 'vitest';
 
+export const mockParameters = {
+  vercelAccessToken: 'abc-123',
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const mockSdk: any = {
   app: {
     onConfigure: vi.fn(),
     getParameters: vi.fn().mockReturnValueOnce({}),
     setReady: vi.fn(),
     getCurrentState: vi.fn(),
+    onConfigurationCompleted: vi.fn(),
+    isInstalled: vi.fn().mockReturnValue(true),
+  },
+  parameters: {
+    instance: [],
+    installation: {
+      tenantId: mockParameters.vercelAccessToken,
+    },
   },
   ids: {
     app: 'test-app',
+  },
+  notifier: {
+    error: vi.fn(),
   },
 };
 
