618f693c410ca5547e7c84b90f0615e48a049c61
Ethan Ozelius <ethan.ozelius@contentful.com>
2024-02-12 12:05:44 -0700

    fix(ContentTypeSelectionModal & ChannelSelectionModalProps): Clicking entire row should select the option.

--- apps/microsoft-teams/frontend/src/components/config/ContentTypeSelectionModal/ContentTypeSelectionModal.spec.tsx
+++ apps/microsoft-teams/frontend/src/components/config/ContentTypeSelectionModal/ContentTypeSelectionModal.spec.tsx
@@ -1,7 +1,8 @@
 import ContentTypeSelectionModal from './ContentTypeSelectionModal';
 import { describe, expect, it, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { contentTypeSelection } from '@constants/configCopy';
+import { mockContentType } from '@test/mocks';
 
 describe('ContentTypeSelectionModal component', () => {
   it('mounts and renders the correct content', () => {
@@ -35,5 +36,40 @@ describe('ContentTypeSelectionModal component', () => {
     );
 
     expect(screen.getByText(errorMessage)).toBeTruthy();
+  });
+
+  describe('selecting a content type', () => {
+    it("clicking the entire row, selects the row's content type", () => {
+      const notificationEditSpy = vi.fn();
+
+      render(
+        <ContentTypeSelectionModal
+          isShown={true}
+          onClose={vi.fn()}
+          savedContentTypeId=""
+          handleNotificationEdit={notificationEditSpy}
+          contentTypes={[mockContentType]}
+          contentTypeConfigLink=""
+          error={false}
+        />
+      );
+
+      const row = screen.getByText(mockContentType.name).closest('tr');
+      fireEvent.click(row as Element);
+
+      // verify that radio button is checked
+      const radioBtn = screen.getByRole('radio') as HTMLInputElement;
+      expect(radioBtn.checked).toEqual(true);
+
+      // submit form
+      const submitBtn = screen.getByRole('button', { name: 'Select' });
+      fireEvent.click(submitBtn);
+
+      // ensure that notificationEdit() was called with correct value
+      expect(notificationEditSpy).toHaveBeenCalledTimes(1);
+
+      // todo: move this to a before/after block.  Ideally an after
+      vi.restoreAllMocks();
+    });
   });
 });
