c8b25bcfbc5230a4e0d3ac37d9ef9b926313505b
Matt Gordon <matt.gordon@contentful.com>
2024-02-22 13:49:35 -0700

    [WIP] refactor to smaller components for readability

---
+++ apps/vercel/src/components/config-screen/ContentTypeSelect.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, Dispatch, SetStateAction, useEffect } from 'react';
+import { Box, FormControl, Select } from '@contentful/f36-components';
+import { styles } from './ConfigScreen.styles';
+import { AppInstallationParameters } from '../../types';
+import { actions } from '../parameterReducer';
+import { ConfigAppSDK } from '@contentful/app-sdk';
+
+const ContentTypeSelect = ({
+  parameters,
+  dispatch,
+  sdk,
+}: {
+  parameters: AppInstallationParameters;
+  dispatch: Dispatch<SetStateAction<any>>;
+  sdk: ConfigAppSDK;
+}) => {
+  useEffect(() => {
+    async function getContentTypes() {
+      const contentTypesResponse = await sdk.cma.contentType.getMany({});
+
+      if (contentTypesResponse.items.length) {
+        dispatch({
+          type: actions.UPDATE_CONTENT_TYPES,
+          payload: contentTypesResponse.items,
+        });
+      }
+    }
+
+    getContentTypes();
+  }, []);
+
+  const handleContentTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    dispatch({
+      type: actions.APPLY_SELECTED_CONTENT_TYPE,
+      payload: event.target.value,
+    });
+  };
+
+  return (
+    <Box style={styles.selectSection.select}>
+      <FormControl id="contentTypeSelect">
+        <FormControl.Label>Content Types</FormControl.Label>
+        <Select
+          id="contentTypeSelect"
+          name="contentTypeSelect"
+          value={parameters.selectedContentType}
+          onChange={handleContentTypeChange}>
+          {parameters.contentTypes && parameters.contentTypes.length ? (
+            <>
+              <Select.Option value="" isDisabled>
+                Please select a Content Type...
+              </Select.Option>
+              {parameters.contentTypes.map((contentType) => (
+                <Select.Option key={`option-${contentType.sys.id}`} value={contentType.sys.id}>
+                  {contentType.name}
+                </Select.Option>
+              ))}
+            </>
+          ) : (
+            <Select.Option value="">No Content Types currently configured.</Select.Option>
+          )}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default ContentTypeSelect;
---
+++ apps/vercel/src/components/config-screen/ProjectSelect.tsx
@@ -0,0 +1,67 @@
+import { ChangeEvent, Dispatch, SetStateAction, useEffect } from 'react';
+import { Box, FormControl, Select } from '@contentful/f36-components';
+import { AppInstallationParameters } from '../../types';
+import { actions } from '../parameterReducer';
+import VercelClient from '../../clients/Vercel';
+
+const ContentTypeSelect = ({
+  parameters,
+  dispatch,
+  client,
+}: {
+  parameters: AppInstallationParameters;
+  dispatch: Dispatch<SetStateAction<any>>;
+  client: VercelClient;
+}) => {
+  useEffect(() => {
+    async function getProjects() {
+      const data = await client.listProjects();
+
+      dispatch({
+        type: actions.UPDATE_VERCEL_PROJECTS,
+        payload: data.projects,
+      });
+    }
+
+    if (parameters && parameters.vercelAccessToken) {
+      getProjects();
+    }
+  }, [parameters.vercelAccessToken]);
+
+  const handleProjectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    dispatch({
+      type: actions.APPLY_SELECTED_PROJECT,
+      payload: event.target.value,
+    });
+  };
+
+  return (
+    <Box>
+      <FormControl id="optionProjectSelect" isRequired={true}>
+        <FormControl.Label>Project</FormControl.Label>
+        <Select
+          id="optionProjectSelect"
+          name="optionProjectSelect"
+          value={parameters.selectedProject}
+          onChange={handleProjectChange}>
+          {parameters.projects && parameters.projects.length ? (
+            <>
+              <Select.Option value="" isDisabled>
+                Please select a project...
+              </Select.Option>
+              {parameters.projects.map((project) => (
+                <Select.Option key={`option-${project.id}`} value={project.id}>
+                  {project.name}
+                </Select.Option>
+              ))}
+            </>
+          ) : (
+            <Select.Option value="">No Projects currently configured.</Select.Option>
+          )}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default ContentTypeSelect;
