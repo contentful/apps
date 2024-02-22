8289e777927a568b18041f193b2a43380f045b92
Matt Gordon <matt.gordon@contentful.com>
2024-02-22 11:28:29 -0700

    [WIP] fix broken import reference

--- apps/vercel/src/clients/Vercel.spec.ts
+++ apps/vercel/src/clients/Vercel.spec.ts
@@ -1,5 +1,5 @@
 import { Mock, afterAll, afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-import VercelClient from './vercel';
+import VercelClient from './Vercel';
 
 global.fetch = vi.fn();
 
