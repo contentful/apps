2b694e72ff63d9101e0909bd2ae7e14df51bd43b
Ethan Ozelius <ethan.ozelius@contentful.com>
2024-02-28 16:40:14 -0700

    feat(microsoft-teams): batch request to retrieve content types.  This will enable pagination or retrieving all records for spaces with 100+ contentTypes

--- apps/microsoft-teams/frontend/src/hooks/useGetContentTypes.spec.ts
+++ apps/microsoft-teams/frontend/src/hooks/useGetContentTypes.spec.ts
@@ -3,6 +3,10 @@ import { describe, expect, it, vi } from 'vitest';
 import useGetContentTypes from './useGetContentTypes';
 import { mockCma, mockSdk, mockGetManyContentType } from '@test/mocks';
 
+vi.mock('@constants/contentTypeLimit', () => ({
+  CONTENT_TYPE_LIMIT: 1,
+}));
+
 vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
   useCMA: () => mockCma,
@@ -16,6 +20,8 @@ describe('useGetContentTypes', () => {
       expect(result.current.contentTypes).toEqual(mockGetManyContentType.items);
       expect(result.current.loading).toEqual(false);
     });
+
+    vi.restoreAllMocks();
   });
 
   it('should return generalized error if error is thrown', async () => {
@@ -26,5 +32,21 @@ describe('useGetContentTypes', () => {
       expect(result.current.error?.message).toEqual('Unable to get content types');
       expect(result.current.loading).toEqual(false);
     });
+
+    vi.restoreAllMocks();
+  });
+
+  it('batches requests if the number of available content types exceeds the request limit', async () => {
+    const getManyResponse = { ...mockGetManyContentType, total: 2 };
+    getManyResponse.total = 2;
+    const getManySpy = (mockSdk.cma.contentType.getMany = vi.fn().mockReturnValue(getManyResponse));
+
+    renderHook(() => useGetContentTypes());
+
+    await waitFor(() => {
+      expect(getManySpy).toHaveBeenCalledTimes(2);
+    });
+
+    vi.restoreAllMocks();
   });
 });
