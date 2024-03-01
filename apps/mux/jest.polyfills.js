600483cc014334acdfee6bb2a0fb4d1b2b9d48c5
Jim Dalton <jim.dalton@gmail.com>
2024-02-29 16:51:02 -0800

    fix lint error

--- apps/mux/jest.polyfills.js
+++ apps/mux/jest.polyfills.js
@@ -1,5 +1,7 @@
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const { TextDecoder, TextEncoder } = require('node:util');
 
+/* eslint-disable-next-line no-undef */
 Object.defineProperties(globalThis, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
