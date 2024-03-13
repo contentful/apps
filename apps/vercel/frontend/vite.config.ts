6f8190b80233a80f26e13f461a47cd86152f251e
Jim Dalton <jim.dalton@gmail.com>
2024-03-13 10:27:02 -0700

    update vercel build directory

--- apps/vercel/frontend/vite.config.ts
+++ apps/vercel/frontend/vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig(() => ({
   server: {
     port: 3000,
   },
+  build: {
+    // the parent project that combines both the frontend and the hosted app action backend needs to be able to override
+    // the default location to its own build path
+    outDir: process.env.BUILD_PATH || './build',
+  },
   plugins: [react()],
   test: {
     environment: 'happy-dom',
