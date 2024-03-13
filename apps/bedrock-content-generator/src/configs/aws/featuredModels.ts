60bd23a904d301f0ddc6b91ef93e6cce8ba65dc5
Malte Janßen <maltjan@amazon.de>
2024-03-13 14:05:16 +0100

    add mistral mixtral and claude v3, fix claude prompt response

--- apps/bedrock-content-generator/src/configs/aws/featuredModels.ts
+++ apps/bedrock-content-generator/src/configs/aws/featuredModels.ts
@@ -8,13 +8,13 @@ export interface BedrockModel {
     prompt: string,
     maxTokens?: number
   ) => InvokeModelCommandInput;
-  outputKey: string;
+  parseResponse: (response: any) => string;
 }
 
 class ClaudeModel implements BedrockModel {
   id: string;
   name: string;
-  outputKey = 'completion';
+
   constructor(id: string, name: string) {
     this.id = id;
     this.name = name;
@@ -27,8 +27,7 @@ Make sure you follow the exact requirements in the input.
 
 Human: ${prompt}
 
-Assistant: Here is a creative response:
-`;
+Assistant:`;
 
     return {
       modelId: this.id,
@@ -40,12 +39,16 @@ Assistant: Here is a creative response:
       }),
     };
   }
+
+  parseResponse(response: { completion: string }) {
+    return response.completion;
+  }
 }
 
 class LlamaModel implements BedrockModel {
   id: string;
   name: string;
-  outputKey = 'generation';
+
   constructor(id: string, name: string) {
     this.id = id;
     this.name = name;
@@ -56,8 +59,7 @@ ${systemPrompt}
 
 Human: ${prompt}
 
-Assistant:
-`;
+Assistant:`;
 
     return {
       modelId: this.id,
@@ -68,12 +70,46 @@ Assistant:
       }),
     };
   }
+
+  parseResponse(response: { generation: string }) {
+    return response.generation;
+  }
+}
+
+class MistralModel implements BedrockModel {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  invokeCommand(systemPrompt: string, prompt: string, maxTokens?: number): InvokeModelCommandInput {
+    const completePrompt = `<s>[INST] ${systemPrompt} [/INST]
+[INST] ${prompt} [/INST]`;
+
+    return {
+      modelId: this.id,
+      contentType: 'application/json',
+      body: JSON.stringify({
+        prompt: completePrompt,
+        ...(maxTokens && { max_tokens: maxTokens }),
+      }),
+    };
+  }
+
+  parseResponse(response: { outputs: { text: string }[] }) {
+    return response['outputs'][0]['text'];
+  }
 }
 
 export const featuredModels: BedrockModel[] = [
+  new ClaudeModel('anthropic.claude-3-sonnet-20240229-v1:0', 'Anthropic Claude v3 Sonnet'),
   new ClaudeModel('anthropic.claude-v2:1', 'Anthropic Claude v2.1'),
   new ClaudeModel('anthropic.claude-instant-v1', 'Anthropic Claude Instant v1.2'),
   new LlamaModel('meta.llama2-70b-chat-v1', 'Meta Llama 2 70B'),
+  new MistralModel('mistral.mixtral-8x7b-instruct-v0:1', 'Mistral Mixtral 8x7B'),
 ];
 
 export const defaultModelId = featuredModels[0].id;
--- apps/bedrock-content-generator/src/configs/prompts/titlePrompt.ts
+++ apps/bedrock-content-generator/src/configs/prompts/titlePrompt.ts
@@ -1,4 +1,4 @@
 const titlePrompt = (blog: string) =>
-  `Suggest a short one-liner for a written blog post in the language defined. Output ONLY the title, nothing else. Only output content in the requested language. Here is the blog post: '${blog}'`;
+  `Suggest a short one-liner for a written blog post in the language defined. Output ONLY the title, nothing else, not even quotation marks. Only output content in the requested language. Here is the blog post: '${blog}'`;
 
 export default titlePrompt;
