62c059ce5202cbd3b23e9f66ff8d3f4db174a163
Lillian Bitner <lillian.bitner@contentful.com>
2024-03-29 13:56:08 -0600

    chore: fix defaultParams

--- apps/vercel/frontend/src/constants/defaultParams.ts
+++ apps/vercel/frontend/src/constants/defaultParams.ts
@@ -3,8 +3,6 @@ import { AppInstallationParameters } from '../types';
 export const initialParameters: AppInstallationParameters = {
   vercelAccessToken: '',
   vercelAccessTokenStatus: null,
-  projects: [],
-  contentTypes: [],
   selectedProject: '',
   contentTypePreviewPathSelections: [],
 };
