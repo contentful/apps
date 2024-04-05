9562f869d8b3946a5cb11b5e10e34cdce57e1328
Jim Dalton <jim.dalton@gmail.com>
2024-04-05 07:38:59 -0700

    fix: don't include configuration if preview path not specified

--- apps/vercel/app-actions/src/helpers/build-preview-urls-for-content-types.spec.ts
+++ apps/vercel/app-actions/src/helpers/build-preview-urls-for-content-types.spec.ts
@@ -12,4 +12,19 @@ describe('buildPreviewUrlsForContentTypes', () => {
       'https://team-integrations-vercel-playground-gqmys2z3c.vercel.app/api/enable-draft?x-vercel-protection-bypass=ukkdTdqAgnG5DQHwFkIeQ22N1nUDWeU7&path=%2Fblogs%2F{entry.fields.slug}'
     );
   });
+
+  describe('when a contenTypePreviewPath previewPath is empty', () => {
+    const contentTypePreviewPathsWithEmpty = [
+      ...contentTypePreviewPaths,
+      { contentType: 'articles', previewPath: ' ' },
+    ];
+
+    it('does not include the empty path', async () => {
+      const result = buildPreviewUrlsForContentTypes(
+        vercelProject,
+        contentTypePreviewPathsWithEmpty
+      );
+      expect(result['articles']).to.be.undefined;
+    });
+  });
 });
