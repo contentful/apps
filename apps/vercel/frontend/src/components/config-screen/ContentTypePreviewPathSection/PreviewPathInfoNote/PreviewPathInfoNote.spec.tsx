8083301272ed15715ebe2a68185b7c1226267292
Lillian Bitner <lillian.bitner@contentful.com>
2024-04-09 14:56:50 -0600

    fix: test

--- apps/vercel/frontend/src/components/config-screen/ContentTypePreviewPathSection/PreviewPathInfoNote/PreviewPathInfoNote.spec.tsx
+++ apps/vercel/frontend/src/components/config-screen/ContentTypePreviewPathSection/PreviewPathInfoNote/PreviewPathInfoNote.spec.tsx
@@ -2,16 +2,18 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import { PreviewPathInfoNote } from './PreviewPathInfoNote';
+import { copies } from '@constants/copies';
 
 describe('PreviewPathInfoNote', () => {
+  const { description, link } = copies.configPage.contentTypePreviewPathSection.infoNote;
   it('renders content', () => {
     render(<PreviewPathInfoNote />);
-    const text = screen.getByText('Preview path and token example:');
+    const text = screen.getByText(description);
     const example = screen.getByTestId('info-box');
-    const link = screen.getByText('View more examples');
+    const textLink = screen.getByText(link.copy);
 
     expect(text).toBeTruthy();
     expect(example).toBeTruthy();
-    expect(link).toBeTruthy();
+    expect(textLink).toBeTruthy();
   });
 });
