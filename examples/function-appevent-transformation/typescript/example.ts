d9792acdd682e15b7aa48c2ac21a5dd4488104f7
Mitch Goudy <mitchell.goudy@contentful.com>
2024-04-10 14:09:43 -0600

    feat: transformation example

--- examples/function-appevent-transformation/typescript/example.ts
+++ examples/function-appevent-transformation/typescript/example.ts
@@ -1,15 +1,50 @@
 import { FunctionEventHandler as EventHandler } from '@contentful/node-apps-toolkit';
+import https from 'https';
 
-const appEventFilteringHandler: EventHandler<'appevent.filter'> = (event, context) => {
-  // Filter out all events that are not of type 'Entry' (Assets, etc.)
-  return {
-    result: event.entityType === 'Entry',
-  };
+const appEventTransformationingHandler: EventHandler<'appevent.transformation'> = (
+  event,
+  context
+) => {
+  // If event is an entry, get lat and long fields and geocode them
+  if (event.entityType === 'Entry') {
+    const lat = event.entityProps.fields.lat;
+    const long = event.entityProps.fields.long;
+    if (lat && long) {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=YOUR_API_KEY`;
+
+      https
+        .get(url, (response) => {
+          let data = '';
+
+          response.on('data', (chunk) => {
+            data += chunk;
+          });
+
+          response.on('end', () => {
+            const result = JSON.parse(data);
+            console.log(result);
+
+            if (result.results.length > 0) {
+              const address = result.results[0].formatted_address;
+              console.log(`Geocoded address: ${address}`);
+            } else {
+              console.log('No results found.');
+            }
+          });
+        })
+        .on('error', (error) => {
+          console.error('Error:', error.message);
+        });
+    }
+  } else {
+    // If event is not an entry, throw an error
+    throw new Error('Event is not an Entry');
+  }
 };
 
 export const handler: EventHandler = (event, context) => {
-  if (event.type === 'appevent.filter') {
-    return appEventFilteringHandler(event, context);
+  if (event.type === 'appevent.transformation') {
+    return appEventTransformationingHandler(event, context);
   }
   throw new Error('Unknown Event');
 };
