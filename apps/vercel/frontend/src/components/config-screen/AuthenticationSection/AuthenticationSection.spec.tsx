87109a7eb205a1b0a335d3eb7dbd5968dddfcd6e
Lillian Bitner <lillian.bitner@contentful.com>
2024-04-11 14:44:53 -0600

    chore: fix tests

--- apps/vercel/frontend/src/components/config-screen/AuthenticationSection/AuthenticationSection.spec.tsx
+++ apps/vercel/frontend/src/components/config-screen/AuthenticationSection/AuthenticationSection.spec.tsx
@@ -21,14 +21,12 @@ describe('AuthenticationSection', () => {
       />
     );
 
-    const status = screen.getByText(invalidTokenMessage);
     const tokenInput = screen.getByPlaceholderText(input.placeholder);
 
-    expect(status).toBeTruthy();
     expect(tokenInput).toBeTruthy();
   });
 
-  it('renders hidden token and valid content when token is valid and app is installed', () => {
+  it('renders hidden token and valid content when token is valid', () => {
     const parameters = {
       vercelAccessToken: '12345',
       vercelAccessTokenStatus: 'valid',
@@ -39,6 +37,26 @@ describe('AuthenticationSection', () => {
 
     const token = screen.queryByText(parameters.vercelAccessToken);
 
+    expect(token).toBeFalsy();
+  });
+
+  it('renders hidden token and invalid content when token is invalid', () => {
+    const parameters = {
+      vercelAccessToken: '12345',
+      vercelAccessTokenStatus: 'valid',
+    } as unknown as AppInstallationParameters;
+    render(
+      <AuthenticationSection
+        handleTokenChange={vi.fn()}
+        parameters={parameters}
+        isTokenValid={false}
+      />
+    );
+
+    const status = screen.getByText(invalidTokenMessage);
+    const token = screen.queryByText(parameters.vercelAccessToken);
+
+    expect(status).toBeTruthy();
     expect(token).toBeFalsy();
   });
 });
