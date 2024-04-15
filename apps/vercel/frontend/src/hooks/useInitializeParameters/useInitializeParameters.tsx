5b6c7e7dfa8bdc8f1335a52650bfe69ff48e88e1
Lillian Bitner <lillian.bitner@contentful.com>
2024-04-15 13:12:30 -0600

    fix: broken import

--- apps/vercel/frontend/src/hooks/useInitializeParameters/useInitializeParameters.tsx
+++ apps/vercel/frontend/src/hooks/useInitializeParameters/useInitializeParameters.tsx
@@ -2,7 +2,8 @@ import { Dispatch, useEffect, useCallback } from 'react';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import type { ConfigAppSDK } from '@contentful/app-sdk';
 import { AppInstallationParameters } from '@customTypes/configPage';
-import { ParameterAction, actions } from '@components/parameterReducer';
+import { ParameterAction } from '@components/parameterReducer';
+import { actions } from '@constants/enums';
 
 /**
  * This hook is used to initialize the parameters of the app.
