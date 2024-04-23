b89fb23791a1f303ccac1a0da03baa2a1db295ff
Lisa White <lisa.white@contentful.com>
2024-04-23 12:52:46 -0600

    feat: add deeplink to install MS Teams app

---
+++ apps/microsoft-teams/frontend/src/constants/msTeamsAppId.ts
@@ -0,0 +1,5 @@
+/**
+ * @description - This is the App Id from our app manifest for our
+ * Microsoft Teams app that is published in the Teams App Store.
+ */
+export const MS_TEAMS_APP_ID = 'e4e4e40e-18a1-48e7-becf-49bb3f348e13';
