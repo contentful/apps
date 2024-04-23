bdf544317c54352ab3f6f64b31eef1c0a83cddb3
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2024-04-23 16:59:25 +0200

    style character details component

--- examples/function-potterdb-rest-api/src/components/CharacterDetails.tsx
+++ examples/function-potterdb-rest-api/src/components/CharacterDetails.tsx
@@ -8,6 +8,7 @@ import {
   Skeleton,
   Box,
   Paragraph,
+  Caption,
 } from '@contentful/f36-components';
 import tokens from '@contentful/f36-tokens';
 import { css } from 'emotion';
@@ -52,23 +53,50 @@ export function CharacterDetails({ character, onClick, ctaText = 'Select' }: Pro
     <Flex flexDirection="column">
       <Flex>
         <img src={character?.image} alt={character?.name} className={styles.image} />
-        <Flex marginLeft={'spacingM'} flexDirection='column'>
+        <Flex marginLeft={'spacingM'} flexDirection="column">
           <Flex alignItems="center">
             <Subheading className={styles.title}>{character.name}</Subheading>
           </Flex>
           <Flex justifyContent="space-between">
             <Flex flexDirection="column">
-              <Paragraph>Born: {character?.born?.slice(0, 25)  ?? '??'}</Paragraph>
-              <Paragraph>Gender: {character.gender ?? '??'}</Paragraph>
+              <Flex>
+                <Caption fontWeight="fontWeightMedium" marginRight='spacingS'>Born:</Caption>{' '}
+                <Caption>{character?.born?.slice(0, 25) ?? '??'}</Caption>
+              </Flex>
+              <Flex>
+                <Caption fontWeight="fontWeightMedium" marginRight='spacingS'>Nationality:</Caption>{' '}
+                <Caption>{character?.nationality?? '??'}</Caption>
+              </Flex>
+              <Flex>
+                <Caption fontWeight="fontWeightMedium" marginRight='spacingS'>Gender:</Caption>{' '}
+                <Caption>{character.gender ?? '??'}</Caption>
+              </Flex>
             </Flex>
             <Flex flexDirection="column" marginLeft={'spacingM'}>
-              <Paragraph>House: {character.house ?? '??'}</Paragraph>
-              <Paragraph>Species: {character.species ?? '??'}</Paragraph>
+              <Flex>
+                <Caption fontWeight="fontWeightMedium" marginRight='spacingS'>House:</Caption>{' '}
+                <Caption>{character?.house ?? '??'}</Caption>
+              </Flex>
+              <Flex>
+                <Caption fontWeight="fontWeightMedium" marginRight='spacingS'> Species:</Caption>{' '}
+                <Caption>{character.species ?? '??'}</Caption>
+              </Flex>
+              <Flex>
+                <Caption fontWeight="fontWeightMedium" marginRight='spacingS'> Titles:</Caption>{' '}
+                <Caption>{character.titles?.join(', ') ?? '??'}</Caption>
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
       </Flex>
-      {onClick && <Button style={{ marginTop: '20px', marginBottom: '20px', width: '90px' }} size="small" onClick={() => onClick(character)}>{ctaText}</Button>}
+      {onClick && (
+        <Button
+          style={{ marginTop: '20px', marginBottom: '20px', width: '90px' }}
+          size="small"
+          onClick={() => onClick(character)}>
+          {ctaText}
+        </Button>
+      )}
     </Flex>
   );
 }
