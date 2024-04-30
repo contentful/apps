bfaff38c37674281bb10b2e3f4a1eeb905833014
Lillian Bitner <lillian.bitner@contentful.com>
2024-04-30 07:56:42 -0600

    fix: useError tests

--- apps/vercel/frontend/src/hooks/useError/useError.spec.tsx
+++ apps/vercel/frontend/src/hooks/useError/useError.spec.tsx
@@ -2,26 +2,32 @@ import { renderHook } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { useError } from './useError';
 import { errorMessages } from '@constants/errorMessages';
+import { renderConfigPageHook as wrapper } from '@test/helpers/renderConfigPageHook';
 
 describe('useError', () => {
   it('should return correct error message and error status when error exists', async () => {
     const authError = { invalidToken: true, invalidTeamScope: false, expiredToken: false };
-
-    renderHook(() => {
-      const { isError, message } = useError({ error: authError });
-      expect(message).toBe(errorMessages.invalidToken);
-      expect(isError).toBe(true);
-    });
+    renderHook(
+      () => {
+        const { isError, message } = useError({ error: authError });
+        expect(message).toBe(errorMessages.invalidToken);
+        expect(isError).toBe(true);
+      },
+      { wrapper }
+    );
   });
 
   it('should return no error message and false error status when error does not exist', async () => {
     const authError = { invalidToken: false, invalidTeamScope: false, expiredToken: false };
 
-    renderHook(() => {
-      const { isError, message } = useError({ error: authError });
-      expect(message).toBeFalsy();
-      expect(isError).toBe(false);
-    });
+    renderHook(
+      () => {
+        const { isError, message } = useError({ error: authError });
+        expect(message).toBeFalsy();
+        expect(isError).toBe(false);
+      },
+      { wrapper }
+    );
   });
 
   it('should return error message when error provided includes a contentType', async () => {
@@ -31,12 +37,15 @@ describe('useError', () => {
       emptyPreviewPathInput: true,
     };
 
-    renderHook(() => {
-      const { message } = useError({
-        error: previewPathError,
-        contentType: previewPathError.contentType,
-      });
-      expect(message).toBe(errorMessages.emptyPreviewPathInput);
-    });
+    renderHook(
+      () => {
+        const { message } = useError({
+          error: previewPathError,
+          contentType: previewPathError.contentType,
+        });
+        expect(message).toBe(errorMessages.emptyPreviewPathInput);
+      },
+      { wrapper }
+    );
   });
 });
