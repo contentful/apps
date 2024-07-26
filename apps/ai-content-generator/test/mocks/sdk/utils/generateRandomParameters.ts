2a2ae7a39e53d44e6f2207d3a2bed8caeb345b0e
david shibley <david.shibley@contentful.com>
2024-07-26 10:55:40 -0700

    Fixing build

--- apps/ai-content-generator/test/mocks/sdk/utils/generateRandomParameters.ts
+++ apps/ai-content-generator/test/mocks/sdk/utils/generateRandomParameters.ts
@@ -1,13 +1,11 @@
 import AppInstallationParameters from '@components/config/appInstallationParameters';
-import { gptModels } from '@configs/ai/gptModels';
 
 const generateRandomParameters = (): AppInstallationParameters => {
-  const randomModelIndex = Math.floor(Math.random() * gptModels.length);
   const randomApiKey = window.btoa(Math.random().toString() + Math.random().toString).slice(0, 48);
   const randomProfile = Math.random().toString(36).substring(7);
 
   return {
-    model: gptModels[randomModelIndex].id,
+    model: 'gpt-4',
     key: 'sk-' + randomApiKey,
     profile: randomProfile,
     brandProfile: {},
