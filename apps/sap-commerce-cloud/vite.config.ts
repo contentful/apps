ad38310755182447fbb20761d4b0f42c7608a213
Sarah Lessner <sarah.lessner@contentful.com>
2024-08-13 15:05:43 -0600

    attempt to fix ci with config updates

--- apps/sap-commerce-cloud/vite.config.ts
+++ apps/sap-commerce-cloud/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
+  base: '', // relative paths
   server: {
     port: 3000,
   },
@@ -11,6 +12,6 @@ export default defineConfig(() => ({
   plugins: [react()],
   test: {
     environment: 'happy-dom',
-    setupFiles: ['./test/setup.ts'],
+    setupFiles: ['/src/setupTests.ts'],
   },
 }));
