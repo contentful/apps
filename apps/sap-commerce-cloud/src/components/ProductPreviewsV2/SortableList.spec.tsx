44329d57c5100dcb7060d863e21e0195a6f55657
Lisa White <lisa.white@contentful.com>
2024-08-13 10:54:04 -0600

    test: add tests for new components and update mocks

---
+++ apps/sap-commerce-cloud/src/components/ProductPreviewsV2/SortableList.spec.tsx
@@ -0,0 +1,23 @@
+import { render, cleanup, screen } from '@testing-library/react';
+import { Props, SortableList } from './SortableList';
+import { mockProductPreview } from '../../__mocks__';
+
+const defaultProps: Props = {
+  disabled: false,
+  productPreviews: [mockProductPreview],
+  deleteFn: jest.fn(),
+};
+
+const renderComponent = (props: Props) => {
+  return render(<SortableList {...props} />);
+};
+
+describe('SortableList', () => {
+  afterEach(cleanup);
+
+  it('should render successfully', async () => {
+    const { queryAllByTestId } = renderComponent(defaultProps);
+
+    expect(queryAllByTestId(`sortable-list-item`)).toHaveLength(1);
+  });
+});
