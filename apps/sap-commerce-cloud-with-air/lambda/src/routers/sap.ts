b84b615c3f4482b2b88a74eb51dc05838b4901c1
Mitch Goudy <mgoudy91@gmail.com>
2024-08-15 10:40:16 -0600

    refactor: double deployment script [ext-5484] (#8555)
    * refactor: double deployment script
    
    * BREAKING CHANGE: removing air app

--- apps/sap-commerce-cloud-with-air/lambda/src/routers/sap.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProductList, getProductPreview } from '../controllers';
-
-const router = Router();
-
-router.get('/product-list', getProductList);
-router.get('/product-preview', getProductPreview);
-
-export default router;
