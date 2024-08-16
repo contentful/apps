ea954e519899a90bd8b0151d55d3b4c3d4850db5
Lisa White <lisa.white@contentful.com>
2024-08-16 14:06:58 -0600

    feat: add hosted app actions for sap [EXT-5612] (#8603)
    * feat: reorganize app to introduce haa
    
    * chore: add more template files to app actions and remove deploy scripts from frontend

---
+++ apps/sap-commerce-cloud/app-actions/src/types.ts
@@ -0,0 +1,51 @@
+export interface Product {
+  sku: string;
+  image: string;
+  id: string;
+  name: string;
+  externalLink?: string;
+  isMissing?: boolean;
+  productUrl: string;
+}
+
+export type Hash = Record<string, any>;
+
+export type FieldsConfig = Record<string, Record<string, string | undefined> | undefined>;
+
+export interface ConfigurationParameters {
+  projectKey?: string;
+  clientId?: string;
+  clientSecret?: string;
+  apiEndpoint?: string;
+  authApiEndpoint?: string;
+  locale?: string;
+  fieldsConfig?: FieldsConfig;
+  baseSites?: string;
+}
+
+export interface ActionError {
+  type: string;
+  message: string;
+  details?: Record<string, any>;
+}
+
+export interface AppActionCallResponseSuccess<TResult> {
+  ok: true;
+  data: TResult;
+}
+
+export interface AppActionCallResponseError {
+  ok: false;
+  error: ActionError;
+}
+
+export type AppActionCallResponse<T> = AppActionCallResponseSuccess<T> | AppActionCallResponseError;
+
+// WIP types
+export interface BaseSite {
+  channel: string;
+  name: string;
+  uid: string;
+}
+
+export type BaseSites = BaseSite[];
