f3dc53bde38a5060cab9a016998c04769145b363
Tyler Collins <tyler.collins@contentful.com>
2024-08-20 16:23:58 -0600

    fix: (Typeform) use updated preview embed component

--- apps/typeform/frontend/src/TypeFormWidget.tsx
+++ apps/typeform/frontend/src/TypeFormWidget.tsx
@@ -1,33 +1,28 @@
-import React, { useEffect, useRef } from 'react';
-import * as typeformEmbed from '@typeform/embed';
-import { DialogExtensionSDK } from '@contentful/app-sdk';
+import React, { useEffect } from 'react';
+import { DialogAppSDK } from '@contentful/app-sdk';
 import { Hash } from './typings';
 import { SDK_WINDOW_HEIGHT } from './constants';
+import { Widget } from '@typeform/embed-react';
 
 interface Props {
-  sdk: DialogExtensionSDK;
+  sdk: DialogAppSDK;
 }
 
 export function TypeformPreviewWidget({ sdk }: Props) {
-  const el = useRef(null);
   const { value } = sdk.parameters.invocation as Hash;
+  const formId = value.split('/').pop();
+  console.log('formId', formId);
   useEffect(() => {
-    let element = el.current;
-    typeformEmbed.makeWidget(element, value, {
-      hideFooter: true,
-      hideHeaders: true,
-      opacity: 0,
-    });
     sdk.window.updateHeight(SDK_WINDOW_HEIGHT);
-  }, [value, sdk.window]);
+  }, [sdk.window]);
 
   return (
-    <div
-      ref={el}
-      style={{
-        width: '100%',
-        height: SDK_WINDOW_HEIGHT,
-      }}
+    <Widget
+      id={formId}
+      style={{ width: '100%', height: SDK_WINDOW_HEIGHT }}
+      opacity={0}
+      hideFooter={true}
+      hideHeaders={true}
     />
   );
 }
