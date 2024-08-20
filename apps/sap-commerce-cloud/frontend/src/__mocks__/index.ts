edec0cd4a162f484a8edb454f960b63b4994a838
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-08-20 13:49:26 -0600

    feat:adding tests [EXT-5600] (#8606)
    * Feat: adding tests

--- apps/sap-commerce-cloud/frontend/src/__mocks__/index.ts
+++ apps/sap-commerce-cloud/frontend/src/__mocks__/index.ts
@@ -1,2 +1,5 @@
 export { makeSdkMock } from './mockSdk';
 export { mockProductPreview } from './mockProductPreview';
+export { mockBaseSite, mockApiEndpoint } from './endpoints';
+export { mockFetch } from './mockFetch';
+export { mockContentTypes } from './mockContentTypes';
---
+++ apps/sap-commerce-cloud/frontend/src/index.spec.tsx
@@ -0,0 +1,55 @@
+import { vi } from 'vitest';
+import { locations } from '@contentful/app-sdk';
+import AppConfig from './AppConfig/AppConfig';
+
+let renderMock: any;
+
+vi.mock('@contentful/app-sdk', () => ({
+  init: vi.fn((cb) => {
+    const sdk = {
+      location: {
+        is: vi.fn((location) => location === mockLocation),
+      },
+    };
+    cb(sdk);
+  }),
+  locations: {
+    LOCATION_APP_CONFIG: 'app-config',
+    LOCATION_ENTRY_FIELD: 'entry-field',
+    LOCATION_DIALOG: 'dialog',
+  },
+}));
+
+let mockLocation: any = locations.LOCATION_APP_CONFIG;
+
+document.body.innerHTML = '<div id="root"></div>';
+
+describe('index.tsx', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    mockLocation = locations.LOCATION_APP_CONFIG;
+
+    renderMock = vi.fn();
+    vi.doMock('react-dom/client', () => ({
+      createRoot: vi.fn().mockReturnValue({
+        render: renderMock,
+      }),
+    }));
+  });
+
+  it('renders AppConfig component when location is LOCATION_APP_CONFIG', async () => {
+    await import('./index');
+
+    expect(renderMock).toHaveBeenCalledWith(
+      <AppConfig
+        sdk={expect.anything()}
+        name="SAP Commerce Cloud App"
+        description={expect.any(String)}
+        logo={expect.any(String)}
+        color={expect.any(String)}
+        parameterDefinitions={expect.any(Array)}
+        validateParameters={expect.any(Function)}
+      />
+    );
+  });
+});
