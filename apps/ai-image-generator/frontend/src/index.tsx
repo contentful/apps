fe57fa454b1673cf4b87d9bad718e89230310254
Mitch Goudy <mitchell.goudy@contentful.com>
2024-08-21 15:45:16 -0600

    fix: linting configs

--- apps/ai-image-generator/frontend/src/index.tsx
+++ apps/ai-image-generator/frontend/src/index.tsx
@@ -4,7 +4,7 @@ import { SDKProvider } from '@contentful/react-apps-toolkit';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') ?? document.createElement('div');
 const root = createRoot(container);
 
 root.render(
