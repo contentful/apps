cc61a4e25cf3719f5ac728a93ed436210183b99a
Mitch Goudy <mitchell.goudy@contentful.com>
2024-08-21 15:44:57 -0600

    feat: ms teams linting

---
+++ apps/microsoft-teams/frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    require.resolve('../../../ally.js'),
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
