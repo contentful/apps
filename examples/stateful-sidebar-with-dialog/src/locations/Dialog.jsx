ab1625e4167a95bf3914242afcbbec7d2b34d50f
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-09-09 12:08:38 -0600

    Update examples/stateful-sidebar-with-dialog/src/locations/Dialog.jsx

--- examples/stateful-sidebar-with-dialog/src/locations/Dialog.jsx
+++ examples/stateful-sidebar-with-dialog/src/locations/Dialog.jsx
@@ -5,7 +5,7 @@ import { reducer } from '../model/reducerAndInitialSidebarState';
 
 const Dialog = () => {
   const sdk = useSDK();
-  const { serializedSidebarState } = (sdk.parameters.invocation || {});
+  const { serializedSidebarState } = sdk.parameters.invocation || {};
   const [state, dispatch] = useReducer(reducer, {});
   /*
      To use the cma, inject it as follows.
@@ -20,20 +20,28 @@ const Dialog = () => {
     });
   }, []);
 
-  return <>
-    <Paragraph>Hello Dialog Component (AppId: {sdk.ids.app})</Paragraph>
-    <Paragraph>{`Time is ${state.timestamp}`}</Paragraph>
-    <Button onClick={() => {
-      dispatch({
-        type: 'SET_TIME',
-        payload: new Date().toString(),
-      });
-    }}>SET TIME</Button>
-    <Button
-      onClick={() => {
-        sdk.close(JSON.stringify(state));
-      }}>CLOSE</Button>
-    </>;
+  return (
+    <>
+      <Paragraph>Hello Dialog Component (AppId: {sdk.ids.app})</Paragraph>
+      <Paragraph>{`Time is ${state.timestamp}`}</Paragraph>
+      <Button
+        onClick={() => {
+          dispatch({
+            type: 'SET_TIME',
+            payload: new Date().toString(),
+          });
+        }}>
+        SET TIME
+      </Button>
+      <Button
+        onClick={() => {
+          sdk.close(JSON.stringify(state));
+        }}>
+        CLOSE
+      </Button>
+    </>
+  );
 };
 
 export default Dialog;
+
