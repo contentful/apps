3e40e2c843f07228e0a47d1d48bad13333545601
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-09-16 09:05:07 -0600

    Setting seo limit to 100 (#8900)

--- apps/ai-content-generator/src/configs/prompts/seoDescriptionPrompt.ts
+++ apps/ai-content-generator/src/configs/prompts/seoDescriptionPrompt.ts
@@ -1,4 +1,4 @@
 const seoDescriptionPrompt = (prompt: string) =>
-  `Write an optimized SEO description with max 150 characters for the following text: "${prompt}"`;
+  `Write an optimized SEO description with max 100 characters for the following text: "${prompt}"`;
 
 export default seoDescriptionPrompt;
