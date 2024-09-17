abc85d2d49fc717242fa43b1a3e2bd60964f1d6d
david shibley <david.shibley@contentful.com>
2024-09-17 11:00:37 -0600

    Creating package eslint-plugin-contentful-apps

---
+++ packages/eslint-plugin-contentful-apps/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf8'));
+
+module.exports = {
+  plugins: ['jsx-a11y'],
+  rules: {
+    'jsx-a11y/no-autofocus': 'error',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
+    'jsx-a11y/no-noninteractive-tabindex': 'warn',
+    'jsx-a11y/tabindex-no-positive': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/interactive-supports-focus': 'warn',
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/label-has-associated-control': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+  },
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+  },
+};
