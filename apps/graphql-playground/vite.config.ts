4629e0079da7fcaaf1a463d916d55d640f52ad57
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-09-25 12:03:24 -0600

    chore: remove vulnerable dependencies from graphql-playground (#8965)
    * chore: remove vulnerable dependencies from graphql-playground
    
    * Fixing build
    
    * adding global definition
    
    * fixing entry for build

---
+++ apps/graphql-playground/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'graphql-playground',
+    },
+  },
+  define: {
+    global: {},
+  },
+});
