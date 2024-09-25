71172a011bc6e6481ad6d1f037a80d7291b035dd
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-09-25 13:19:57 -0600

    Revert "Chore/vuln deps image focal point [EXT-5794] (#8953)" (#8966)
    This reverts commit 77093cfd02c9e1bf62000876fa91799577cdb3d9.

--- apps/image-focal-point/src/index.spec.js
+++ apps/image-focal-point/src/index.spec.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { App } from './index';
 import { render, cleanup, configure } from '@testing-library/react';
-import { vi } from 'vitest';
 
 import mockProps from './test/mockProps';
 
 const sdk = {
   ...mockProps.sdk,
   field: {
-    getValue: vi.fn(),
-    onValueChanged: vi.fn(),
-    setValue: vi.fn(),
-    removeValue: vi.fn(),
+    getValue: jest.fn(),
+    onValueChanged: jest.fn(),
+    setValue: jest.fn(),
+    removeValue: jest.fn(),
   },
   window: {
-    startAutoResizer: vi.fn(),
+    startAutoResizer: jest.fn(),
   },
 };
 
-vi.mock('./utils', () => ({
-  getField: vi.fn(),
+jest.mock('./utils', () => ({
+  getField: jest.fn(),
   isCompatibleImageField: () => true,
 }));
 
@@ -33,7 +32,7 @@ function renderComponent(sdk) {
 
 describe('App', () => {
   beforeEach(() => {
-    vi.resetAllMocks();
+    jest.resetAllMocks();
   });
 
   afterEach(cleanup);
