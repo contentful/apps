1ab256d0c635c4ce63056ad37e4ba1bc44d7a6c9
david shibley <david.shibley@contentful.com>
2024-09-26 14:29:36 -0600

    adding resolve alias to vite config

--- apps/dropbox/vite.config.js
+++ apps/dropbox/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@contentful/dam-app-base': './node_modules/@contentful/dam-app-base/lib/index.js',
+    },
+  },
   server: {
     port: 3000,
   },
