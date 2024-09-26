01d2209cb605a902debf28ba1f4ef146fa2f74fc
Sarah Lessner <sarah.lessner@contentful.com>
2024-09-26 11:58:59 -0600

    removes react-scripts and replaces with vite

---
+++ apps/optimizely/vite.config.js
@@ -0,0 +1,29 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => ({
+  base: '', // relative paths
+  server: {
+    port: 3000,
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.jsx?$/,
+    exclude: [],
+  },
+  define: {
+    'process.env': {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  plugins: [react()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+  },
+}));
