1fddc2cebe5247559408820e944e6703e5d46cf0
david shibley <david.shibley@contentful.com>
2024-09-26 14:12:57 -0600

    chore: remove vulnerable dependencies from dropbox

---
+++ apps/dropbox/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+    lib: {
+      entry: 'src/index.jsx',
+      name: 'dropbox',
+    },
+  },
+});
