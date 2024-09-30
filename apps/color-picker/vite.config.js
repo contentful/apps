5289cfb3549f09e26b0269da26d0fe0b6e144139
david shibley <david.shibley@contentful.com>
2024-09-30 10:29:46 -0600

    opting out of library mode

--- apps/color-picker/vite.config.js
+++ apps/color-picker/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
-import babel from 'vite-plugin-babel';
 
 export default defineConfig({
   server: {
@@ -10,22 +9,10 @@ export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ['defaults', 'not IE 11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
-    babel({
-      babelConfig: {
-        babelrc: false,
-        configFile: false,
-        plugins: ['@babel/plugin-transform-runtime'],
-      },
     }),
   ],
   build: {
     outDir: 'build',
-    lib: {
-      entry: 'src/index.tsx',
-      name: 'color-picker',
-    },
   },
 });
