c66728e2385c3a77a6080833ac38c6cefc9d036d
david shibley <david.shibley@contentful.com>
2024-09-30 14:30:15 -0600

    Got it working in staging

--- apps/json-viewer/vite.config.js
+++ apps/json-viewer/vite.config.js
@@ -1,14 +1,19 @@
+import { env } from 'process';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  base: './',
+  define: {
+    process: {
+      env: {
+        NODE_ENV: env.NODE_ENV,
+      },
+    },
+  },
   build: {
     outDir: 'build',
-    lib: {
-      entry: 'src/index.tsx',
-      name: 'json-viewer',
-    },
   },
 });
