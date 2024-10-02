a8bedd87a824d5bea7690f0d04b701274483731a
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-10-02 09:58:00 -0600

    Chore/vuln deps color picker (#9032)
    * chore: remove vulnerable dependencies from color picker app
    
    * opting out of library mode
    
    * Working on staging

--- apps/color-picker/src/index.tsx
+++ apps/color-picker/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import regeneratorRuntime from 'regenerator-runtime';
 import { GlobalStyles } from '@contentful/f36-components';
 import { SDKProvider } from '@contentful/react-apps-toolkit';
 
 import App from './App';
 import './index.css';
+
+window.regeneratorRuntime = regeneratorRuntime;
 
 const root = document.getElementById('root');
 
---
+++ apps/color-picker/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  base: './',
+  plugins: [
+    react(),
+    legacy({
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
+  build: {
+    outDir: 'build',
+  },
+});
