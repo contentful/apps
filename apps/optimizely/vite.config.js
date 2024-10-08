304a38d415444b508df5b4c4355387ee02916872
Sarah Lessner <sarah.lessner@contentful.com>
2024-10-08 14:09:41 -0600

    wip updating to modern react

--- apps/optimizely/vite.config.js
+++ apps/optimizely/vite.config.js
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 import { defineConfig } from 'vite';
 
@@ -9,9 +10,14 @@ export default defineConfig(() => ({
   },
 
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: [
+      {
+        find: /node_modules/,
+        replacement: path.resolve(__dirname, 'node_modules'),
+        // find: /@use-it\/interval/,
+        // replacement: path.resolve(__dirname, 'node_modules', '@use-it', 'interval', 'dist', 'index.js'),
+      },
+    ],
   },
   build: {
     minify: false,
@@ -20,8 +26,6 @@ export default defineConfig(() => ({
       // exclude: ['*/index.js', 'node_modules/@testing-library/dom/dist/@testing-library/dom.esm.js', 'node_modules/@sheerun/mutationobserver-shim/dist/mutationobserver.min.js',],
       include: ['node_modules/**'],
       exclude: ['node_modules/@testing-library/**'],
-
-      // include: ['node_modules/**', '**/index.js', '**/warning.js', '**/truncate.js', '**/dayjs.min.js', '**/utc.js', '**/relativeTime.js', '**/calendar.js', '**/react/jsx-runtime.js', '**/react-modal/lib/index.js', '**/lodash.get/index.js', '**/lodash.intersection/index.js', '**/use-methods/dist/index.js', '**/@testing-library/dom/dist/@testing-library/dom.esm.js', '**/pretty-format/build-es5/index.js'],
     },
     rollupOptions: {
       // treeshake: true,
@@ -29,7 +33,6 @@ export default defineConfig(() => ({
   },
   optimizeDeps: {
     esbuildOptions: {
-      include: [],
       //  exclude: ['node_modules/@testing-library/**']
       // loader: {
       //   '.js': 'jsx',
@@ -39,7 +42,6 @@ export default defineConfig(() => ({
   // build: {
   //   commonjsOptions: {
   //     exclude: [],
-  //     include: ['node_modules/pretty-format/build-es5/index.js','node_modules/@testing-library/dom/dist/@testing-library/dom.esm.js','node_modules/use-methods/dist/index.js','node_modules/lodash.intersection/index.js',"node_modules/lodash.get/index.js",'node_modules/react-modal/lib/index.js','node_modules/react/jsx-runtime.js','node_modules/dayjs/plugin/calendar.js','node_modules/dayjs/plugin/relativeTime.js','node_modules/dayjs/plugin/utc.js','node_modules/dayjs/dayjs.min.js','node_modules/truncate/truncate.js', 'node_modules/warning/warning.js','node_modules/react-fast-compare/index.js', 'node_modules/react/index.js', 'node_modules/@contentful/f36-tokens/dist/index.js', 'node_modules/react-dom/index.js', 'node_modules/prop-types/index.js', 'node_modules/react-is/index.js'],
   //   },
   // },
   // esbuild: {
