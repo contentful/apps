8f25e17b5205cd90c68a596aba4f32a4f7afba5c
david shibley <david.shibley@contentful.com>
2024-10-08 14:12:46 -0600

    chore: remove vulnerable dependencies from Jira app

--- apps/jira/jira-app/src/constants.ts
+++ apps/jira/jira-app/src/constants.ts
@@ -1,9 +1,15 @@
 let LAMBDA_URI = 'https://api.jira.ctfapps.net';
 let CLIENT_ID = 'XD9k9QU9VT4Rt26u6lbO3NM0fOqvvXan';
 
-if (process.env.NODE_ENV === 'development') {
-  LAMBDA_URI = `${process.env.REACT_APP_NGROK_URL}/test`;
-  CLIENT_ID = process.env.REACT_APP_ATLASSIAN_APP_CLIENT_ID || '';
+interface ImportMetaValue extends ImportMeta {
+  NODE_ENV?: string;
+  VITE_NGROK_URL?: string;
+  VITE_ATLASSIAN_APP_CLIENT_ID?: string;
+}
+
+if ((import.meta as ImportMetaValue).NODE_ENV === 'development') {
+  LAMBDA_URI = `${(import.meta as ImportMetaValue).VITE_NGROK_URL}/test`;
+  CLIENT_ID = (import.meta as ImportMetaValue).VITE_ATLASSIAN_APP_CLIENT_ID || '';
 }
 
 const constants = {
