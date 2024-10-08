74bf2c9909069afda64f7fe2cede1f05f16f4a08
david shibley <david.shibley@contentful.com>
2024-10-08 09:42:38 -0600

    reverting stack overflow change and adding optional dependencies

--- apps/image-focal-point/vite.config.ts
+++ apps/image-focal-point/vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
   optimizeDeps: {
     include: ['reactjsx-runtime', 'react-dom'],
   },
-  esbuild: {
-    target: 'esnext',
-    platform: 'node',
-  },
   build: {
     outDir: 'build',
   },
