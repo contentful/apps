a00176b7d93f72fb83c9659cbe6b7327ee5ae587
Lisa White <lisa.white@contentful.com>
2024-10-16 16:50:41 -0600

    chore: update app to use vite

--- examples/function-appaction/src/locations/Page.spec.tsx
+++ examples/function-appaction/src/locations/Page.spec.tsx
@@ -2,8 +2,9 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { mockCma, mockSdk } from '../../test/mocks';
 import Page from './Page';
 import { act } from 'react';
+import { vi } from 'vitest';
 
-jest.mock('@contentful/react-apps-toolkit', () => ({
+vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
   useCMA: () => mockCma,
   setAppActions: () => jest.fn(),
@@ -16,9 +17,7 @@ describe('Page component', () => {
     });
 
     await waitFor(() => {
-      expect(screen.getByText('App Actions')).toBeInTheDocument();
-      expect(screen.getByRole('button')).toBeInTheDocument();
-      expect(screen.getByRole('button')).toHaveTextContent('name');
+      expect(screen.getByText('App Actions Demo Console')).toBeDefined();
     });
   });
 });
