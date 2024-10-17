f7e298b32c5ef4f5581b599163f8e46911a71aa3
david shibley <david.shibley@contentful.com>
2024-10-17 15:34:45 -0600

    using dirname

--- apps/dropbox/vite.config.js
+++ apps/dropbox/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@contentful/dam-app-base': './node_modules/@contentful/dam-app-base/lib/index.js',
-      '@contentful/app-sdk': '../../app-sdk/dist/cf-extension-api.js',
+      '@contentful/dam-app-base': path.resolve(
+        __dirname,
+        './node_modules/@contentful/dam-app-base/lib/index.js'
+      ),
+      '@contentful/app-sdk': path.resolve(
+        __dirname,
+        './node_modules/@contentful/app-sdk/dist/cf-extension-api.js'
+      ),
     },
   },
   base: './',
