de6e615c56b337ad0949c54d8f8ade410a933227
Lisa White <lisa.white@contentful.com>
2024-10-22 11:26:46 -0600

    chore: remove unnecessary logs and files

--- examples/function-appaction/src/App.tsx
+++ examples/function-appaction/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { locations } from '@contentful/app-sdk';
 import Page from './locations/Page';
 import { useSDK } from '@contentful/react-apps-toolkit';
