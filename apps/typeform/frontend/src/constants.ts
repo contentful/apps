8e2a64d164b092b88ec702f11d9c2f341cb3fc0a
Mitch Goudy <mitchell.goudy@contentful.com>
2024-10-30 14:15:13 -0600

    fix: env process client id

--- apps/typeform/frontend/src/constants.ts
+++ apps/typeform/frontend/src/constants.ts
@@ -2,6 +2,6 @@ const SDK_WINDOW_HEIGHT = 450;
 const BASE_URL = 'https://api.typeform.com';
 
 // TODO Move to env var
-const CLIENT_ID = '7EugkySJjsYnSihDiYkGnMXrzbLBz7K8CFXS438Toqix';
+const CLIENT_ID = process.env.CLIENT_ID;
 
 export { SDK_WINDOW_HEIGHT, BASE_URL, CLIENT_ID };
