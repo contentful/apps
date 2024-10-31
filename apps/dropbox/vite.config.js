7aeec2567b79d3120027cdc053fd70beeb17fbd2
david shibley <david.shibley@contentful.com>
2024-10-31 15:24:08 -0600

    removing alias

--- apps/dropbox/vite.config.js
+++ apps/dropbox/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@contentful/dam-app-base': './node_modules/@contentful/dam-app-base/lib/index.js',
-    },
-  },
   base: './',
   server: {
     port: 3000,
