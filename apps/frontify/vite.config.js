bfaa8027ffb1db7301e2f042e4cef8f9c91ea859
Sarah Lessner <sarah.lessner@contentful.com>
2024-11-07 15:02:48 -0700

    chore: frontify remediate vuln deps integ-2278 (#9198)
    * chore: frontify remediate vuln deps integ-2278
    
    * remove test libraries and setup because there aren't any tests
    
    * update other vuln deps
    
    * add rollupOptions to resolve jsx-runtime error

---
+++ apps/frontify/vite.config.js
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  base: '', // relative paths
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react/jsx-runtime'],
+    },
+  },
+});
