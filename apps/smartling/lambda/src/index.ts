002922791f11919a175c3a97496f72ae6897fcee
Sarah Lessner <sarah.lessner@contentful.com>
2024-11-21 11:10:57 -0700

    try another cache control setting

--- apps/smartling/lambda/src/index.ts
+++ apps/smartling/lambda/src/index.ts
@@ -143,7 +143,7 @@ export function makeApp(fetchFn: any, issuer: any) {
   } else {
     // in production mode, frontend requests are served from the static smartling-frontend build folder
     const lastModified = computeLastModifiedTime();
-    app.set('Cache-Control', 'no-cache');
+    app.set('Cache-Control', 'no-store');
     app.use(
       '/frontend',
       express.static(path.dirname(require.resolve('@contentful/smartling-frontend')), {
