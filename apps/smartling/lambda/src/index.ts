6784ec0105463d91d2a1ba28dbb13309da08807e
Sarah Lessner <sarah.lessner@contentful.com>
2024-11-21 14:05:11 -0700

    clean up

--- apps/smartling/lambda/src/index.ts
+++ apps/smartling/lambda/src/index.ts
@@ -143,25 +143,7 @@ export function makeApp(fetchFn: any, issuer: any) {
   } else {
     // in production mode, frontend requests are served from the static smartling-frontend build folder
     const lastModified = computeLastModifiedTime();
-    // app.set('Cache-Control', 'no-store');
-    // app.use(
-    //   '/frontend',
-    //   express.static(path.dirname(require.resolve('@contentful/smartling-frontend')), {
-    //     lastModified,
-    //   })
-    // );
 
-    // const location = path.dirname(require.resolve('@contentful/smartling-frontend'));
-    // app.use(
-    //   '/frontend',
-    //   express.static(location, {
-    //     lastModified,
-    //     cacheControl: true,
-    //     setHeaders: function (res: any) {
-    //       res.set('Cache-Control', 'max-age=0, must-revalidate');
-    //     },
-    //   })
-    // );
     app.use(
       '/frontend',
       express.static(path.dirname(require.resolve('@contentful/smartling-frontend')), {
