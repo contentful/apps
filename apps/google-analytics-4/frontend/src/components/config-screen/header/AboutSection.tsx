7cb64f098e5af95b9f00a9219cb6b4cc5ab383f1
Sarah Lessner <sarah.lessner@contentful.com>
2024-11-27 12:20:04 -0700

    log backendApiUrl and node env values and test that fe change actually deployed

--- apps/google-analytics-4/frontend/src/components/config-screen/header/AboutSection.tsx
+++ apps/google-analytics-4/frontend/src/components/config-screen/header/AboutSection.tsx
@@ -4,7 +4,7 @@ import { Box, Heading, Paragraph } from '@contentful/f36-components';
 export default function AboutSection() {
   return (
     <Box>
-      <Heading>About Google Analytics 4 for Contentful</Heading>
+      <Heading>Aboot Google Analytics 4 for Contentful</Heading>
       <Paragraph>
         The Google Analytics 4 app displays real-time page-based analytics data from your
         organization’s Google Analytics 4 properties alongside relevant content entries.
