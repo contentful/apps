bc9389d94e4049f1be0b7fcf9acd45093e339730
Sarah Lessner <sarah.lessner@contentful.com>
2024-12-17 15:33:08 -0700

    Chore: Update Optimizely app - react update, react vite, emotion, forma libraries (#9077)
    * removes react-scripts and replaces with vite
    
    * replace emotion library
    
    * forma 36 upgrade optimizely
    
    * gitignore
    
    * wip resolving build errors
    
    * update vite
    
    * wip fix build
    
    * wip fix optimizely test
    
    * remove unused deps
    
    * remove unused deps
    
    * wip updating to modern react
    
    * updates packages to support react 18
    
    * fix sidebar buttons
    
    * resolve errors with components f36 update
    
    * clean up vite config
    
    * wip fix refetch errors
    
    * fix fetchEntry called too often
    
    * optimizely tests passing
    
    * replace some snapshots with checking test ids to be defined
    
    * cleanup several files props
    
    * fix tests
    
    * remove react fragment tag
    
    * adds contentful scripts and deploy to staging script
    
    * install compatible react testing library version
    
    * Update apps/optimizely/package.json
    
    Co-authored-by: Mitch Goudy <mgoudy91@gmail.com>
    
    ---------
    
    Co-authored-by: Mitch Goudy <mgoudy91@gmail.com>

---
+++ apps/optimizely/src/Sidebar.spec.jsx
@@ -0,0 +1,190 @@
+import React from 'react';
+import { cleanup, render, configure } from '@testing-library/react';
+
+import Sidebar from '../src/Sidebar';
+import { ProjectType } from './constants';
+import { vi } from 'vitest';
+
+let LOCATION = 'entry-sidebar';
+let PROJECT_ID = '123';
+let VALID_FIELDS = true;
+
+const mockUnsub = vi.fn();
+
+function mockClient() {}
+
+function mockSdk() {
+  return {
+    parameters: {
+      installation: {
+        optimizelyProjectId: PROJECT_ID,
+        optimizelyProjectType: ProjectType.FeatureExperimentation,
+      },
+    },
+    location: {
+      is: vi.fn((l) => {
+        return l === LOCATION;
+      }),
+    },
+    window: {
+      startAutoResizer: vi.fn(),
+      stopAutoResizer: vi.fn(),
+    },
+    ids: {},
+    space: {},
+    locales: {},
+    entry: {
+      fields: {
+        experimentId: {
+          getValue: vi.fn(() => 'exp123'),
+          onValueChanged: vi.fn(() => mockUnsub),
+        },
+        experimentKey: {
+          getValue: vi.fn(() => 'exp123'),
+          onValueChanged: vi.fn(() => mockUnsub),
+        },
+        environment: {
+          getValue: vi.fn(() => 'production'),
+          onValueChanged: vi.fn(() => mockUnsub),
+        },
+        flagKey: {
+          getValue: vi.fn(() => 'flag123'),
+          onValueChanged: vi.fn(() => mockUnsub),
+        },
+        revision: {
+          getValue: vi.fn(() => 'random'),
+          onValueChanged: vi.fn(() => mockUnsub),
+        },
+        meta: {
+          getValue: vi.fn(),
+          onValueChanged: vi.fn(() => vi.fn()),
+        },
+        variations: {
+          getValue: vi.fn(),
+          onValueChanged: vi.fn(() => vi.fn()),
+        },
+      },
+    },
+    contentType: {
+      sys: {
+        space: {
+          sys: {
+            type: 'Link',
+            linkType: 'Space',
+            id: 'cyu19ucaypb9',
+          },
+        },
+        id: 'variationContainer',
+        type: 'ContentType',
+        createdAt: '2019-05-24T07:45:48.863Z',
+        updatedAt: '2019-05-30T04:28:43.488Z',
+        environment: {
+          sys: {
+            id: 'master',
+            type: 'Link',
+            linkType: 'Environment',
+          },
+        },
+        revision: 3,
+      },
+      name: 'Variation Container',
+      description: null,
+      displayField: 'experimentTitle',
+      fields: [
+        {
+          id: 'experimentTitle',
+          name: 'Experiment title',
+          type: 'Symbol',
+          localized: false,
+          required: false,
+          validations: [],
+          disabled: false,
+          omitted: false,
+        },
+        {
+          id: 'experimentId',
+          name: 'Experiment ID',
+          type: 'Symbol',
+          localized: false,
+          required: false,
+          validations: [],
+          disabled: false,
+          omitted: false,
+        },
+        {
+          id: 'meta',
+          name: 'Meta',
+          type: 'Object',
+          localized: false,
+          required: false,
+          validations: [],
+          disabled: false,
+          omitted: false,
+        },
+        {
+          id: 'variations',
+          name: 'Variations',
+          type: 'Array',
+          localized: false,
+          required: false,
+          validations: [],
+          disabled: false,
+          omitted: false,
+          items: {
+            type: 'Link',
+            validations: [],
+            linkType: 'Entry',
+          },
+        },
+      ].concat(
+        VALID_FIELDS
+          ? {
+              id: 'experimentKey',
+              name: 'Experiment key',
+              type: 'Symbol',
+              localized: false,
+              required: false,
+              validations: [],
+              disabled: false,
+              omitted: false,
+            }
+          : []
+      ),
+    },
+  };
+}
+
+configure({ testIdAttribute: 'data-test-id' });
+
+describe('Sidebar', () => {
+  afterEach(cleanup);
+
+  it('should run all lifecycle methods', () => {
+    const sdk = mockSdk();
+    const { unmount } = render(<Sidebar sdk={sdk} />);
+
+    expect(sdk.window.startAutoResizer).toHaveBeenCalledTimes(1);
+
+    expect(sdk.entry.fields.experimentKey.getValue).toHaveBeenCalledTimes(2);
+    expect(sdk.entry.fields.revision.onValueChanged).toHaveBeenCalledTimes(1);
+    expect(typeof sdk.entry.fields.revision.onValueChanged.mock.calls[0][0]).toEqual('function');
+
+    unmount();
+
+    expect(sdk.window.stopAutoResizer).toHaveBeenCalledTimes(1);
+    expect(mockUnsub).toHaveBeenCalledTimes(1);
+  });
+
+  it('should use the project ID to create urls', () => {
+    const sdk = mockSdk();
+    const { getByTestId } = render(<Sidebar sdk={sdk} />);
+
+    expect(getByTestId('view-experiment').href).toBe(
+      'https://app.optimizely.com/v2/projects/123/flags/manage/flag123/rules/production/edit/exp123'
+    );
+
+    expect(getByTestId('view-all').href).toBe(
+      'https://app.optimizely.com/v2/projects/123/flags/list?environment=production'
+    );
+  });
+});
