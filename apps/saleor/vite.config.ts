6966ee75bc9db90c4ccde74e12a1a3a5f6c23b72
Mitch Goudy <mitchell.goudy@contentful.com>
2024-12-19 16:06:23 -0700

    chore: migrate to vite

---
+++ apps/saleor/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+  server: {
+    port: 3000,
+  },
+  base: '',
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
+}));
