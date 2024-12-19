7d2ad6c7c66e052b2560d210ea3dfecd6be4a6be
Sarah Lessner <sarah.lessner@contentful.com>
2024-12-19 13:20:01 -0700

    update import.meta logic

--- apps/jira/jira-app/src/constants.ts
+++ apps/jira/jira-app/src/constants.ts
@@ -1,15 +1,9 @@
 let LAMBDA_URI = 'https://api.jira.ctfapps.net';
 let CLIENT_ID = 'XD9k9QU9VT4Rt26u6lbO3NM0fOqvvXan';
 
-interface ImportMetaValue extends ImportMeta {
-  NODE_ENV?: string;
-  VITE_NGROK_URL?: string;
-  VITE_ATLASSIAN_APP_CLIENT_ID?: string;
-}
-
-if ((import.meta as ImportMetaValue).NODE_ENV === 'development') {
-  LAMBDA_URI = `${(import.meta as ImportMetaValue).VITE_NGROK_URL}/test`;
-  CLIENT_ID = (import.meta as ImportMetaValue).VITE_ATLASSIAN_APP_CLIENT_ID || '';
+if (import.meta.env.DEV) {
+  LAMBDA_URI = `${import.meta.env.VITE_NGROK_URL}/test`;
+  CLIENT_ID = import.meta.env.VITE_ATLASSIAN_APP_CLIENT_ID || '';
 }
 
 const constants = {
