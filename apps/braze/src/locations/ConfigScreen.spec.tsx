9296d8cf57edd73a28a01a5f58881740cd49aa27
francobanfi <franco.banfi@external.contentful.com>
2025-02-25 13:44:13 -0300

    adding test to check if the api key is set correctly

--- apps/braze/src/locations/ConfigScreen.spec.tsx
+++ apps/braze/src/locations/ConfigScreen.spec.tsx
@@ -2,6 +2,8 @@ import {fireEvent, screen, render} from '@testing-library/react';
 import { describe, expect, it, vi} from 'vitest';
 import { mockCma, mockSdk } from '../../test/mocks';
 import ConfigScreen, {BRAZE_DOCUMENTATION} from './ConfigScreen';
+import userEvent from '@testing-library/user-event';
+
 
 vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
@@ -38,4 +40,17 @@ describe('Config Screen component', () => {
     const inputExpected = screen.getAllByTestId('apiKey')[0] as HTMLInputElement;
     expect(inputExpected.value).toEqual(`A test value for input`)
   });
+
+  it('when installed the api key is set correctly', async () => {
+    const user = userEvent.setup();
+
+    const apiKeyInput = screen.getAllByTestId('apiKey')[0];
+    await user.type(apiKeyInput, 'valid-api-key-123');
+    const result = await mockSdk.app.onConfigure.mock.calls.at(-1)[0]();
+
+    expect(result).toEqual({
+      parameters: { apiKey: 'valid-api-key-123' }
+    });
+  });
+
 });
