686ff2c7336f1ae1dd8160c1be6f17d8c83fe963
david shibley <david.shibley@contentful.com>
2025-03-05 14:01:02 -0700

    feat: update last-fm example to use vite instead of react scripts

---
+++ examples/last-fm/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: './dist',
+  },
+  server: {
+    port: 3000,
+  },
+});
