d04af0a51e9d22785499132055324789400d087c
david shibley <david.shibley@contentful.com>
2025-03-05 12:46:15 -0700

    feat: update editor-assignment example to use vite instead of react scripts

---
+++ examples/editor-assignment/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
+
+export default defineConfig({
+  plugins: [react(), commonjs()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  test: {
+    globals: true, // Enables Jest-like global test functions (test, expect)
+    environment: 'jsdom', // Simulates a browser for component tests
+    setupFiles: './src/setupTests.ts', // Equivalent to Jest's setup file
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
