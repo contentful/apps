de3f2980760ec573e9ea4a75255272d7a89e040f
david shibley <david.shibley@contentful.com>
2025-03-05 14:18:08 -0700

    feat: update native-external-references example to use vite instead of react scripts

--- examples/native-external-references/src/locations/Home.spec.tsx
+++ examples/native-external-references/src/locations/Home.spec.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Home from './Home';
 import { render } from '@testing-library/react';
 import { mockCma, mockSdk } from '../../test/mocks';
+import { describe, expect, it, vi } from 'vitest';
 
-jest.mock('@contentful/react-apps-toolkit', () => ({
+vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
   useCMA: () => mockCma
 }));
@@ -12,8 +13,6 @@ describe('Home component', () => {
   it('Component text exists', () => {
     const { getByText } = render(<Home />);
 
-    expect(
-      getByText('Hello Home Component (AppId: test-app)')
-    ).toBeInTheDocument();
+    expect(getByText('Hello Home Component (AppId: test-app)')).toBeDefined();
   });
 });
