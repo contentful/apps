f867def4bed72f568d082448f29b778cdb14333a
david shibley <david.shibley@contentful.com>
2025-03-05 12:41:41 -0700

    feat: update dam-app example to use vite instead of react scripts

---
+++ examples/dam-app/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
