a6c31516d50e8c47440bf6ac5f147f3120baac1c
joaquin <joaquin.casal@10pines.com>
2025-03-10 12:35:34 -0300

    Display example response

--- apps/braze/src/helpers/generateLiquidTags.spec.ts
+++ apps/braze/src/helpers/generateLiquidTags.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import generateLiquidTags from './generateLiquidTags';
-import { Field } from '../dialogaux';
+import { Field } from './assembleQuery';
 
 describe('Generate liquid tags', () => {
   it('Content type with text field transforms it into a liquid tag', () => {
--- apps/braze/src/helpers/generateLiquidTags.ts
+++ apps/braze/src/helpers/generateLiquidTags.ts
@@ -1,19 +1,19 @@
-import { Field } from '../dialogaux';
-import {ASSET_FIELDS, LOCATION_LAT, LOCATION_LONG, SAVED_RESPONSE} from './utils';
+import { Field } from './assembleQuery';
+import { ASSET_FIELDS, LOCATION_LAT, LOCATION_LONG, SAVED_RESPONSE } from './utils';
 
 export default function generateLiquidTags(contentTypeId: string, fields: Field[]): string[] {
   const liquidTags: string[] = [];
   const responseData = `${SAVED_RESPONSE}.data`;
 
   fields.forEach((field) => {
-    console.log("Field: ", field);
+    console.log('Field: ', field);
     let content = `${responseData}.${contentTypeId}.${field.id}`;
 
     if (field.type === 'Link') {
       if (field.linkType === 'Asset') {
-        ASSET_FIELDS.forEach(field => {
+        ASSET_FIELDS.forEach((field) => {
           liquidTags.push(`{{${content}.${field}}}`);
-        })
+        });
       }
     } else if (field.type === 'Location') {
       liquidTags.push(`{{${content}.${LOCATION_LAT}}}`);
