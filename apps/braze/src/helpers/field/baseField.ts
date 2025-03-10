5a610701553bfbff1ad4483a66ca1b0cd1a21e8e
francobanfi <franco.banfi@external.contentful.com>
2025-03-10 10:54:05 -0300

    initial structure for polimorfic fields

---
+++ apps/braze/src/helpers/field/baseField.ts
@@ -0,0 +1,17 @@
+import {FieldType} from "@contentful/app-sdk";
+
+export abstract class BaseField {
+    constructor(public id: string) {}
+
+    abstract toLiquidTag(contentTypeId: string, responseData: string): string[];
+}
+
+export class BasicField extends BaseField {
+    constructor(id: string, public type: Exclude<FieldType, 'Array' | 'Link'>) {
+        super(id);
+    }
+
+    toLiquidTag(contentTypeId: string, responseData: string): string[] {
+        return [`{{${responseData}.${contentTypeId}.${this.id}}}`];
+    }
+}
