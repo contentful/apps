d45bb1923142bb5bfd7be7579d1d11ed9834964d
Juli Rossi <juliana.rossi@10pines.com>
2025-03-12 17:29:18 -0300

    Fixing prettier

--- apps/braze/src/helpers/utils.ts
+++ apps/braze/src/helpers/utils.ts
@@ -1,8 +1,4 @@
 export const SAVED_RESPONSE = 'response';
-export const ASSET_FIELDS = [
-  'title',
-  'description',
-  'url',
-];
+export const ASSET_FIELDS = ['title', 'description', 'url'];
 export const LOCATION_LAT = 'lat';
 export const LOCATION_LONG = 'long';
