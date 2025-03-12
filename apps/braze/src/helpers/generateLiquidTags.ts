fb8935c6a7f1bbfb29f335c54450f1509d69f9c5
francobanfi <franco.banfi@external.contentful.com>
2025-03-12 09:43:48 -0300

    skiping generation of rich text liquid tag

--- apps/braze/src/helpers/generateLiquidTags.ts
+++ apps/braze/src/helpers/generateLiquidTags.ts
@@ -4,29 +4,33 @@ import {ASSET_FIELDS, LOCATION_LAT, LOCATION_LONG, SAVED_RESPONSE} from './utils
 export default function generateLiquidTags(prefix: string, fields: Field[]): string[] {
   const liquidTags: string[] = [];
 
+  console.log("Fields", fields);
+
   fields.forEach((field, index) => {
     const content = `${SAVED_RESPONSE}.data.${prefix}.${field.id}`;
-    //TODO: make it skip generating liquid tags for rich-text fields
-    if (field.type === 'Link') {
-      if (field.linkType === 'Asset') {
-        liquidTags.push(...generateLiquidAssetFields(content));
+
+    if (field.type !== 'RichText') {
+      if (field.type === 'Link') {
+        if (field.linkType === 'Asset') {
+          liquidTags.push(...generateLiquidAssetFields(content));
+        }
+        if (field.linkType === 'Entry') {
+          // TODO : check if this works for entry within entry within entry
+          liquidTags.push(...generateLiquidTags(`${prefix}.${field.id}`, field.fields));
+        }
+      } else if (field.type === 'Location') {
+        liquidTags.push(...generateLiquidLocationFields(content));
+      } else if (field.type === 'Array') {
+        if (field.arrayType === 'Entry') {
+          pushEntryArrayLiquidTag(field, prefix, liquidTags);
+        } else if (field.arrayType === 'Asset') {
+          liquidTags.push(...generateAssetArrayLiquidTag(content, index));
+        } else if (field.arrayType === 'Symbol') {
+          liquidTags.push(generateTextArrayLiquidTag(content));
+        }
+      } else {
+        liquidTags.push(`{{${content}}}`);
       }
-      if (field.linkType === 'Entry') {
-        // TODO : check if this works for entry within entry within entry
-        liquidTags.push(...generateLiquidTags(`${prefix}.${field.id}`, field.fields));
-      }
-    } else if (field.type === 'Location') {
-      liquidTags.push(...generateLiquidLocationFields(content));
-    } else if (field.type === 'Array') {
-      if (field.arrayType === 'Entry') {
-        pushEntryArrayLiquidTag(field, prefix, liquidTags);
-      } else if (field.arrayType === 'Asset') {
-        liquidTags.push(...generateAssetArrayLiquidTag(content, index));
-      } else if (field.arrayType === 'Symbol') {
-        liquidTags.push(generateTextArrayLiquidTag(content));
-      }
-    } else {
-      liquidTags.push(`{{${content}}}`);
     }
   });
 
