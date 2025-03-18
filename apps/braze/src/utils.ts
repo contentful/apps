f87244493eea8fa442d521a3e5531c1e59ba1870
joaquin <joaquin.casal@10pines.com>
2025-03-18 15:33:59 -0300

    Set first letter of content type to lowercase

--- apps/braze/src/utils.ts
+++ apps/braze/src/utils.ts
@@ -16,3 +16,7 @@ export const SIDEBAR_BUTTON_TEXT = 'Generate Braze Connected Content';
 export function capitalize(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+export function firstLetterToLowercase(s: string): string {
+  return s.charAt(0).toLocaleLowerCase() + s.slice(1);
+}
