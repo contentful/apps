781f4bb4de6a3e7a9dacb574dd9c9954f229542a
Juli Rossi <juliana.rossi@external.contentful.com>
2025-03-31 14:15:19 -0300

    Fix indentation

--- apps/braze/src/fields/Entry.ts
+++ apps/braze/src/fields/Entry.ts
@@ -26,17 +26,17 @@ export class Entry {
 
   generateConnectedContentCall(locales: string[]) {
     return `{% capture body %}
-    ${this.assembleQuery(locales)}
+  ${this.assembleQuery(locales)}
 {% endcapture %}
   
 {% connected_content
-      https://graphql.contentful.com/content/v1/spaces/${this.spaceId}
-      :method post
-      :headers {"Authorization": "Bearer ${this.contentfulToken}"}
-      :body {{body}}
-      :content_type application/json
-      :save ${SAVED_RESPONSE}
-      :retry
+  https://graphql.contentful.com/content/v1/spaces/${this.spaceId}
+  :method post
+  :headers {"Authorization": "Bearer ${this.contentfulToken}"}
+  :body {{body}}
+  :content_type application/json
+  :save ${SAVED_RESPONSE}
+  :retry
 %}
 
 {% if response.__http_status_code__ != 200 %}
