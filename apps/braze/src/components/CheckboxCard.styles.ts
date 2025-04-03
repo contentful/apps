cc7daca5e217ea729c18ed5154748e89fc6fa7b5
francobanfi <franco.banfi@external.contentful.com>
2025-04-03 15:50:14 -0300

    formating with prettier

--- apps/braze/src/components/CheckboxCard.styles.ts
+++ apps/braze/src/components/CheckboxCard.styles.ts
@@ -1,5 +1,5 @@
 import tokens from '@contentful/f36-tokens';
-import {css} from "emotion";
+import { css } from 'emotion';
 
 export const checkboxCard = css({
   border: `1px solid ${tokens.gray200}`,
