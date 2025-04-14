2d299a68ce4f5076909301b567884f9338499c99
joaquin <joaquin.casal@10pines.com>
2025-04-14 10:17:24 -0300

    Braze app: FieldsFactory refactor
    This commit inverts the way in which the fields from the entry are obtained: first it gets the content type (which is enough for most fields) and then the entry itself is fetched, including the references (this is for reference fields only). This way if a field is empty, it can still be selected

--- apps/braze/src/fields/FieldsFactory.ts
+++ apps/braze/src/fields/FieldsFactory.ts
@@ -1,4 +1,9 @@
-import { ContentTypeProps, PlainClientAPI } from 'contentful-management';
+import {
+  ContentFields,
+  ContentTypeProps,
+  KeyValueMap,
+  PlainClientAPI,
+} from 'contentful-management';
 import { Field } from './Field';
 import { AssetArrayField } from './AssetArrayField';
 import { AssetField } from './AssetField';
@@ -9,101 +14,51 @@ import { ReferenceField } from './ReferenceField';
 import { ReferenceItem } from './ReferenceItem';
 import { RichTextField } from './RichTextField';
 import { TextArrayField } from './TextArrayField';
+import { EntryInfo } from '../locations/Dialog';
+import resolveResponse from 'contentful-resolve-response';
 
 export class FieldsFactory {
   private contentTypes: { [key: string]: ContentTypeProps };
+  private entryInfo: EntryInfo;
   private cma: PlainClientAPI;
   NESTED_DEPTH = 5;
 
-  public constructor(cma: PlainClientAPI) {
+  public constructor(entryInfo: EntryInfo, cma: PlainClientAPI) {
+    this.entryInfo = entryInfo;
     this.cma = cma;
     this.contentTypes = {};
   }
 
-  public async createFields(entry: any, depth: number = 1): Promise<Field[]> {
-    const contentType = await this.getContentType(entry.sys.contentType.sys.id);
-    const contentTypeId = entry.sys.contentType.sys.id;
-    const fields = [];
-    for (const [fieldId, fieldValues] of Object.entries(entry.fields)) {
-      const field = Object.values(fieldValues as { [key: string]: any })[0];
-      const fieldInfo = contentType.fields.find((f) => f.id === fieldId);
-      if (!fieldInfo) {
-        throw new Error('Field not found');
-      }
-      const fieldName = fieldInfo.name;
-      const localized = fieldInfo.localized;
+  public async createFields() {
+    const response = await this.cma.entry.references({ entryId: this.entryInfo.id, include: 5 });
+    const items = resolveResponse(response);
+    const contentType = await this.getContentType(this.entryInfo.contentTypeId);
+    return this.createFieldsForEntry(items[0].fields, contentType);
+  }
 
-      if (fieldInfo.type === 'Link') {
-        if (fieldInfo.linkType === 'Asset') {
-          const newField = new AssetField(fieldId, fieldName, contentTypeId, localized);
-          fields.push(newField);
-        } else {
-          if (depth >= this.NESTED_DEPTH) {
-            continue;
-          }
-          const fieldContentType = await this.getContentType(field.sys.contentType.sys.id);
-          const title = !!fieldContentType.displayField
-            ? Object.values(
-                field.fields[fieldContentType.displayField] as { [key: string]: any }
-              )[0]
-            : 'Untitled';
-          const newField = new ReferenceField(
-            fieldId,
-            fieldName,
-            contentTypeId,
-            title,
-            localized,
-            fieldContentType.sys.id,
-            fieldContentType.name,
-            await this.createFields(field, depth + 1)
-          );
-          fields.push(newField);
-        }
-      } else if (fieldInfo.type === 'Array') {
-        if (fieldInfo.items && fieldInfo.items.type === 'Symbol') {
-          const newField = new TextArrayField(fieldId, fieldName, contentTypeId, localized);
-          fields.push(newField);
-        } else if (fieldInfo.items && fieldInfo.items.linkType === 'Asset') {
-          const newField = new AssetArrayField(fieldId, fieldName, contentTypeId, localized);
-          fields.push(newField);
-        } else {
-          if (depth >= this.NESTED_DEPTH) {
-            continue;
-          }
-          const items = await Promise.all(
-            field.map(async (f: any, index: number) => {
-              const fieldContentType = await this.getContentType(f.sys.contentType.sys.id);
-              return new ReferenceItem(
-                crypto.randomUUID(),
-                `${fieldName} item #${index + 1}`,
-                contentTypeId,
-                Object.values(f.fields[fieldContentType.displayField] as { [key: string]: any })[0],
-                fieldInfo.localized,
-                fieldContentType.sys.id,
-                fieldContentType.name,
-                await this.createFields(f, depth + 1)
-              );
-            })
-          );
-          const newField = new ReferenceArrayField(
-            fieldId,
-            fieldName,
-            contentTypeId,
-            localized,
-            items
-          );
-          fields.push(newField);
-        }
+  private async createFieldsForEntry(
+    entryFields: any,
+    contentType: ContentTypeProps,
+    currentDepth: number = 1
+  ): Promise<Field[]> {
+    const fields = [];
+    for (const fieldInfo of contentType.fields) {
+      if (!this.isReferenceField(fieldInfo) && !this.isReferenceArrayField(fieldInfo)) {
+        fields.push(this.createSimpleField(fieldInfo, contentType));
       } else {
-        if (fieldInfo.type === 'RichText') {
-          const newField = new RichTextField(fieldId, fieldName, contentTypeId, localized);
-          fields.push(newField);
-        } else if (fieldInfo.type === 'Location') {
-          const newField = new LocationField(fieldId, fieldName, contentTypeId, localized);
-          fields.push(newField);
-        } else {
-          const newField = new BasicField(fieldId, fieldName, contentTypeId, localized);
-          fields.push(newField);
+        const field = entryFields[fieldInfo.id];
+        if (currentDepth >= this.NESTED_DEPTH || !field) {
+          continue;
+        }
+        const fieldValue = Object.values(field as { [key: string]: any })[0];
+        if (this.isReferenceField(fieldInfo)) {
+          fields.push(
+            await this.createReferenceField(fieldInfo, fieldValue, contentType, currentDepth)
+          );
+        } else if (this.isReferenceArrayField(fieldInfo)) {
+          fields.push(
+            await this.createReferenceArrayField(fieldInfo, fieldValue, contentType, currentDepth)
+          );
         }
       }
     }
@@ -115,5 +70,90 @@ export class FieldsFactory {
       this.contentTypes[contentTypeId] = await this.cma.contentType.get({ contentTypeId });
     }
     return this.contentTypes[contentTypeId];
+  }
+
+  private isReferenceField(fieldInfo: ContentFields<KeyValueMap>) {
+    return fieldInfo.type === 'Link' && fieldInfo.linkType === 'Entry';
+  }
+
+  private isReferenceArrayField(fieldInfo: ContentFields<KeyValueMap>) {
+    return fieldInfo.type === 'Array' && fieldInfo.items && fieldInfo.items.linkType === 'Entry';
+  }
+
+  private createSimpleField(
+    fieldInfo: ContentFields<KeyValueMap>,
+    contentType: ContentTypeProps
+  ): Field {
+    let fieldClass;
+    if (fieldInfo.type === 'Link') {
+      fieldClass = AssetField;
+    } else if (fieldInfo.type === 'Array') {
+      if (fieldInfo.items && fieldInfo.items.type === 'Symbol') {
+        fieldClass = TextArrayField;
+      } else {
+        fieldClass = AssetArrayField;
+      }
+    } else if (fieldInfo.type === 'RichText') {
+      fieldClass = RichTextField;
+    } else if (fieldInfo.type === 'Location') {
+      fieldClass = LocationField;
+    } else {
+      fieldClass = BasicField;
+    }
+    return new fieldClass(fieldInfo.id, fieldInfo.name, contentType.sys.id, fieldInfo.localized);
+  }
+
+  private async createReferenceField(
+    fieldInfo: ContentFields<KeyValueMap>,
+    fieldValue: any,
+    contentType: ContentTypeProps,
+    currentDepth: number
+  ): Promise<ReferenceField> {
+    const fieldContentType = await this.getContentType(fieldValue.sys.contentType.sys.id);
+    const title = !!fieldContentType.displayField
+      ? Object.values(fieldValue.fields[fieldContentType.displayField] as { [key: string]: any })[0]
+      : 'Untitled';
+
+    return new ReferenceField(
+      fieldInfo.id,
+      fieldInfo.name,
+      contentType.sys.id,
+      title,
+      fieldInfo.localized,
+      fieldContentType.sys.id,
+      fieldContentType.name,
+      await this.createFieldsForEntry(fieldValue.fields, fieldContentType, currentDepth + 1)
+    );
+  }
+
+  private async createReferenceArrayField(
+    fieldInfo: ContentFields<KeyValueMap>,
+    fieldValue: any,
+    contentType: ContentTypeProps,
+    currentDepth: number
+  ): Promise<ReferenceArrayField> {
+    const items = await Promise.all(
+      fieldValue.map(async (f: any, index: number) => {
+        const fieldContentType = await this.getContentType(f.sys.contentType.sys.id);
+        return new ReferenceItem(
+          crypto.randomUUID(),
+          `${fieldInfo.name} item #${index + 1}`,
+          contentType.sys.id,
+          Object.values(f.fields[fieldContentType.displayField] as { [key: string]: any })[0],
+          fieldInfo.localized,
+          fieldContentType.sys.id,
+          fieldContentType.name,
+          await this.createFieldsForEntry(f.fields, fieldContentType, currentDepth + 1)
+        );
+      })
+    );
+
+    return new ReferenceArrayField(
+      fieldInfo.id,
+      fieldInfo.name,
+      contentType.sys.id,
+      fieldInfo.localized,
+      items
+    );
   }
 }
