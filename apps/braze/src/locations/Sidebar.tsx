f4b40d7443a248a4e55e8af164858c85cba11568
Franco Banfi <62450599+FBanfi@users.noreply.github.com>
2025-04-23 15:46:06 -0300

    Braze app: create and generate sidebar buttons [INTEG-2535] (#9717)
    * generate and create button + refactor to reuse repeated logic from config screen and side bar
    
    * adding test for the InformationWithLink.tsx component + refactor in tests
    
    * changing sidebar buttons color

--- apps/braze/src/locations/Sidebar.tsx
+++ apps/braze/src/locations/Sidebar.tsx
@@ -1,8 +1,32 @@
-import { SidebarAppSDK } from '@contentful/app-sdk';
-import { Button } from '@contentful/f36-components';
+import { BaseAppSDK, IdsAPI, SharedEditorSDK, SidebarAppSDK, WindowAPI } from '@contentful/app-sdk';
+import { Box, Button, Subheading } from '@contentful/f36-components';
 import { useAutoResizer, useSDK } from '@contentful/react-apps-toolkit';
 import { EntryInfo } from './Dialog';
-import { DIALOG_TITLE, SIDEBAR_BUTTON_TEXT } from '../utils';
+import {
+  BRAZE_CONTENT_BLOCK_DOCUMENTATION,
+  CONNECTED_CONTENT_DOCUMENTATION,
+  DIALOG_TITLE,
+  SIDEBAR_CREATE_BUTTON_TEXT,
+  SIDEBAR_GENERATE_BUTTON_TEXT,
+} from '../utils';
+import { styles } from './Sidebar.styles';
+import { KeyValueMap } from 'contentful-management';
+import InformationWithLink from '../components/InformationWithLink';
+
+function openDialog(
+  sdk: Omit<BaseAppSDK<KeyValueMap, KeyValueMap, never>, 'ids'> &
+    SharedEditorSDK & {
+      ids: Omit<IdsAPI, 'field'>;
+      window: WindowAPI;
+    },
+  invocationParams: EntryInfo
+) {
+  sdk.dialogs.openCurrentApp({
+    title: DIALOG_TITLE,
+    parameters: invocationParams,
+    width: 'fullWidth',
+  });
+}
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
@@ -15,18 +39,46 @@ const Sidebar = () => {
   };
 
   return (
-    <Button
-      variant="primary"
-      isFullWidth={true}
-      onClick={() => {
-        sdk.dialogs.openCurrentApp({
-          title: DIALOG_TITLE,
-          parameters: invocationParams,
-          width: 'fullWidth',
-        });
-      }}>
-      {SIDEBAR_BUTTON_TEXT}
-    </Button>
+    <>
+      <Box>
+        <Subheading className={styles.subheading}>Connected Content</Subheading>
+        <InformationWithLink
+          url={CONNECTED_CONTENT_DOCUMENTATION}
+          linkText={'Learn more'}
+          fontColor="gray500"
+          marginTop="spacing2Xs"
+          marginBottom="spacingS">
+          Generate a Connected Content call to copy and paste into Braze.
+        </InformationWithLink>
+        <Button
+          variant="secondary"
+          isFullWidth={true}
+          onClick={() => {
+            openDialog(sdk, invocationParams);
+          }}>
+          {SIDEBAR_GENERATE_BUTTON_TEXT}
+        </Button>
+      </Box>
+      <Box marginTop="spacingS">
+        <Subheading className={styles.subheading}>Content Blocks</Subheading>
+        <InformationWithLink
+          url={BRAZE_CONTENT_BLOCK_DOCUMENTATION}
+          linkText={'Learn more'}
+          fontColor="gray500"
+          marginTop="spacing2Xs"
+          marginBottom="spacingS">
+          Send individual entry fields to Braze to create Content Blocks.
+        </InformationWithLink>
+        <Button
+          variant="secondary"
+          isFullWidth={true}
+          onClick={() => {
+            openDialog(sdk, invocationParams);
+          }}>
+          {SIDEBAR_CREATE_BUTTON_TEXT}
+        </Button>
+      </Box>
+    </>
   );
 };
 
--- apps/braze/src/utils.ts
+++ apps/braze/src/utils.ts
@@ -11,7 +11,17 @@ export const ASSET_FIELDS_QUERY = [
 ];
 export const ASSET_FIELDS = ['title', 'description', 'url'];
 export const DIALOG_TITLE = 'Generate Braze Connected Content Call';
-export const SIDEBAR_BUTTON_TEXT = 'Generate Braze Connected Content';
+export const SIDEBAR_GENERATE_BUTTON_TEXT = 'Generate';
+export const SIDEBAR_CREATE_BUTTON_TEXT = 'Create';
+
+export const CONNECTED_CONTENT_DOCUMENTATION =
+  'https://www.braze.com/docs/user_guide/personalization_and_dynamic_content/connected_content';
+export const CONTENT_TYPE_DOCUMENTATION =
+  'https://www.contentful.com/help/content-types/configure-content-type/';
+export const BRAZE_APP_DOCUMENTATION = 'https://www.contentful.com/help/apps/braze-app/';
+export const BRAZE_API_KEY_DOCUMENTATION = `https://dashboard.braze.com/app_settings/developer_console/apisettings#apikeys`;
+export const BRAZE_CONTENT_BLOCK_DOCUMENTATION =
+  'https://www.braze.com/docs/api/endpoints/templates/content_blocks_templates/post_create_email_content_block';
 
 export function capitalize(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1);
