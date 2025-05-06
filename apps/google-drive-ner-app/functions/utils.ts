a563dc94a21e07a988043c9af722a092c1135877
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2025-05-06 11:39:10 -0600

    created contentful app from native external references template

---
+++ apps/google-drive-ner-app/functions/utils.ts
@@ -0,0 +1,26 @@
+import { FunctionEventContext } from '@contentful/node-apps-toolkit';
+import { Product } from './types';
+
+export const getMockShopUrl = (context: FunctionEventContext<Record<string, any>>) => {
+  const mockShopUrl = context.appInstallationParameters?.apiEndpoint;
+
+  if (!mockShopUrl) {
+    console.warn(`No API url configured, using default: https://mock.shop/api`);
+    return 'https://mock.shop/api';
+  }
+  return mockShopUrl;
+};
+
+export function withUrn(node: Product) {
+  return {
+    ...node,
+    urn: node.id,
+  };
+}
+
+export function withBadge(node: Product) {
+  return {
+    ...node,
+    badge: { variant: 'primary', label: 'it works' },
+  };
+}
---
+++ apps/google-drive-ner-app/src/tools/http.ts
@@ -0,0 +1,52 @@
+import { createClient, ResourceProviderProps, ResourceTypeProps } from 'contentful-management';
+import { accessToken, appDefinitionId, manifest, organizationId, contentfulHost } from './imports';
+
+const host = contentfulHost || 'api.contentful.com';
+const client = createClient({ accessToken, host }, { type: 'plain' });
+
+export const getResourceProvider = async () => {
+  return client.resourceProvider.get({ organizationId, appDefinitionId });
+};
+
+export const createResourceProvider = async (resourceProvider: ResourceProviderProps) => {
+  const resourceProviderWithFunctionId: ResourceProviderProps = {
+    ...resourceProvider,
+    function: {
+      ...resourceProvider.function,
+      sys: {
+        ...resourceProvider.function.sys,
+        id: manifest.functions[0].id,
+      },
+    },
+  };
+
+  return client.resourceProvider.upsert(
+    { organizationId, appDefinitionId },
+    resourceProviderWithFunctionId
+  );
+};
+
+export const deleteResourceProvider = async () => {
+  return client.resourceProvider.delete({ organizationId, appDefinitionId });
+};
+
+export const createResourceType = async (resourceType: ResourceTypeProps) => {
+  const { sys, ...data } = resourceType;
+
+  return client.resourceType.upsert(
+    { organizationId, appDefinitionId, resourceTypeId: resourceType.sys.id },
+    data
+  );
+};
+
+export const listResourceTypes = async () => {
+  return client.resourceType.getMany({ organizationId, appDefinitionId });
+};
+
+export const deleteResourceType = async (resourceType: ResourceTypeProps) => {
+  return client.resourceType.delete({
+    organizationId,
+    appDefinitionId,
+    resourceTypeId: resourceType.sys.id,
+  });
+};
