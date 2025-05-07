8f657cfd51a7f2a5fc6eac423310edcc20292fd0
vikaskumr <vikas.kumar@contentful.com>
2025-05-07 10:45:04 -0600

    fix: added the google drive logic back in resolver

--- apps/google-drive-ner-app/functions/googleDriveGraphqlSchema.ts
+++ apps/google-drive-ner-app/functions/googleDriveGraphqlSchema.ts
@@ -22,61 +22,59 @@ const schema = makeExecutableSchema({
   resolvers: {
     Query: {
       file: async (_parent, { id, search }, _context) => {
-        // if (!search && !id) {
-        //   throw new GraphQLError('Either "search" or "id" must be provided');
-        // }
+        if (!search && !id) {
+          throw new GraphQLError('Either "search" or "id" must be provided');
+        }
 
-        // let files = [];
+        let files = [];
 
-        // if (search) {
-        //   const url = `https://www.googleapis.com/drive/v3/files?q=name contains '${search}'&fields=files(id,name,thumbnailLink)`;
+        if (search) {
+          const url = `https://www.googleapis.com/drive/v3/files?q=name contains '${search}'&fields=files(id,name,thumbnailLink)`;
 
-        //   const response = await fetch(url, {
-        //     method: 'GET',
-        //     headers: {
-        //       Accept: 'application/json',
-        //       'content-type': 'application/json',
-        //     },
-        //   });
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'content-type': 'application/json',
+            },
+          });
 
-        //   if (!response.ok) {
-        //     throw new GraphQLError(
-        //       `Google Drive API returned a non-200 status code: ${response.status}`
-        //     );
-        //   }
+          if (!response.ok) {
+            throw new GraphQLError(
+              `Google Drive API returned a non-200 status code: ${response.status}`
+            );
+          }
 
-        //   const json = await response.json();
-        //   files = json.files ?? [];
-        // } else if (id) {
-        //   const url = `https://www.googleapis.com/drive/v3/files/${id}?fields=id,name,thumbnailLink`;
+          const json = await response.json();
+          files = json.files ?? [];
+        } else if (id) {
+          const url = `https://www.googleapis.com/drive/v3/files/${id}?fields=id,name,thumbnailLink`;
 
-        //   const response = await fetch(url, {
-        //     method: 'GET',
-        //     headers: {
-        //       Accept: 'application/json',
-        //       'content-type': 'application/json',
-        //     },
-        //   });
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'content-type': 'application/json',
+            },
+          });
 
-        //   if (!response.ok) {
-        //     throw new GraphQLError(
-        //       `Google Drive API returned a non-200 status code: ${response.status}`
-        //     );
-        //   }
+          if (!response.ok) {
+            throw new GraphQLError(
+              `Google Drive API returned a non-200 status code: ${response.status}`
+            );
+          }
 
-        //   const json = await response.json();
-        //   files = [json];
-        // }
+          const json = await response.json();
+          files = [json];
+        }
 
         // console.log('Google Drive API Response:', files);
         return {
-          items: [
-            {
-              id: '1234',
-              title: 'Sample File',
-              image: 'https://example.com/image.png',
-            },
-          ],
+          items: files.map((file: any) => ({
+            id: file.id,
+            title: file.name,
+            image: file.thumbnailLink,
+          })),
         };
       },
     },
