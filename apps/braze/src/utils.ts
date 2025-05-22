5fade946dd9396a765dfef7f061f53f72025b135
JuliRossi <juliana.rossi@external.contentful.com>
2025-05-22 10:04:26 -0300

    Braze app: Fixing dialog multiselect height [INTEG-2617] (#9849)
    * Fixing dialog multiselect height
    
    * Clarifying height numbers

--- apps/braze/src/utils.ts
+++ apps/braze/src/utils.ts
@@ -37,6 +37,8 @@ export const CONFIG_CONTENT_TYPE_ID = 'brazeConfig';
 export const CONFIG_FIELD_ID = 'connectedFields';
 export const CONFIG_ENTRY_ID = 'brazeConfig';
 
+export const MULTISELECT_DIALOG_HEIGHT = 36 * 4; // Shows 4 items
+
 export enum EntryStatus {
   Draft = 'DRAFT',
   Changed = 'CHANGED',
