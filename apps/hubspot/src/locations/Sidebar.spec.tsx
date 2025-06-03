7ba8145cb6dae3e7bfc9f46230998d7b17f26316
ryunsong-contentful <124832189+ryunsong-contentful@users.noreply.github.com>
2025-06-03 10:54:53 -0600

    feat: initialize hubspot app (#9887)

---
+++ apps/hubspot/src/locations/Sidebar.spec.tsx
@@ -0,0 +1,17 @@
+import Sidebar from './Sidebar';
+import { render } from '@testing-library/react';
+import { mockCma, mockSdk } from '../../test/mocks';
+import { vi } from 'vitest';
+
+vi.mock('@contentful/react-apps-toolkit', () => ({
+  useSDK: () => mockSdk,
+  useCMA: () => mockCma,
+}));
+
+describe('Sidebar component', () => {
+  it('Component text exists', () => {
+    const { getByText } = render(<Sidebar />);
+
+    expect(getByText('Hello Sidebar Component (AppId: test-app)')).toBeInTheDocument();
+  });
+});
