8cd59a99f38c0b74a473aac8bd6f61b3b345c7ce
francobanfi <franco.banfi@external.contentful.com>
2025-06-13 16:46:45 -0300

    removing some locations and moving tests files

--- apps/hubspot/test/locations/Sidebar.spec.tsx
+++ apps/hubspot/test/locations/Sidebar.spec.tsx
@@ -1,6 +1,6 @@
-import Sidebar from './Sidebar';
+import Sidebar from '../../src/locations/Sidebar';
 import { render } from '@testing-library/react';
-import { mockCma, mockSdk } from '../../test/mocks';
+import { mockCma, mockSdk } from '../mocks';
 import { vi } from 'vitest';
 
 vi.mock('@contentful/react-apps-toolkit', () => ({
