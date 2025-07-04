48e33192271b92ff8c70d54a7339ec792ef237a2
joaquin <joaquin.casal@10pines.com>
2025-07-04 11:21:09 -0300

    Update config screen instructions and video

--- apps/hubspot/src/utils/utils.ts
+++ apps/hubspot/src/utils/utils.ts
@@ -10,6 +10,7 @@ export const CONFIG_SCREEN_INSTRUCTIONS = [
   'Navigate to your Hubspot account settings',
   "In the left hand navigation, click 'Integrations' and select 'Private apps' from the sub menu",
   'Create a new private app',
+  "Your private app must include the scope 'content'",
   "Within your private app, navigate to the 'Auth' tab. There, you can view and copy your private app access token.",
   'Paste your private app access token in the field above',
 ];
