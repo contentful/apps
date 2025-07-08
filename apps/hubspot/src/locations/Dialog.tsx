b61de00ee3619226481b2b438683d34d45929e99
joaquin <joaquin.casal@10pines.com>
2025-07-08 08:56:39 -0300

    Persist error in sidebar

--- apps/hubspot/src/locations/Dialog.tsx
+++ apps/hubspot/src/locations/Dialog.tsx
@@ -87,7 +87,6 @@ const Dialog = () => {
       );
       const { success, failed, invalidToken, missingScopes } = JSON.parse(response.response.body);
       showResults(success, failed, invalidToken, missingScopes);
-      sdk.close();
     } catch (error) {
       console.error('Error creating modules: ', error);
     } finally {
@@ -101,25 +100,25 @@ const Dialog = () => {
     invalidToken: boolean,
     missingScopes: boolean
   ) => {
-    const successMessage = `${success.length} entry field${
-      success.length === 1 ? '' : 's'
-    } successfully synced`;
-    const failedMessage = `${failed.length} entry field${
-      failed.length === 1 ? '' : 's'
-    } did not sync, please try again`;
-    const mixedMessage = `${successMessage} but ${failedMessage}`;
+    const successfullFields = `${success.length} entry field${success.length === 1 ? '' : 's'}`;
+    const failedFields = `${failed.length} entry field${failed.length === 1 ? '' : 's'}`;
 
-    if (invalidToken) {
-      sdk.notifier.error('Invalid Hubspot access token.');
-    } else if (missingScopes) {
-      sdk.notifier.error('The Hubspot token is missing the required "content" scope.');
+    if (invalidToken || missingScopes) {
+      sdk.notifier.error(
+        'There is an error with your Hubspot private app access token, and entry fields did not sync.'
+      );
+      sdk.close(true);
+      return;
     } else if (failed.length > 0 && success.length > 0) {
-      sdk.notifier.warning(mixedMessage);
+      sdk.notifier.warning(
+        `${successfullFields} were successfully synced, but ${failedFields} did not sync.`
+      );
     } else if (failed.length > 0) {
-      sdk.notifier.error(`${failedMessage}.`);
+      sdk.notifier.error(`${failedFields} did not sync, please try again.`);
     } else {
-      sdk.notifier.success(`${successMessage}.`);
+      sdk.notifier.success(`${successfullFields} successfully synced.`);
     }
+    sdk.close(false);
   };
 
   return (
