94f471a11be63abcf432aebf22d97e3f8b450448
Juli Rossi <juliana.rossi@external.contentful.com>
2025-07-11 16:04:36 -0300

    Hubspot: Enhance text field handling in module creation [INTEG-2785]
    * Added support for long text fields in createModules.ts
    * Introduced LONG_TEXT_MODULE_TEMPLATE in templates.ts
    * Updated logic to format text field values as HTML paragraphs

--- apps/hubspot/functions/createModules.ts
+++ apps/hubspot/functions/createModules.ts
@@ -87,3 +87,117 @@ const createModule = async (field: SelectedSdkField, token: string) => {
   await createModuleFile(fieldsFile, 'fields.json', moduleName, token);
   await createModuleFile(moduleFile, 'module.html', moduleName, token);
 };
+
+const createModuleFile = async (
+  file: string,
+  fileName: string,
+  moduleName: string,
+  token: string
+) => {
+  const fileBuffer = Buffer.from(file);
+  const url = `https://api.hubapi.com/cms/v3/source-code/published/content/${moduleName}.module/${fileName}`;
+  const formData = new FormData();
+  const type = fileName.endsWith('json') ? 'application/json' : 'text/html';
+  formData.append('file', new Blob([fileBuffer], { type: type }), fileName);
+
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    if (error?.category === 'INVALID_AUTHENTICATION') {
+      throw new InvalidHubspotTokenError(error.message);
+    }
+    if (error?.category === 'MISSING_SCOPES') {
+      throw new MissingHubspotScopesError(error.message);
+    }
+    const errorData = await response.text();
+    throw new Error(
+      `HubSpot API request failed: ${response.status} ${response.statusText} - ${errorData}`
+    );
+  }
+};
+
+const getFiles = (field: SelectedSdkField): { fieldsFile: string; moduleFile: string } => {
+  const { type } = field;
+  let fieldsFile;
+  let moduleFile;
+  switch (type) {
+    case 'Symbol':
+      fieldsFile = structuredClone(TEXT_FIELD_TEMPLATE);
+      if (field.value) fieldsFile[0].default = field.value;
+      moduleFile = TEXT_MODULE_TEMPLATE;
+      break;
+    case 'Text':
+      fieldsFile = structuredClone(RICH_TEXT_FIELD_TEMPLATE);
+      if (field.value) {
+        const lines: string[] = field.value.split('\n');
+        fieldsFile[0].default = lines.map((line) => `<p>${line}</p>`).join('');
+      }
+      moduleFile = LONG_TEXT_MODULE_TEMPLATE;
+      break;
+    case 'RichText':
+      fieldsFile = structuredClone(RICH_TEXT_FIELD_TEMPLATE);
+      if (field.value) fieldsFile[0].default = documentToHtmlString(field.value);
+      moduleFile = RICH_TEXT_MODULE_TEMPLATE;
+      break;
+    case 'Number':
+    case 'Integer':
+      fieldsFile = structuredClone(NUMBER_FIELD_TEMPLATE);
+      if (field.value) fieldsFile[0].default = field.value;
+      moduleFile = NUMBER_MODULE_TEMPLATE;
+      break;
+    case 'Date':
+      const value = field.value as string;
+      if (!value || value.includes('T')) {
+        fieldsFile = structuredClone(DATETIME_FIELD_TEMPLATE);
+        moduleFile = DATETIME_MODULE_TEMPLATE;
+      } else {
+        fieldsFile = structuredClone(DATE_FIELD_TEMPLATE);
+        moduleFile = DATE_MODULE_TEMPLATE;
+      }
+      fieldsFile[0].default = new Date(value).getTime();
+      break;
+    case 'Location':
+      fieldsFile = structuredClone(TEXT_FIELD_TEMPLATE);
+      if (field.value) fieldsFile[0].default = `lat:${field.value.lat}, long:${field.value.lon}`;
+      moduleFile = TEXT_MODULE_TEMPLATE;
+      break;
+    case 'Array':
+      fieldsFile = structuredClone(TEXT_FIELD_TEMPLATE);
+      if (field.value) fieldsFile[0].default = field.value.join(', ');
+      moduleFile = TEXT_MODULE_TEMPLATE;
+      break;
+    case 'Link':
+      fieldsFile = structuredClone(IMAGE_FIELD_TEMPLATE);
+      if (field.value) {
+        fieldsFile[0].default.src = field.value.url;
+        fieldsFile[0].default.width = field.value.width;
+        fieldsFile[0].default.height = field.value.height;
+      }
+      moduleFile = IMAGE_MODULE_TEMPLATE;
+      break;
+    default:
+      throw new Error(`Unsupported field type: ${type}`);
+  }
+  return { fieldsFile: JSON.stringify(fieldsFile), moduleFile };
+};
+
+class InvalidHubspotTokenError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalidHubspotTokenError';
+  }
+}
+
+class MissingHubspotScopesError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'MissingHubspotScopesError';
+  }
+}
