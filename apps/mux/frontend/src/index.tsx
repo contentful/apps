9a979ef26c46047d6819673fdf676de025b62b5a
Sam Ashley <166558611+sam-is-content@users.noreply.github.com>
2025-08-14 13:21:55 -0600

    use getmanyforenv for app actions

--- apps/mux/frontend/src/index.tsx
+++ apps/mux/frontend/src/index.tsx
@@ -53,7 +53,6 @@ import {
   generateAutoCaptions,
 } from './util/muxApi';
 import Sidebar from './locations/Sidebar';
-import { APP_ORGANIZATION_ID, isMarketplaceVersion } from './util/constants';
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -105,7 +104,6 @@ export class App extends React.Component<AppProps, AppState> {
   muxPlayerRef = React.createRef<any>(); // eslint-disable-line @typescript-eslint/no-explicit-any
   getSignedTokenActionId: string;
   private pollPending = false;
-  orgId: string;
 
   constructor(props: AppProps) {
     super(props);
@@ -153,10 +151,6 @@ export class App extends React.Component<AppProps, AppState> {
       storyboardToken: undefined,
       raw: undefined,
     };
-
-    this.orgId = isMarketplaceVersion({ appId: this.props.sdk.ids.app })
-      ? APP_ORGANIZATION_ID
-      : this.props.sdk.ids.organization;
   }
 
   // eslint-disable-next-line  @typescript-eslint/ban-types
@@ -212,10 +206,7 @@ export class App extends React.Component<AppProps, AppState> {
   };
 
   async componentDidMount() {
-    const appActionsResponse = await this.cmaClient.appAction.getMany({
-      organizationId: this.orgId,
-      appDefinitionId: this.props.sdk.ids.app!,
-    });
+    const appActionsResponse = await this.cmaClient.appAction.getManyForEnvironment({});
     this.getSignedTokenActionId =
       appActionsResponse.items.find((x) => x.name === 'getSignedUrlTokens')?.sys.id ?? '';
 
@@ -509,7 +500,7 @@ export class App extends React.Component<AppProps, AppState> {
         response: { body },
       } = await this.cmaClient.appActionCall.createWithResponse(
         {
-          organizationId: this.orgId,
+          organizationId: this.props.sdk.ids.organization,
           appDefinitionId: this.props.sdk.ids.app!,
           appActionId: this.getSignedTokenActionId,
         },
