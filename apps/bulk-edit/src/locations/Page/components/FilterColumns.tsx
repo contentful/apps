4eb8d98c7a6cf6121b7fe99b4b95a3f8d62c7a45
Juli Rossi <juliana.rossi@external.contentful.com>
2025-08-26 15:47:01 -0300

    select all

--- apps/bulk-edit/src/locations/Page/components/FilterColumns.tsx
+++ apps/bulk-edit/src/locations/Page/components/FilterColumns.tsx
@@ -1,6 +1,9 @@
 import { Flex, Text } from '@contentful/f36-components';
 import { Multiselect, MultiselectOption } from '@contentful/f36-multiselect';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
+import { multiselectOverrides } from './FieldColumns.styles';
 
 const options = [
   { label: 'Display name', value: 'displayName' },
@@ -16,10 +19,23 @@ const FilterColumns = ({
   selectedFields: { label: string; value: string }[];
   setSelectedFields: (fields: { label: string; value: string }[]) => void;
 }) => {
+  const toggleAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = e.target.checked;
+    if (checked) {
+      setSelectedFields(options);
+    } else {
+      setSelectedFields([]);
+    }
+  };
+
+  const areAllSelected = useMemo(() => {
+    return options.every((option) => selectedFields.some((field) => field.value === option.value));
+  }, [selectedFields, options]);
+
   return (
     <Flex style={{ maxWidth: '500px' }} gap="spacing2Xs" flexDirection="column">
-      <Text fontWeight="fontWeightDemiBold">Filter by field</Text>
-      <Multiselect placeholder="Select one or more">
+      <Multiselect placeholder="Filter columns" css={multiselectOverrides}>
+        <Multiselect.SelectAll onSelectItem={toggleAll} isChecked={areAllSelected} />
         {options.map((option) => (
           <MultiselectOption
             key={option.value}
