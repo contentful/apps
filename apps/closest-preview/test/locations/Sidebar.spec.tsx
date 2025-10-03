05b7c3d3c3946c786c520fab586a15733bf9d5a3
joaquin <joaquin.casal@10pines.com>
2025-10-03 16:19:00 -0300

    Add sidebar ui

--- apps/closest-preview/test/locations/Sidebar.spec.tsx
+++ apps/closest-preview/test/locations/Sidebar.spec.tsx
@@ -1,17 +1,23 @@
 import Sidebar from '../../src/locations/Sidebar';
-import { render } from '@testing-library/react';
-import { mockCma, mockSdk } from '../mocks';
+import { render, waitFor } from '@testing-library/react';
+import { mockSdk } from '../mocks';
 import { vi } from 'vitest';
 
 vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
-  useCMA: () => mockCma,
+  useAutoResizer: () => {},
 }));
 
 describe('Sidebar component', () => {
-  it('Component text exists', () => {
-    const { getByText } = render(<Sidebar />);
+  it('Renders entry list with titles and relative dates', async () => {
+    const { getAllByText } = render(<Sidebar />);
 
-    expect(getByText('Hello Sidebar Component (AppId: test-app)')).toBeInTheDocument();
+    await waitFor(() => {
+      const entryTitles = getAllByText('Entry id', { exact: false });
+      expect(entryTitles).toHaveLength(5);
+    });
+
+    const updatedTexts = getAllByText(/Updated/);
+    expect(updatedTexts).toHaveLength(5);
   });
 });
