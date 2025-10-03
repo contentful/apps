e6959dc6f28a04b8a1150d3484a0ed95d98a0077
Franco Banfi <62450599+FBanfi@users.noreply.github.com>
2025-10-03 09:25:02 -0300

    Closest Preview: Adding Config Screen [MAPS-7] (#10143)
    * adding config screen for Closest Preview
    
    * changing interface for type
    
    * updating fomra version + initial version of content type preview exclusion
    
    * encapsulating live preview content types fetch

--- apps/closest-preview/src/locations/ConfigScreen.tsx
+++ apps/closest-preview/src/locations/ConfigScreen.tsx
@@ -1,67 +1,76 @@
 import { useCallback, useState, useEffect } from 'react';
 import { ConfigAppSDK } from '@contentful/app-sdk';
-import { Heading, Form, Paragraph, Flex } from '@contentful/f36-components';
-import { css } from 'emotion';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
-
-export interface AppInstallationParameters {}
+import { Heading, Form, Paragraph, Flex, Box, FormControl } from '@contentful/f36-components';
+import { useSDK } from '@contentful/react-apps-toolkit';
+import ContentTypeMultiSelect from '../components/ContentTypeMultiSelect';
+import { ContentType } from '../types';
 
 const ConfigScreen = () => {
-  const [parameters, setParameters] = useState<AppInstallationParameters>({});
   const sdk = useSDK<ConfigAppSDK>();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
+  const [selectedContentTypes, setSelectedContentTypes] = useState<ContentType[]>([]);
 
   const onConfigure = useCallback(async () => {
-    // This method will be called when a user clicks on "Install"
-    // or "Save" in the configuration screen.
-    // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
+    const editorInterface = selectedContentTypes.reduce((acc, contentType) => {
+      return {
+        ...acc,
+        [contentType.id]: {
+          sidebar: { position: 0 },
+        },
+      };
+    }, {});
 
-    // Get current the state of EditorInterface and other entities
-    // related to this app installation
     const currentState = await sdk.app.getCurrentState();
 
     return {
-      // Parameters to be persisted as the app configuration.
-      parameters,
-      // In case you don't want to submit any update to app
-      // locations, you can just pass the currentState as is
-      targetState: currentState,
+      targetState: {
+        ...currentState,
+        EditorInterface: editorInterface,
+      },
     };
-  }, [parameters, sdk]);
+  }, [sdk, selectedContentTypes]);
 
   useEffect(() => {
-    // `onConfigure` allows to configure a callback to be
-    // invoked when a user attempts to install the app or update
-    // its configuration.
     sdk.app.onConfigure(() => onConfigure());
   }, [sdk, onConfigure]);
 
   useEffect(() => {
     (async () => {
-      // Get current parameters of the app.
-      // If the app is not installed yet, `parameters` will be `null`.
-      const currentParameters: AppInstallationParameters | null = await sdk.app.getParameters();
-
-      if (currentParameters) {
-        setParameters(currentParameters);
-      }
-
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
       sdk.app.setReady();
     })();
-  }, [sdk]);
+  }, []);
 
   return (
-    <Flex flexDirection="column" className={css({ margin: '80px', maxWidth: '800px' })}>
-      <Form>
-        <Heading>App Config</Heading>
-        <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
-      </Form>
+    <Flex justifyContent="center" alignItems="flex-start">
+      <Box margin="spacing2Xl">
+        <Form>
+          <Heading as="h2" marginBottom="spacingS">
+            Set up Closest Preview
+          </Heading>
+          <Paragraph marginBottom="spacingXl">
+            Closest Preview allows users to quickly navigate to the closest page level element for a
+            given entry in order to preview the item.
+          </Paragraph>
+
+          <Box marginBottom="spacing2Xl">
+            <Heading as="h3" marginBottom="spacingXs">
+              Assign content types
+            </Heading>
+            <Paragraph marginBottom="spacingL">
+              Select the content type(s) you want to use with Closest Preview. You can change this
+              anytime by navigating to the 'Sidebar' tab in your content model.
+            </Paragraph>
+            <FormControl id="contentTypes">
+              <FormControl.Label>Content types</FormControl.Label>
+              <ContentTypeMultiSelect
+                selectedContentTypes={selectedContentTypes}
+                setSelectedContentTypes={setSelectedContentTypes}
+                sdk={sdk}
+                cma={sdk.cma}
+              />
+            </FormControl>
+          </Box>
+        </Form>
+      </Box>
     </Flex>
   );
 };
--- apps/closest-preview/test/locations/ConfigScreen.spec.tsx
+++ apps/closest-preview/test/locations/ConfigScreen.spec.tsx
@@ -1,24 +1,211 @@
+import React from 'react';
+import { render, screen, waitFor, cleanup, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { vi, describe, it, beforeEach, afterEach, expect } from 'vitest';
 import ConfigScreen from '../../src/locations/ConfigScreen';
-import { act, render } from '@testing-library/react';
-import { mockCma, mockSdk } from '../mocks';
-import { vi } from 'vitest';
+import { mockSdk, mockCma } from '../mocks';
 
 vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
   useCMA: () => mockCma,
 }));
 
-describe('Config Screen component', () => {
-  it('Component text exists', async () => {
-    const { getByText } = render(<ConfigScreen />);
+async function saveAppInstallation() {
+  return await mockSdk.app.onConfigure.mock.calls.at(-1)[0]();
+}
 
-    // simulate the user clicking the install button
-    await act(async () => {
-      await mockSdk.app.onConfigure.mock.calls[0][0]();
+describe('ConfigScreen', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    mockSdk.app.getParameters.mockResolvedValue({});
+    mockSdk.app.getCurrentState.mockResolvedValue({});
+    mockSdk.app.setReady.mockResolvedValue();
+    mockSdk.app.onConfigure.mockImplementation((cb: () => Promise<any>) => {
+      // Simulate Contentful's onConfigure callback registration
+      mockSdk._onConfigure = cb;
+    });
+    mockSdk.ids.space = 'test-space';
+    mockSdk.cma = mockCma;
+    mockCma.contentType = {
+      getMany: vi.fn().mockResolvedValue({ items: [] }),
+    };
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('renders all main sections and UI elements', async () => {
+    render(<ConfigScreen />);
+    await waitFor(() => {
+      expect(screen.getByText('Set up Closest Preview')).toBeInTheDocument();
+      expect(screen.getByText('Assign content types')).toBeInTheDocument();
+      expect(screen.getByText('Content types')).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          'Closest Preview allows users to quickly navigate to the closest page level element for a given entry in order to preview the item.'
+        )
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          "Select the content type(s) you want to use with Closest Preview. You can change this anytime by navigating to the 'Sidebar' tab in your content model."
+        )
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('renders content type multi-select and allows selection', async () => {
+    mockCma.contentType.getMany.mockResolvedValue({
+      items: [
+        { sys: { id: 'blogPost' }, name: 'Blog Post' },
+        { sys: { id: 'article' }, name: 'Article' },
+      ],
+    });
+    render(<ConfigScreen />);
+    const autocomplete = await screen.findByPlaceholderText('Search content types');
+    await userEvent.click(autocomplete);
+    await userEvent.type(autocomplete, 'Blog');
+
+    const option = await screen.findByText('Blog Post');
+    await userEvent.click(option);
+
+    expect(await screen.findByTestId('pill-Blog Post')).toBeInTheDocument();
+  });
+
+  it('handles empty state for content types', async () => {
+    mockCma.contentType.getMany.mockResolvedValue({ items: [] });
+    render(<ConfigScreen />);
+    const autocomplete = await screen.findByPlaceholderText('Search content types');
+    await userEvent.click(autocomplete);
+    expect(screen.queryByText('No matches found')).toBeInTheDocument();
+  });
+
+  it('successfully configures app with selected content types', async () => {
+    mockCma.contentType.getMany.mockResolvedValue({
+      items: [
+        { sys: { id: 'blogPost' }, name: 'Blog Post' },
+        { sys: { id: 'article' }, name: 'Article' },
+      ],
     });
 
-    expect(
-      getByText('Welcome to your contentful app. This is your config page.')
-    ).toBeInTheDocument();
+    render(<ConfigScreen />);
+
+    const autocomplete = await screen.findByPlaceholderText('Search content types');
+    await userEvent.click(autocomplete);
+
+    const blogPostOption = await screen.findByText('Blog Post');
+    await userEvent.click(blogPostOption);
+
+    const articleOption = await screen.findByText('Article');
+    await userEvent.click(articleOption);
+
+    expect(await screen.findByTestId('pill-Blog Post')).toBeInTheDocument();
+    expect(await screen.findByTestId('pill-Article')).toBeInTheDocument();
+
+    const result = await act(async () => {
+      return await saveAppInstallation();
+    });
+
+    expect(result).toEqual({
+      parameters: {},
+      targetState: {
+        EditorInterface: {
+          blogPost: {
+            sidebar: { position: 0 },
+          },
+          article: {
+            sidebar: { position: 0 },
+          },
+        },
+      },
+    });
+  });
+
+  it('handles configuration with no content types selected', async () => {
+    render(<ConfigScreen />);
+
+    const result = await act(async () => {
+      return await saveAppInstallation();
+    });
+
+    expect(result).toEqual({
+      parameters: {},
+      targetState: {
+        EditorInterface: {},
+      },
+    });
+  });
+
+  it('loads existing parameters on mount', async () => {
+    const existingParameters = { someParam: 'value' };
+    mockSdk.app.getParameters.mockResolvedValue(existingParameters);
+
+    render(<ConfigScreen />);
+
+    await waitFor(() => {
+      expect(mockSdk.app.getParameters).toHaveBeenCalled();
+      expect(mockSdk.app.setReady).toHaveBeenCalled();
+    });
+  });
+
+  it('registers onConfigure callback on mount', async () => {
+    render(<ConfigScreen />);
+
+    await waitFor(() => {
+      expect(mockSdk.app.onConfigure).toHaveBeenCalled();
+    });
+  });
+
+  it('allows removing selected content types', async () => {
+    mockCma.contentType.getMany.mockResolvedValue({
+      items: [
+        { sys: { id: 'blogPost' }, name: 'Blog Post' },
+        { sys: { id: 'article' }, name: 'Article' },
+      ],
+    });
+
+    render(<ConfigScreen />);
+
+    const autocomplete = await screen.findByPlaceholderText('Search content types');
+    await userEvent.click(autocomplete);
+
+    const blogPostOption = await screen.findByText('Blog Post');
+    await userEvent.click(blogPostOption);
+
+    const articleOption = await screen.findByText('Article');
+    await userEvent.click(articleOption);
+
+    expect(await screen.findByTestId('pill-Blog Post')).toBeInTheDocument();
+    expect(await screen.findByTestId('pill-Article')).toBeInTheDocument();
+
+    const closeButtons = await screen.findAllByLabelText('Close');
+    const blogPostCloseButton = closeButtons.find((button) =>
+      button.closest('[data-test-id="pill-Blog Post"]')
+    );
+    await userEvent.click(blogPostCloseButton!);
+
+    expect(screen.queryByTestId('pill-Blog Post')).not.toBeInTheDocument();
+    expect(screen.getByTestId('pill-Article')).toBeInTheDocument();
+  });
+
+  it('filters content types based on search input', async () => {
+    mockCma.contentType.getMany.mockResolvedValue({
+      items: [
+        { sys: { id: 'blogPost' }, name: 'Blog Post' },
+        { sys: { id: 'article' }, name: 'Article' },
+        { sys: { id: 'newsItem' }, name: 'News Item' },
+      ],
+    });
+
+    render(<ConfigScreen />);
+
+    const autocomplete = await screen.findByPlaceholderText('Search content types');
+    await userEvent.click(autocomplete);
+
+    await userEvent.type(autocomplete, 'Blog');
+
+    expect(await screen.findByText('Blog Post')).toBeInTheDocument();
+    expect(screen.queryByText('Article')).not.toBeInTheDocument();
+    expect(screen.queryByText('News Item')).not.toBeInTheDocument();
   });
 });
