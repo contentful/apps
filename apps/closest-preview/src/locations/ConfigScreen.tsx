6095c42edcbff16b9b82a2eaa68e3eb8bd669b2c
joaquin <joaquin.casal@10pines.com>
2025-10-14 11:53:23 -0300

    Add note with live preview instructions in config screen

--- apps/closest-preview/src/locations/ConfigScreen.tsx
+++ apps/closest-preview/src/locations/ConfigScreen.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useState, useEffect } from 'react';
 import { ConfigAppSDK } from '@contentful/app-sdk';
-import { Heading, Form, Paragraph, Flex, Box, FormControl } from '@contentful/f36-components';
+import { Heading, Form, Paragraph, Flex, Box, FormControl, Note } from '@contentful/f36-components';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import ContentTypeMultiSelect from '../components/ContentTypeMultiSelect';
 import { ContentType } from '../types';
+import { styles } from './ConfigScreen.styles';
 
 const ConfigScreen = () => {
   const sdk = useSDK<ConfigAppSDK>();
@@ -40,8 +41,8 @@ const ConfigScreen = () => {
   }, []);
 
   return (
-    <Flex justifyContent="center" alignItems="flex-start">
-      <Box margin="spacing2Xl">
+    <Flex justifyContent="center" alignItems="center">
+      <Box margin="spacing2Xl" className={styles.body}>
         <Form>
           <Heading as="h2" marginBottom="spacingS">
             Set up Closest Preview
@@ -69,6 +70,11 @@ const ConfigScreen = () => {
               />
             </FormControl>
           </Box>
+          <Note>
+            This app assumes that all content types with Live Preview enabled include a field whose
+            id is 'slug'. If that field is missing or uses a different id, the app will not function
+            correctly.
+          </Note>
         </Form>
       </Box>
     </Flex>
