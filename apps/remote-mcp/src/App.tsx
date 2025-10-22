16c17fe8f7092dbd20bd59ec095230f36437a45d
Tyler Pina <tyler.pina@contentful.com>
2025-10-22 08:40:06 -0600

    feat: app framework created

---
+++ apps/remote-mcp/src/App.tsx
@@ -0,0 +1,24 @@
+import { useMemo } from 'react';
+import { locations } from '@contentful/app-sdk';
+import ConfigScreen from './locations/ConfigScreen';
+import { useSDK } from '@contentful/react-apps-toolkit';
+
+const ComponentLocationSettings = {
+  [locations.LOCATION_APP_CONFIG]: ConfigScreen,
+};
+
+const App = () => {
+  const sdk = useSDK();
+
+  const Component = useMemo(() => {
+    for (const [location, component] of Object.entries(ComponentLocationSettings)) {
+      if (sdk.location.is(location)) {
+        return component;
+      }
+    }
+  }, [sdk.location]);
+
+  return Component ? <Component /> : null;
+};
+
+export default App;
