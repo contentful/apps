edfe8fa920539755e83772b5a0b21d03e091b1dc
JuliRossi <juliana.rossi@external.contentful.com>
2025-11-03 10:18:48 -0300

    Add manifest to slack [] (#10222)
    * Update Node.js version in Dockerfile and serverless.yml to 18.x
    
    * updating controller to have response
    
    * wip: new manifest for slack app
    
    * updating backend packages
    
    * updating fe deps
    
    * adding script
    
    * refactoring
    
    * Updating test
    
    * Fixing old tests
    
    * Removing unused dep
    
    * Revert "Update Node.js version in Dockerfile and serverless.yml to 18.x"
    
    This reverts commit f7ca3bcc7e81bfee8ffa93952f6fd032d6b73023.
    
    * Updating instructions
    
    * Revert "updating fe deps"
    
    This reverts commit 8f3796f1bb4b0fbfe40a28f9bb04f43d8321b6b9.
    
    * Revert "updating backend packages"
    
    This reverts commit 8756b0eb590959ec413a37242604fb8cf8d1edc0.
    
    * Moving everything to the back
    
    * Avoiding updating lambda deps and making script simpler
    
    * Removing unnecessary export

--- apps/slack/lambda/lib/errors/index.ts
+++ apps/slack/lambda/lib/errors/index.ts
@@ -1,4 +1,5 @@
 export { errorMiddleware } from './middleware';
 export { NotFoundException } from './not-found';
 export { UnprocessableEntityException } from './unprocessable-entity';
+export { ConflictException } from './conflict';
 export { SlackError } from './slack-error';
