42c88077340de853fc836e7c7289eac1c4d3fcc3
Juli Rossi <juliana.rossi@external.contentful.com>
2025-11-05 16:25:35 -0300

    Fix lint

--- apps/auto-internal-name/src/locations/ConfigScreen.tsx
+++ apps/auto-internal-name/src/locations/ConfigScreen.tsx
@@ -4,6 +4,7 @@ import { useSDK } from '@contentful/react-apps-toolkit';
 import { css } from 'emotion';
 import { useCallback, useEffect, useState } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface AppInstallationParameters {}
 
 const ConfigScreen = () => {
