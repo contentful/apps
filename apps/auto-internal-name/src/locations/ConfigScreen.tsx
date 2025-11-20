76380849d0a6d6a86bda1cf074acef732bb19c62
francobanfi <franco.banfi@external.contentful.com>
2025-11-20 18:47:28 -0300

    removing ConfigureSection.tsx

--- apps/auto-internal-name/src/locations/ConfigScreen.tsx
+++ apps/auto-internal-name/src/locations/ConfigScreen.tsx
@@ -1,11 +1,25 @@
 import { ConfigAppSDK } from '@contentful/app-sdk';
-import { Box, Flex, Form, Heading, Paragraph } from '@contentful/f36-components';
+import {
+  Box,
+  Flex,
+  Form,
+  Heading,
+  Paragraph,
+  FormControl,
+  TextInput,
+  Autocomplete,
+} from '@contentful/f36-components';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import { useCallback, useEffect, useState } from 'react';
 import { styles } from './ConfigScreen.styles';
 import OverrideSection from '../components/OverrideSection';
-import ConfigureSection from '../components/ConfigureSection';
-import { AppInstallationParameters, OverrideState, Override } from '../utils/types';
+import {
+  AppInstallationParameters,
+  OverrideState,
+  Override,
+  AutocompleteItem,
+} from '../utils/types';
+import { getUniqueShortTextFields, normalizeString } from '../utils/override';
 
 const ConfigScreen = () => {
   const sdk = useSDK<ConfigAppSDK>();
@@ -17,6 +31,8 @@ const ConfigScreen = () => {
 
   const [isSourceFieldMissing, setIsSourceFieldMissing] = useState<boolean>(false);
   const [overridesAreInvalid, setOverridesAreInvalid] = useState<OverrideState>({});
+  const [fields, setFields] = useState<AutocompleteItem[]>([]);
+  const [filteredSourceFields, setFilteredSourceFields] = useState<AutocompleteItem[]>([]);
 
   const onConfigure = useCallback(async () => {
     const currentState = await sdk.app.getCurrentState();
@@ -62,11 +78,51 @@ const ConfigScreen = () => {
     })();
   }, [sdk]);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const contentTypes = await sdk.cma.contentType.getMany({
+          spaceId: sdk.ids.space,
+          environmentId: sdk.ids.environment,
+        });
+
+        const uniqueFields = getUniqueShortTextFields(contentTypes);
+
+        setFields(uniqueFields);
+        setFilteredSourceFields(uniqueFields);
+      } catch (error) {
+        console.warn('[Error] Failed to load source fields:', error);
+      }
+    })();
+  }, [sdk]);
+
   const handleOverridesChange = (updater: (prev: Override[]) => Override[]) => {
     setParameters((prev) => ({
       ...prev,
       overrides: updater(prev.overrides),
     }));
+  };
+
+  const handleSourceFieldInputChange = (name: string) => {
+    if (!name) {
+      setParameters((prev) => ({ ...prev, sourceFieldId: '' }));
+      setFilteredSourceFields(fields);
+      return;
+    }
+
+    const newFilteredItems = fields.filter((item) =>
+      normalizeString(item.name).includes(normalizeString(name))
+    );
+    setFilteredSourceFields(newFilteredItems);
+  };
+
+  const handleSourceFieldIdSelection = (value: string) => {
+    const selectedField = fields.find(
+      (item) => normalizeString(item.name) === normalizeString(value)
+    );
+    if (selectedField) {
+      setParameters((prev) => ({ ...prev, sourceFieldId: selectedField.id }));
+    }
   };
 
   return (
@@ -88,15 +144,43 @@ const ConfigScreen = () => {
           </Paragraph>
         </Box>
 
-        <ConfigureSection
-          separator={parameters.separator}
-          sourceFieldId={parameters.sourceFieldId}
-          isSourceFieldMissing={isSourceFieldMissing}
-          onSeparatorChange={(value) => setParameters((prev) => ({ ...prev, separator: value }))}
-          onSourceFieldIdChange={(fieldId: string) =>
-            setParameters((prev) => ({ ...prev, sourceFieldId: fieldId }))
-          }
-        />
+        <Box>
+          <Heading as="h3">Configure</Heading>
+          <FormControl id="separator">
+            <FormControl.Label marginBottom="spacingS">Separator</FormControl.Label>
+            <TextInput
+              value={parameters.separator}
+              onChange={(e) => setParameters((prev) => ({ ...prev, separator: e.target.value }))}
+            />
+            <FormControl.HelpText marginTop="spacingS">
+              The separator can be any character or symbol and will append to the entry name.
+            </FormControl.HelpText>
+          </FormControl>
+          <FormControl id="sourceFieldId" isInvalid={isSourceFieldMissing}>
+            <FormControl.Label marginBottom="spacingS" isRequired>
+              Source field ID
+            </FormControl.Label>
+            <Autocomplete
+              selectedItem={
+                fields.find((field) => field.id === parameters.sourceFieldId)?.name || ''
+              }
+              items={filteredSourceFields.map((field) => field.name)}
+              onInputValueChange={handleSourceFieldInputChange}
+              onSelectItem={handleSourceFieldIdSelection}
+              placeholder="Search field name"
+            />
+            {isSourceFieldMissing && (
+              <FormControl.ValidationMessage>
+                Source field ID is required
+              </FormControl.ValidationMessage>
+            )}
+            <FormControl.HelpText marginTop="spacingS">
+              The source field ID should be the name of the field that you want to use from the
+              parent entry. This will be applied to any content types that include the same field
+              ID.
+            </FormControl.HelpText>
+          </FormControl>
+        </Box>
 
         <OverrideSection
           overrides={parameters.overrides}
