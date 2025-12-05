e7ef48a582136ced5a41b36ea40ed71cedd0eb58
Franco Banfi <62450599+FBanfi@users.noreply.github.com>
2025-12-05 17:35:51 -0300

    Auto Internal Name: Fix warnings in tests + config buttons alignment [MAPS-140] (#10314)
    * Fix act warning in tests and autocomplete buttons alignment
    
    * moving method + removing comment

--- apps/auto-internal-name/src/locations/ConfigScreen.styles.ts
+++ apps/auto-internal-name/src/locations/ConfigScreen.styles.ts
@@ -8,4 +8,11 @@ export const styles = {
   formControl: css({
     flex: 1,
   }),
+  autocomplete: css({
+    '& button': {
+      height: '100%',
+      display: 'flex',
+      alignItems: 'center',
+    },
+  }),
 };
