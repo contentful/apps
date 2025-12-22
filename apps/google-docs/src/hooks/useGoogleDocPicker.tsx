ca88b5ecfdb85d6e0561e3e3b2f38c1e73adff2f
harika kondur <harika.kondur@contentful.com>
2025-12-22 15:33:14 -0700

    chore:fix gapi path

--- apps/google-docs/src/hooks/useGoogleDocPicker.tsx
+++ apps/google-docs/src/hooks/useGoogleDocPicker.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import { loadGapi, loadPickerApi } from '../utils/googleApis';
+import { loadGapi, loadPickerApi } from '../utils/googleapis';
 
 type PickerCallbackData = {
   id: string;
