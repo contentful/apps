1377be645d296104ff609634e9218a4274993fe8
david shibley <david.shibley@contentful.com>
2026-01-30 14:19:28 -0700

    removing more console logs

--- apps/typeform/lambda/fetch-access-token.js
+++ apps/typeform/lambda/fetch-access-token.js
@@ -7,8 +7,6 @@ module.exports = async (code, origin, baseUrl, { fetch }) => {
   const effectiveBaseUrl = baseUrl || 'https://api.typeform.com';
   const ENDPOINT = `${effectiveBaseUrl}/oauth/token`;
 
-  console.log('CLIENT_ID', process.env.CLIENT_ID);
-  console.log('CLIENT_SECRET', process.env.CLIENT_SECRET);
   const body =
     `grant_type=authorization_code&code=${code}&client_id=${process.env.CLIENT_ID}&` +
     `client_secret=${process.env.CLIENT_SECRET}&redirect_uri=${encodeURIComponent(
