a51990ece9caec1004aa5d10b5cd44bc0461fe32
francobanfi <franco.banfi@external.contentful.com>
2026-02-11 09:50:52 -0300

    removing unused comments and fixing build

--- apps/marketo/src/locations/ConfigScreen.tsx
+++ apps/marketo/src/locations/ConfigScreen.tsx
@@ -8,49 +8,27 @@ const ConfigScreen = () => {
   const [parameters, setParameters] = useState({});
   const sdk = useSDK<ConfigAppSDK>();
 
-  /*
-     To use the cma, access it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = sdk.cma;
-
   const onConfigure = useCallback(async () => {
-    // This method will be called when a user clicks on "Install"
-    // or "Save" in the configuration screen.
-    // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
-
-    // Get current the state of EditorInterface and other entities
-    // related to this app installation
     const currentState = await sdk.app.getCurrentState();
 
     return {
-      // Parameters to be persisted as the app configuration.
       parameters,
-      // In case you don't want to submit any update to app
-      // locations, you can just pass the currentState as is
       targetState: currentState,
     };
   }, [parameters, sdk]);
 
   useEffect(() => {
-    // `onConfigure` allows to configure a callback to be
-    // invoked when a user attempts to install the app or update
-    // its configuration.
     sdk.app.onConfigure(() => onConfigure());
   }, [sdk, onConfigure]);
 
   useEffect(() => {
     (async () => {
-      // Get current parameters of the app.
-      // If the app is not installed yet, `parameters` will be `null`.
-      const currentParameters: AppInstallationParameters | null = await sdk.app.getParameters();
+      const currentParameters = await sdk.app.getParameters();
 
       if (currentParameters) {
         setParameters(currentParameters);
       }
 
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
       sdk.app.setReady();
     })();
   }, [sdk]);
