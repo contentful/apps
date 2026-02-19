495a4344cf12195b384d9a880eb834b61814b011
francobanfi <franco.banfi@external.contentful.com>
2026-02-19 14:29:17 -0300

    adding tests

--- apps/marketo/test/mocks/mockSdk.ts
+++ apps/marketo/test/mocks/mockSdk.ts
@@ -3,9 +3,12 @@ import { vi } from 'vitest';
 const mockSdk: any = {
   app: {
     onConfigure: vi.fn(),
-    getParameters: vi.fn().mockReturnValueOnce({}),
+    getParameters: vi.fn().mockResolvedValue(null),
     setReady: vi.fn(),
-    getCurrentState: vi.fn(),
+    getCurrentState: vi.fn().mockResolvedValue(null),
+  },
+  notifier: {
+    error: vi.fn(),
   },
   ids: {
     app: 'test-app',
