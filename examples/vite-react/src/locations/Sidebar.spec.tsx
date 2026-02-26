97b99ed422b29526b61eb3249037e34901a21e15
joaquin <joaquin.casal@10pines.com>
2026-02-26 02:22:57 -0300

    Update vite-react example

--- examples/vite-react/src/locations/Sidebar.spec.tsx
+++ examples/vite-react/src/locations/Sidebar.spec.tsx
@@ -1,11 +1,10 @@
 import { render } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
-import { mockCma, mockSdk } from '../../test/mocks';
+import { mockSdk } from '../../test/mocks';
 import Sidebar from './Sidebar';
 
 vi.mock('@contentful/react-apps-toolkit', () => ({
   useSDK: () => mockSdk,
-  useCMA: () => mockCma,
 }));
 
 describe('Sidebar component', () => {
