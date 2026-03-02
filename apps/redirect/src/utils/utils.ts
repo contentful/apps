d50e591e065e5e5e591140f117495b93d7ef5c99
francobanfi <franco.banfi@external.contentful.com>
2026-03-02 16:02:01 -0300

    first version of page filters

--- apps/redirect/src/utils/utils.ts
+++ apps/redirect/src/utils/utils.ts
@@ -2,3 +2,9 @@ export const truncateText = (text: string, maxLength: number): string => {
   if (text.length <= maxLength) return text;
   return text.slice(0, maxLength) + '...';
 };
+
+export const isNumericSearch = (query: string): boolean => {
+  const decimalPattern = /^-?\d*\.?\d+$/;
+  const integerPattern = /^-?\d+$/;
+  return decimalPattern.test(query.trim()) || integerPattern.test(query.trim());
+};
