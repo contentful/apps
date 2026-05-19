c8d1db686b6a48ca03100a72d2049979089f1f34
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2026-05-19 10:56:55 -0600

    revert: fix(rich-text-versioning): inject reference recommendations (#10993)
    This reverts commit 1b12204585c78cb01f20f835f5f9bc33c850a2c1.

--- apps/rich-text-versioning/src/locations/ConfigScreen.styles.ts
+++ apps/rich-text-versioning/src/locations/ConfigScreen.styles.ts
@@ -1,5 +1,5 @@
 import tokens from '@contentful/f36-tokens';
-import { css } from '@emotion/css';
+import { css } from 'emotion';
 
 export const styles = {
   configScreenContainer: css({
--- apps/rich-text-versioning/src/locations/Dialog.styles.ts
+++ apps/rich-text-versioning/src/locations/Dialog.styles.ts
@@ -1,4 +1,4 @@
-import { css } from '@emotion/css';
+import { css } from 'emotion';
 import tokens from '@contentful/f36-tokens';
 
 export const styles = {
