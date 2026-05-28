4ee0acb54d3f77a6339532d407749baa062ee19a
francobanfi <franco.banfi@external.contentful.com>
2026-05-28 14:35:35 -0300

    refactor(drive-integration): defensive preventDefault [INTEG-3889]

--- apps/drive-integration/src/utils/keyboard.ts
+++ apps/drive-integration/src/utils/keyboard.ts
@@ -2,6 +2,7 @@ export const handleMultiselectKeyDown = (e: React.KeyboardEvent) => {
   if (e.key !== 'Enter') return;
   const target = e.target as HTMLInputElement;
   if (target.type === 'checkbox') {
+    e.preventDefault();
     target.click();
     setTimeout(() => target.focus(), 0);
   }
